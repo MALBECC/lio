d6097d94b86dfbdbe2a2ace18a04bee60c3d3398
v01d <phreakuencies@gmail.com>
2007-10-12 18:16:07 +0000

    - #define para el BLOCK_SIZE en esta revision esta el test de distancias completo andando

--- gpu/cuda/test.cu
+++ gpu/cuda/test.cu
@@ -2,6 +2,8 @@
 
 #include "cuda_extra.h"
 
+#define BLOCK_SIZE 16
+
 __global__ void dists_kernel(float* coords, float* dists, int atoms);
 __global__ void dists_cached_kernel(float* coords, float* dists, int atoms);
 
@@ -15,8 +17,8 @@ extern "C" void run_test(float* coords_cpu, float* dists_cpu, int atoms) {
 	//int pitch;	
 	//cudaMallocPitch(&dists, &pitch, atoms * atoms * sizeof(float), atoms);
 	cudaMalloc((void**)&dists, atoms * atoms * sizeof(float));
-	
-	dim3 dimBlock(16, 16);
+
+	dim3 dimBlock(BLOCK_SIZE, BLOCK_SIZE);
 	dim3 dimSize(atoms / dimBlock.x, atoms / dimBlock.y);
 	//dists_kernel<<<dimSize, dimBlock>>>(coords, dists, atoms);
 	dists_cached_kernel<<<dimSize, dimBlock>>>(coords, dists, atoms);	
@@ -31,15 +33,15 @@ __global__ void dists_cached_kernel(float* coords, float* dists, int atoms)
 {
 	const uint3 pos = index(blockDim, blockIdx, threadIdx);
 
-	__shared__ float coords_block[32];
+	__shared__ float coords_block[BLOCK_SIZE * BLOCK_SIZE];
 
 	coords_block[threadIdx.x] = coords[pos.x];
-	coords_block[threadIdx.y + 16] = coords[pos.y];
+	coords_block[threadIdx.y + BLOCK_SIZE] = coords[pos.y];
 	
 	__syncthreads();
 	
-	dists[pos.x + atoms * pos.y] = coords_block[threadIdx.x] - coords_block[threadIdx.y + 16];
-	
+	dists[pos.x + atoms * pos.y] = coords_block[threadIdx.x] - coords_block[threadIdx.y + BLOCK_SIZE];
+
 	__syncthreads();
 }
 

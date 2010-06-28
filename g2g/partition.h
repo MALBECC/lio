46242a0654a25be78b3c549d32bd654e4be946cc
v01d <phreakuencies@gmail.com>
2010-06-28 14:54:12 -0300

    Pruebas con version sse2

--- g2g/partition.h
+++ g2g/partition.h
@@ -29,8 +29,10 @@ class PointGroup {
     std::vector<uint> nuc_map;
     #if CPU_KERNELS
     G2G::HostMatrixFloat function_values;
-    G2G::HostMatrixFloat3 gradient_values;
-    G2G::HostMatrixFloat3 hessian_values;
+    //G2G::HostMatrixFloat3 gradient_values;
+    G2G::HostMatrixCFloat3 gradient_values;
+    //G2G::HostMatrixFloat3 hessian_values;
+    G2G::HostMatrixCFloat3 hessian_values;
     #else
     G2G::CudaMatrixFloat function_values;
     G2G::CudaMatrixFloat4 gradient_values;

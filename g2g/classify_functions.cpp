776967add1c4886218e125a69392db216f26466f
v01d <phreakuencies@gmail.com>
2011-05-02 13:46:36 -0300

    classify_functions.cpp tab2space

--- g2g/classify_functions.cpp
+++ g2g/classify_functions.cpp
@@ -18,12 +18,12 @@ using namespace G2G;
 
 void Cube::assign_significative_functions(const double3& cube_coord, const vector<double>& min_exps)
 {
-	uint func = 0;
+  uint func = 0;
 
-	HostMatrix<double> atom_cube_dists(fortran_vars.atoms);
-	for (uint i = 0; i < fortran_vars.atoms; i++) {
-		const double3& atom_pos = fortran_vars.atom_positions(i);
-		double3 dist_vec;
+  HostMatrix<double> atom_cube_dists(fortran_vars.atoms);
+  for (uint i = 0; i < fortran_vars.atoms; i++) {
+    const double3& atom_pos = fortran_vars.atom_positions(i);
+    double3 dist_vec;
 
     for (uint j = 0; j < 3; j++) {
       if (elem(atom_pos,j) < elem(cube_coord,j))
@@ -33,43 +33,43 @@ void Cube::assign_significative_functions(const double3& cube_coord, const vecto
       else
         elem(dist_vec,j) = 0;
     }
-		
-		double len = length(dist_vec);
-		atom_cube_dists(i) = len * len;
-	}
+    
+    double len = length(dist_vec);
+    atom_cube_dists(i) = len * len;
+  }
 
   set<uint> functions_set;
   set<uint> nucleii_set;
-	
-	/** S **/
-	while (func < fortran_vars.s_funcs) {
+  
+  /** S **/
+  while (func < fortran_vars.s_funcs) {
     uint atom_nuc = fortran_vars.nucleii(func) - 1;
-		if (assign_all_functions || (min_exps[func] * atom_cube_dists(atom_nuc)) < max_function_exponent) {
-			functions_set.insert(func); s_functions++;
-			nucleii_set.insert(atom_nuc);
-		}
-		func++;
-	}
-	
-	/** P **/
-	while (func < fortran_vars.s_funcs + fortran_vars.p_funcs * 3) {
+    if (assign_all_functions || (min_exps[func] * atom_cube_dists(atom_nuc)) < max_function_exponent) {
+      functions_set.insert(func); s_functions++;
+      nucleii_set.insert(atom_nuc);
+    }
+    func++;
+  }
+  
+  /** P **/
+  while (func < fortran_vars.s_funcs + fortran_vars.p_funcs * 3) {
     uint atom_nuc = fortran_vars.nucleii(func) - 1;
-		if (assign_all_functions || (min_exps[func] * atom_cube_dists(atom_nuc)) < max_function_exponent) {
-			functions_set.insert(func); p_functions++;
-			nucleii_set.insert(atom_nuc);
-		}
-		func += 3;
-	}
-	
-	/** D **/
-	while (func < fortran_vars.s_funcs + fortran_vars.p_funcs * 3 + fortran_vars.d_funcs * 6) {
+    if (assign_all_functions || (min_exps[func] * atom_cube_dists(atom_nuc)) < max_function_exponent) {
+      functions_set.insert(func); p_functions++;
+      nucleii_set.insert(atom_nuc);
+    }
+    func += 3;
+  }
+  
+  /** D **/
+  while (func < fortran_vars.s_funcs + fortran_vars.p_funcs * 3 + fortran_vars.d_funcs * 6) {
     uint atom_nuc = fortran_vars.nucleii(func) - 1;
-		if (assign_all_functions || (min_exps[func] * atom_cube_dists(atom_nuc)) < max_function_exponent) {
-			functions_set.insert(func); d_functions++;
-			nucleii_set.insert(atom_nuc);
-		}
-		func += 6;
-	}
+    if (assign_all_functions || (min_exps[func] * atom_cube_dists(atom_nuc)) < max_function_exponent) {
+      functions_set.insert(func); d_functions++;
+      nucleii_set.insert(atom_nuc);
+    }
+    func += 6;
+  }
 
   local2global_func.resize(functions_set.size());
   copy(functions_set.begin(), functions_set.end(), local2global_func.begin());
@@ -84,56 +84,56 @@ void Cube::assign_significative_functions(const double3& cube_coord, const vecto
  * Sphere
  *****************************/
 void Sphere::assign_significative_functions(const std::vector<double>& min_exps) {
- 	uint func = 0;
+   uint func = 0;
 
   // TODO: esto solo es necesario para los atomos en nucleii, idem arriba
-	HostMatrix<double> atom_sphere_dists(fortran_vars.atoms);
+  HostMatrix<double> atom_sphere_dists(fortran_vars.atoms);
   const double3& own_atom_pos = fortran_vars.atom_positions(atom);
 
-	for (uint i = 0; i < fortran_vars.atoms; i++) {
+  for (uint i = 0; i < fortran_vars.atoms; i++) {
     if (i == atom) atom_sphere_dists(i) = 0;
     else {
       const double3& atom_pos = fortran_vars.atom_positions(i);
       double3 dist_vec = (atom_pos - own_atom_pos);
-			double dist_to_atom = length(dist_vec);
-			double dist = (radius > dist_to_atom ? 0 : dist_to_atom - radius);
+      double dist_to_atom = length(dist_vec);
+      double dist = (radius > dist_to_atom ? 0 : dist_to_atom - radius);
       atom_sphere_dists(i) = dist * dist;
     }
-	}
+  }
 
   set<uint> functions_set;
   set<uint> nucleii_set;
 
-	/** S **/
-	while (func < fortran_vars.s_funcs) {
+  /** S **/
+  while (func < fortran_vars.s_funcs) {
     uint atom_nuc = fortran_vars.nucleii(func) - 1;
-		if (assign_all_functions || (min_exps[func] * atom_sphere_dists(atom_nuc)) < max_function_exponent) {
-			functions_set.insert(func); s_functions++;
-			nucleii_set.insert(atom_nuc);
-		}
-		func++;
-	}
+    if (assign_all_functions || (min_exps[func] * atom_sphere_dists(atom_nuc)) < max_function_exponent) {
+      functions_set.insert(func); s_functions++;
+      nucleii_set.insert(atom_nuc);
+    }
+    func++;
+  }
 
-	/** P **/
-	while (func < fortran_vars.s_funcs + fortran_vars.p_funcs * 3) {
+  /** P **/
+  while (func < fortran_vars.s_funcs + fortran_vars.p_funcs * 3) {
     uint atom_nuc = fortran_vars.nucleii(func) - 1;
-		if (assign_all_functions || (min_exps[func] * atom_sphere_dists(atom_nuc)) < max_function_exponent) {
-			functions_set.insert(func); p_functions++;
-			nucleii_set.insert(atom_nuc);
-		}
+    if (assign_all_functions || (min_exps[func] * atom_sphere_dists(atom_nuc)) < max_function_exponent) {
+      functions_set.insert(func); p_functions++;
+      nucleii_set.insert(atom_nuc);
+    }
 
-		func += 3;
-	}
+    func += 3;
+  }
 
-	/** D **/
-	while (func < fortran_vars.s_funcs + fortran_vars.p_funcs * 3 + fortran_vars.d_funcs * 6) {
+  /** D **/
+  while (func < fortran_vars.s_funcs + fortran_vars.p_funcs * 3 + fortran_vars.d_funcs * 6) {
     uint atom_nuc = fortran_vars.nucleii(func) - 1;
-		if (assign_all_functions || (min_exps[func] * atom_sphere_dists(atom_nuc)) < max_function_exponent) {
-			functions_set.insert(func); d_functions++;
-			nucleii_set.insert(atom_nuc);
-		}
-		func += 6;
-	}
+    if (assign_all_functions || (min_exps[func] * atom_sphere_dists(atom_nuc)) < max_function_exponent) {
+      functions_set.insert(func); d_functions++;
+      nucleii_set.insert(atom_nuc);
+    }
+    func += 6;
+  }
 
   local2global_func.resize(functions_set.size());
   copy(functions_set.begin(), functions_set.end(), local2global_func.begin());

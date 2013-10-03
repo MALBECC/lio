cde559284e1b2513b78fdfaabaed3cdb1508f778
Manuel Ferreria <m.ferreria@gmail.com>
2013-10-03 13:24:19 -0300

    faltaba lo mas importante

---
+++ g2g/cuda/kernels/transpose.h
@@ -0,0 +1,69 @@
+#define BLOCK_DIM 16
+/*
+template<class scalar_type>
+__global__ void transpose(scalar_type *odata, scalar_type *idata, int width, int height)
+{
+   unsigned int xIndex = blockDim.x * blockIdx.x + threadIdx.x;
+   unsigned int yIndex = blockDim.y * blockIdx.y + threadIdx.y;
+
+   if (xIndex < width && yIndex < height)
+   {
+       unsigned int index_in  = xIndex + width * yIndex;
+       unsigned int index_out = yIndex + height * xIndex;
+       odata[index_out] = idata[index_in]; 
+   }
+}
+*/
+
+template<class scalar_type>
+__global__ void transpose(scalar_type *odata, scalar_type *idata, int width, int height)
+{
+	__shared__ scalar_type block[BLOCK_DIM][BLOCK_DIM+1];
+	
+	// read the matrix tile into shared memory
+	unsigned int xIndex = blockIdx.x * BLOCK_DIM + threadIdx.x;
+	unsigned int yIndex = blockIdx.y * BLOCK_DIM + threadIdx.y;
+	if((xIndex < width) && (yIndex < height))
+	{
+		unsigned int index_in = yIndex * width + xIndex;
+		block[threadIdx.y][threadIdx.x] = idata[index_in];
+	}
+
+	__syncthreads();
+
+	// write the transposed matrix tile to global memory
+	xIndex = blockIdx.y * BLOCK_DIM + threadIdx.x;
+	yIndex = blockIdx.x * BLOCK_DIM + threadIdx.y;
+	if((xIndex < height) && (yIndex < width))
+	{
+		unsigned int index_out = yIndex * height + xIndex;
+		odata[index_out] = block[threadIdx.x][threadIdx.y];
+	}
+}
+
+template<class scalar_type>
+__global__ void transpose_vec(vec_type<scalar_type,WIDTH> *odata, vec_type<scalar_type,WIDTH> *idata, int width, int height)
+{
+	__shared__ vec_type<scalar_type,WIDTH> block[BLOCK_DIM][BLOCK_DIM+1];
+	
+	// read the matrix tile into shared memory
+	unsigned int xIndex = blockIdx.x * BLOCK_DIM + threadIdx.x;
+	unsigned int yIndex = blockIdx.y * BLOCK_DIM + threadIdx.y;
+	if((xIndex < width) && (yIndex < height))
+	{
+		unsigned int index_in = yIndex * width + xIndex;
+		block[threadIdx.y][threadIdx.x] = idata[index_in];
+	}
+
+	__syncthreads();
+
+	// write the transposed matrix tile to global memory
+	xIndex = blockIdx.y * BLOCK_DIM + threadIdx.x;
+	yIndex = blockIdx.x * BLOCK_DIM + threadIdx.y;
+	if((xIndex < height) && (yIndex < width))
+	{
+		unsigned int index_out = yIndex * height + xIndex;
+		odata[index_out] = block[threadIdx.x][threadIdx.y];
+	}
+}
+

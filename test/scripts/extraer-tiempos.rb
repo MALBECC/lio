2845be059a736b147cf94b63ea42dbbf9e7953ca
lab8 <lab8@Racing.fyb.uba.ar>
2014-02-28 17:28:08 -0300

    Borrando cosas inútiles (para versiones viejas)

--- test/scripts/extraer-tiempos.rb
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/ruby
-while s = gets
-  if (s =~ /iteration: (?:([0-9]+)s\. )?([0-9]+)us\./) then
-    if ($1.nil?) then puts "0.%.6u" % $2
-    else puts "%u.%.6u" % [$1,$2] end
-  end  
-end

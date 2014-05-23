afe109a198727cea3bfd7824f982dd0e38a1dc05
Manuel Ferreria <mferreria@gmail.com>
2014-05-23 14:46:46 -0300

    Reindentado matmuls

--- lioamber/matmuldiag.f
+++ lioamber/matmuldiag.f
@@ -1,30 +1,14 @@
-            subroutine matmuldiag(A,B,C,M)
+      subroutine matmuldiag(A,B,C,M)
 !!!!!!!!  Hace C=(A*B) para matrices cuadradas solo la diagonal
-       REAL*8 , intent(inout)  :: A(M,M), B(M,M), C(M,M)
-       logical ta, tb
-
-
-
-         C=0
-         do k= 1,M
-          do i=1,M
-         C(i,i)= C(i,i) + A(i,k)*B(k,i)
-         enddo
+      REAL*8 , intent(inout)  :: A(M,M), B(M,M), C(M,M)
+      logical ta, tb
+      C=0
+      do k= 1,M
+        do i=1,M
+          C(i,i)= C(i,i) + A(i,k)*B(k,i)
         enddo
+      enddo
 
-       return
-
-
-       end
-
-
-
-
-
-
-
-
-
-
-
+      return
+      end
 

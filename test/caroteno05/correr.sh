87e3d4b6e3d9e1f33137dd833b2d5c2706db8cf5
Mariano Gonzalez Lebrero <nano@caribe>
2014-07-02 16:40:29 -0300

    Mas de test

--- test/caroteno05/correr.sh
+++ test/caroteno05/correr.sh
@@ -1,14 +1,10 @@
 # 0.002394810
-LD_LIBRARY_PATH=:/home/nano/liocublas/lioamber/:/home/nano/lio/g2g/:/opt/intel/composer_xe_2013.0.079/compiler/lib/intel64:/opt/intel/mic/coi/host-linux-release/lib:/opt/intel/mic/myo/lib:/opt/intel/composer_xe_2013.0.079/mpirt/lib/intel64:/opt/intel/composer_xe_2013.0.079/ipp/../compiler/lib/intel64:/opt/intel/composer_xe_2013.0.079/ipp/lib/intel64:/opt/intel/composer_xe_2013.0.079/compiler/lib/intel64:/opt/intel/composer_xe_2013.0.079/mkl/lib/intel64:/opt/intel/composer_xe_2013.0.079/tbb/lib/intel64:
 
-runprog=/home/nano/liocublas/liosolo/liosolo
-ldd /home/nano/liocublas/liosolo/liosolo
-${runprog} -i carotenox.in -b DZVP  -c caroteno.xyz -v 
-#cp x.dip dipoleXX.dip
+LD_LIBRARY_PATH=:../../lioamber/:../../g2g/:/opt/intel/composer_xe_2013.0.079/compiler/lib/intel64:/opt/intel/mic/coi/host-linux-release/lib:/opt/intel/mic/myo/lib:/opt/intel/composer_xe_2013.0.079/mpirt/lib/intel64:/opt/intel/composer_xe_2013.0.079/ipp/../compiler/lib/intel64:/opt/intel/composer_xe_2013.0.079/ipp/lib/intel64:/opt/intel/composer_xe_2013.0.079/compiler/lib/intel64:/opt/intel/composer_xe_2013.0.079/mkl/lib/intel64:/opt/intel/composer_xe_2013.0.079/tbb/lib/intel64:
 
-#${runprog} -i aguax.in -b bases.in -c agua.xyz -v > outputY.out
-#cp y.dip dipoleYY.dip
-
-#${runprog} -i aguax.in -b bases.in -c agua.xyz -v > outputZ.out
-#cp z.dip dipoleZZ.dip
+runprog=../../liosolo/liosolo
+ echo 'estas son las bibliotecas que voy a usar'
+ echo 'CHEQUEARLAS!!!!!!!!!!'
+ldd ${runprog}
+${runprog} -i carotenox.in -b DZVP  -c caroteno.xyz -v > salida
 

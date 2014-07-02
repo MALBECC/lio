54f8b2654d2f9c562096c3c7fbad6dacc4cb00d2
Mariano Gonzalez Lebrero <nano@caribe>
2014-07-02 16:39:49 -0300

    Agregado algunos test

---
+++ test/caroteno05/correr.sh
@@ -0,0 +1,14 @@
+# 0.002394810
+LD_LIBRARY_PATH=:/home/nano/liocublas/lioamber/:/home/nano/lio/g2g/:/opt/intel/composer_xe_2013.0.079/compiler/lib/intel64:/opt/intel/mic/coi/host-linux-release/lib:/opt/intel/mic/myo/lib:/opt/intel/composer_xe_2013.0.079/mpirt/lib/intel64:/opt/intel/composer_xe_2013.0.079/ipp/../compiler/lib/intel64:/opt/intel/composer_xe_2013.0.079/ipp/lib/intel64:/opt/intel/composer_xe_2013.0.079/compiler/lib/intel64:/opt/intel/composer_xe_2013.0.079/mkl/lib/intel64:/opt/intel/composer_xe_2013.0.079/tbb/lib/intel64:
+
+runprog=/home/nano/liocublas/liosolo/liosolo
+ldd /home/nano/liocublas/liosolo/liosolo
+${runprog} -i carotenox.in -b DZVP  -c caroteno.xyz -v 
+#cp x.dip dipoleXX.dip
+
+#${runprog} -i aguax.in -b bases.in -c agua.xyz -v > outputY.out
+#cp y.dip dipoleYY.dip
+
+#${runprog} -i aguax.in -b bases.in -c agua.xyz -v > outputZ.out
+#cp z.dip dipoleZZ.dip
+

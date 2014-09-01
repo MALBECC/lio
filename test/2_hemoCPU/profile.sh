90ecee65e11f8bbc7cfcfee3b2c49391f1b76d20
Juan Pablo Darago <jpdarago@gmail.com>
2014-09-01 20:18:35 -0300

    Nueva version del script de profiling

--- test/2_hemoCPU/profile.sh
+++ test/2_hemoCPU/profile.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 runid=$(date +%Y-%m-%d_%H-%M-%S)
 dir=$(readlink -f ../performance-measures/hemo-$runid)
-amplxe-cl -collect advanced-hotspots -result-dir $dir -- ../../liosolo/liosolo -i hemo.profile.in -b DZVP -c hem.xyz
+pushd ../../g2g
+make clean && make -j cpu=1 time=1
+cd ..
+make cpu=1 time=1
+popd
+mkdir -p $dir
+amplxe-cl -collect advanced-hotspots -result-dir $dir -- ../../liosolo/liosolo -i hemo.profile.in -b DZVP -c hem.xyz > $dir/output.txt
 func=$(amplxe-cl -report hotspots -result-dir $dir 2>&1 | tail -n +5 | head -n 1 | cut -d' ' -f1)
-echo "Top consuming functions is $func"
-echo "Run down for top consuming function: "
-echo " "
 amplxe-cl -report hotspots -source-object function="$func" -result-dir $dir > $dir/rundown.txt
+amplxe-cl -report summary -result-dir $dir > $dir/summary.txt

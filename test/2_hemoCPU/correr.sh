ea4bc074326bcaa2540f50119ae1e3301bc2bcff
Juan Pablo Darago <jpdarago@gmail.com>
2014-09-11 14:06:07 -0300

    nuevo script de corrida para hemo

--- test/2_hemoCPU/correr.sh
+++ test/2_hemoCPU/correr.sh
@@ -2,6 +2,6 @@
 if [ -z "$LIOBIN" ] ; then
   LIOBIN=../../liosolo/liosolo
 fi
-export OMP_NUM_THREADS=1
+export OMP_NUM_THREADS=4
 export MKL_NUM_THREADS=1
-$LIOBIN -i hemo.in -b DZVP  -c hem.xyz -v > salida
+$LIOBIN -i hemo.in -b DZVP  -c hem.xyz -v | tee salida

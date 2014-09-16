bbebeee7203bed1d0f2c35b0ea8a5b037717a997
Juan Pablo Darago <jpdarago@gmail.com>
2014-09-16 19:59:46 -0300

    Nuevo analizador de particiones

--- test/2_hemoCPU/coalesce_sizes.py
+++ test/2_hemoCPU/coalesce_sizes.py
@@ -3,13 +3,13 @@
 import fileinput
 import collections
 
-sizes = collections.defaultdict(int)
+sizes = collections.defaultdict(list)
 lines = [line for line in fileinput.input()]
 for line in lines[1:]:
     u,_,v = map(int, line.split(" "))
-    sizes[u] += v
+    sizes[u].append(v)
 
-largest = max(sizes.values())
+largest = max(map(sum,sizes.values()))
 
 for (k,v) in sizes.items(): 
-    print "Thread %d takes %f %% of the largest thread (%d nanounits)" % (k, 100.0 * v / largest, v)
+    print "Thread %d takes %f %% of the largest thread (%d nanounits, %d items)" % (k, 100.0 * sum(v) / largest, sum(v), len(v))

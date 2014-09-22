2590a0165665193977f7a8aa0fafec866843bb29
Juan Pablo Darago <jpdarago@gmail.com>
2014-09-22 20:38:09 -0300

    Nuevo script de particiones

--- test/2_hemoCPU/partition.py
+++ test/2_hemoCPU/partition.py
@@ -24,19 +24,19 @@ class Bin(object):
 def getbinsplit(array, capacity):
     bins = [ Bin(capacity) ]
 
-    for (i,e) in enumerate(array):
-        candidates = filter(lambda i: bins[i].slack() >= e,xrange(0,len(bins)))
+    for (i,e) in array:
+        candidates = filter(lambda u: bins[u].slack() >= e,xrange(0,len(bins)))
         if not candidates:
             candidates = [len(bins)]
             bins.append( Bin(capacity) )
 
-        chosen = min(candidates, key=lambda i: bins[i].slack())
+        chosen = min(candidates, key=lambda u: bins[u].slack())
         bins[chosen].add(i,e)
 
     return bins 
 
 def getbestpartition(array, threads):
-    left, right = 0, sum(array)+1
+    left, right = 0, sum([v for u, v in array])+1
 
     while right-left > 1:
         capacity = (right + left) / 2
@@ -51,12 +51,16 @@ def getbestpartition(array, threads):
 
 if __name__ == '__main__':
     array = [int(line) for line in sys.stdin.readlines()]
+    array = zip(xrange(0,len(array)), array)
+    array = sorted(array, key=lambda p: p[1], reverse=True)
     threads = multiprocessing.cpu_count()
     if len(sys.argv) > 1:
         threads = int(sys.argv[1])
 
     print "%d %d" % (multiprocessing.cpu_count() / 2 / threads, threads)
     partition = getbestpartition(array, threads)
+
+    costs = dict(array)
     for (thread, indexes) in partition.items():
         for index in indexes:
-            print "%d %d %d" % (thread, index, array[index])
+            print "%d %d %d" % (thread, index, costs[index])

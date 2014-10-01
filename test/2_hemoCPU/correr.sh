db13fc7a96d4bbc5b78f1d42b66673f3ad330d8c
Juan Pablo Darago <jpdarago@gmail.com>
2014-10-01 20:24:13 -0300

    Nuevo script de corridas para hemo

--- test/2_hemoCPU/correr.sh
+++ test/2_hemoCPU/correr.sh
@@ -2,10 +2,11 @@
 if [ -z "$LIOBIN" ] ; then
   LIOBIN=../../liosolo/liosolo
 fi
-export OMP_NUM_THREADS=11
+export OMP_NUM_THREADS=12
 export MKL_NUM_THREADS=1
 export OMP_NESTED=true
 export MKL_DYNAMIC=true
 export KMP_AFFINITY=granularity=core,scatter
-export KMP_BLOCKTIME=0
+export LIO_OUTER_THREADS=12
+export LIO_INNER_THREADS=1
 $LIOBIN -i hemo.in -b DZVP  -c hem.xyz -v | tee salida

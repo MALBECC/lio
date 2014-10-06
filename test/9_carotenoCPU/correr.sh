610b14395175d5d98231840e0ee16c92f9da7c5c
Juan Pablo Darago <jpdarago@gmail.com>
2014-10-06 16:24:36 -0300

    Modificado correr.sh de caroteno para usar lio inner threads

--- test/9_carotenoCPU/correr.sh
+++ test/9_carotenoCPU/correr.sh
@@ -3,5 +3,10 @@ if [ -z "$LIOBIN" ] ; then
   LIOBIN=../../liosolo/liosolo
 fi
 
-$LIOBIN -i carotenox.in -b DZVP  -c caroteno.xyz -v > salida
+export OMP_NUM_THREADS=12
+export MKL_NUM_THREADS=1
+export KMP_AFFINITY=granularity=core,scatter
+export LIO_OUTER_THREADS=12
+export LIO_INNER_THREADS=1
+$LIOBIN -i carotenox.in -b DZVP  -c caroteno.xyz -v | tee salida
 

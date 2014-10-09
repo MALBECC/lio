3deab919f561968abb173fc74c3a059241bb23f0
Juan Pablo Darago <jpdarago@gmail.com>
2014-10-09 13:56:37 -0300

    Modificar los scripts de compilacion para ICC 2015

--- compilar-logeando.sh
+++ compilar-logeando.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 cd g2g && make clean && cd ..
 rm -rf liosolo/liosolo
-make -j cpu=1 time=1 openmp=1 cpu_recompute=0 > >(tee stdout.log) 2> >(tee stderr.log >&2)
+make -j cpu=1 time=1 openmp=1 cpu_recompute=0 
+find . -type f -name "*.optrpt" | xargs cat > opt-reports.txt

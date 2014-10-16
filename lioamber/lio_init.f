204e03377d2b7cbb28616ae78d2890a152e8b0eb
Francisco <francisco@eneas.(none)>
2014-10-16 14:12:18 -0300

    Minor aesthethical changes in lio_init.

--- lioamber/lio_init.f
+++ lioamber/lio_init.f
@@ -3,15 +3,24 @@
 !--------------------------------------------------------------------!
 !
 ! (*) Allocation of globar variables.
+! (*) Open files to write.
 !
 !%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%!
        use garcha_mod
        implicit none
        call g2g_timer_start('lio_init')
        write(6,'(A)') ''
+!
+!
+! VARIABLE INITIALIZATION
+!----------------------------------------------------------!
        write(6,'(A)') ' (*) Initializing lio variables...'
        if (.not.allocated(Smat))    allocate(Smat(M,M))
        if (.not.allocated(RealRho)) allocate(RealRho(M,M))
+!
+!
+!
+!----------------------------------------------------------!
        call g2g_timer_stop('lio_init')
        return;end subroutine
 !%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%!

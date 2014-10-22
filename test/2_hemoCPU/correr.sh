5a50e56881f35a2b59ba6ca6f137a9e069df7298
Juan Pablo Darago <jpdarago@gmail.com>
2014-10-22 14:02:12 -0300

    Nuevo script de corridas

--- test/2_hemoCPU/correr.sh
+++ test/2_hemoCPU/correr.sh
@@ -6,7 +6,8 @@ export OMP_NUM_THREADS=12
 export MKL_NUM_THREADS=1
 export OMP_NESTED=true
 export MKL_DYNAMIC=true
-export KMP_AFFINITY=granularity=core,scatter
+export KMP_AFFINITY=granularity=fine,scatter
 export LIO_OUTER_THREADS=12
 export LIO_INNER_THREADS=1
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$(readlink -f ../../g2g):$(readlink -f ../../lioamber)
 $LIOBIN -i hemo.in -b DZVP  -c hem.xyz -v | tee salida

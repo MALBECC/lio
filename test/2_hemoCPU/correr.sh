33791f789984d77b4b483e600dda0039fed421c5
Juan Pablo Darago <jpdarago@gmail.com>
2014-10-22 15:41:53 -0300

    Nuevo script de corridas con fecha y hora

--- test/2_hemoCPU/correr.sh
+++ test/2_hemoCPU/correr.sh
@@ -4,10 +4,10 @@ if [ -z "$LIOBIN" ] ; then
 fi
 export OMP_NUM_THREADS=12
 export MKL_NUM_THREADS=1
-export OMP_NESTED=true
-export MKL_DYNAMIC=true
+export OMP_NESTED=false
+export MKL_DYNAMIC=false
 export KMP_AFFINITY=granularity=fine,scatter
 export LIO_OUTER_THREADS=12
 export LIO_INNER_THREADS=1
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$(readlink -f ../../g2g):$(readlink -f ../../lioamber)
-$LIOBIN -i hemo.in -b DZVP  -c hem.xyz -v | tee salida
+$LIOBIN -i hemo.in -b DZVP  -c hem.xyz -v | tee salida-$(date +"%Y-%m-%d_%H-%M-%S")

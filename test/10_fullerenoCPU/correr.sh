504df595a8e5b8340fb266d9ae9854d1740810c4
manuelF <m.ferreria@gmail.com>
2014-10-25 11:48:58 -0300

    Agregado a los correr que puedan recibir un parametro para decir donde dejar el archivo de corrida, sobreescribiendo el default

--- test/10_fullerenoCPU/correr.sh
+++ test/10_fullerenoCPU/correr.sh
@@ -2,6 +2,11 @@
 if [ -z "$LIOBIN" ] ; then
   LIOBIN=../../liosolo/liosolo
 fi
+SALIDA=salida
+if [ -n "$1" ]
+  then
+    SALIDA=$1
+fi
 
-$LIOBIN -i fullereno.in -b DZVP  -c fullereno.xyz -v > salida
+$LIOBIN -i fullereno.in -b DZVP  -c fullereno.xyz -v > $SALIDA
 
--- test/3_Fe3H2O6GPU/correr.sh
+++ test/3_Fe3H2O6GPU/correr.sh
@@ -2,5 +2,11 @@
 if [ -z "$LIOBIN" ] ; then
   LIOBIN=../../liosolo/liosolo
 fi
-$LIOBIN -i fe3h2o.in -b DZVP  -c fe3h2o.xyz -v > salida
+SALIDA=salida
+if [ -n "$1" ]
+  then
+    SALIDA=$1
+fi
+
+$LIOBIN -i fe3h2o.in -b DZVP  -c fe3h2o.xyz -v > $SALIDA
 

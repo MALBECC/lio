c2972e1aaeff136072db1c0e37f9b78f1aa10228
Juan Pablo Darago <jpdarago@gmail.com>
2014-10-25 18:23:08 -0300

    Nuevo script de profileo para los tests

--- test/correr-profiles.py
+++ test/correr-profiles.py
@@ -1,8 +1,9 @@
-#!/usr/bin/env python
+#!/usr/bin/env python2.7
 
 import os
 import subprocess
 import re
+import click
 
 MSECS_IN_SEC = 1000000.0
 def time2nanos(spec):
@@ -12,41 +13,79 @@ def time2nanos(spec):
     return MSECS_IN_SEC * float(secs) + float(msecs)
 
 progname = "correr-profile.sh"
-testdirs = [subdir for subdir, dirs, files in os.walk(".") if progname in set(files)]
-
 flags_and_options = {
     "OMP_NUM_THREADS":      [1,12,12],
-    "MKL_NUM_THREADS":      [1,1,12],
     "LIO_INNER_THREADS":    [1,1,12],
     "LIO_OUTER_THREADS":    [1,12,1],
 }
+always_set = [("OMP_NESTED", "true")]
 
-always_set = [("OMP_NESTED", "true"), ("MKL_DYNAMIC","false")]
-enviros = [[(key, str(vals[i])) for key,vals in flags_and_options.items()] for i in xrange(0,3)] 
+def subdirs_with_benchmark(basedir):
+    return [subdir for subdir,dirs,files in os.walk(basedir) if progname in set(files)]
 
-for directory in testdirs:
-    prog = os.path.join(directory, progname)
-    path = os.path.join(os.path.abspath("."), os.path.dirname(prog))
+def tuples2str(tuples):
+    return ','.join(["{0} = {1}".format(k,v) for k,v in tuples])
 
-    times = []
-    print "Corriendo %s" % prog
-    for enviro in enviros:
-        env = os.environ.copy()
-        for key,val in enviro + always_set:
-            env[key] = val
+def print_file(filename):
+    with open(filename) as f:
+        for line in f.readlines():
+            print "--> " + line,
+
+@click.command()
+@click.option("--regex", default=".*", help="Filtro para los tests")
+@click.option("--gpu_opts", default="gpu_options", help="Archivo gpu_options a usar (local a la carpeta)")
+def benchmark(regex, gpu_opts):
+    """ 
+    Correr los correr-profile.sh de todas las carpetas que lo posean, 
+    y generar un reporte de cada uno.
+    """
+
+    testrx = re.compile(regex)
+    testdirs = filter(testrx.search, subdirs_with_benchmark("."))
+
+    print "Corriendo %d tests" % len(testdirs)
+
+    enviros = [[(k, str(v[i])) for k,v in flags_and_options.items()] for i in xrange(0,3)] 
+
+    for directory in testdirs:
+        prog = os.path.join(directory, progname)
+        path = os.path.join(os.path.abspath("."), os.path.dirname(prog))
+
+        times = []
+        print "Corriendo %s..." % prog
+        for enviro in enviros:
+            env = os.environ.copy()
+            for key,val in enviro + always_set:
+                env[key] = val
+            env["LIO_OPTIONS_FILE"] = gpu_opts            
+
+            print_file(os.path.join(directory,gpu_opts))
+
+            data = subprocess.Popen([os.path.join(".",progname)], env=env, cwd=path, stdout=subprocess.PIPE).communicate()[0]
+            measures = []
+
+            for line in data.splitlines():
+                groups = re.search("^iteracion total: (.*)$", line)
+
+                if groups: 
+                    measures.append(groups.group(0))
+
+                if re.search("^-->", line):
+                    print line
+
+            if len(measures) < 2:
+                print "No hay resultados para %s, revise el test" % prog
+                break
+
+            print "{0} ({1}) => {2}".format(directory, tuples2str(enviro), measures[-2])
+            times.append(time2nanos(measures[-2]))
         
-        data = subprocess.Popen(["./" + progname], env=env, cwd=path, stdout=subprocess.PIPE).communicate()[0]
-        measures = []
+        if len(times) == 0:
+            print "Ha habido un error en los resultados"
+            break
 
-        for line in data.splitlines():
-            groups = re.search("^iteracion total: (.*)$", line)
-            if groups: 
-                measures.append(groups.group(0))
-            if re.search("^-->", line):
-                print line
+        base = times[0]
+        print "speedups: %s" % " - ".join([str(base / t) for t in times])
 
-        print "{0} ({1}) => {2}".format(directory, ','.join(["{0} = {1}".format(k,v) for k,v in enviro]), measures[-2])
-        times.append(time2nanos(measures[-2]))
-
-    base = times[0]
-    print "speedups: %s" % " - ".join([str(base / t) for t in times])
+if __name__ == "__main__":
+    benchmark()

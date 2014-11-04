b2e059f5be99bd68394091a0238ec45c856a5129
manuelF <m.ferreria@gmail.com>
2014-11-04 13:37:13 -0300

    Arreglado el sortBySize que ordena por size_in_gpu

--- g2g/regenerate_partition.cpp
+++ g2g/regenerate_partition.cpp
@@ -21,7 +21,7 @@ bool comparison_by_size(const T & a, const T & b) {
     return a.size_in_gpu() < b.size_in_gpu();
 }
 template <typename T>
-void sortBySize(std::vector<T> input) {
+void sortBySize(std::vector<T>& input) {
     sort(input.begin(), input.end(), comparison_by_size<T>);
 }
 

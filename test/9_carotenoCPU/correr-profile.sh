21ffe04df64b8d9bcc5829f88ad5c4d57dc505b3
Juan Pablo Darago <jpdarago@gmail.com>
2014-11-05 15:08:50 -0300

    Eliminar el link a LD_LIBRARY_PATH de los perfiles

--- test/9_carotenoCPU/correr-profile.sh
+++ test/9_carotenoCPU/correr-profile.sh
@@ -3,5 +3,4 @@ if [ -z "$LIOBIN" ] ; then
   LIOBIN=../../liosolo/liosolo
 fi
 
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$(readlink -f ../../g2g):$(readlink -f ../../lioamber)
 $LIOBIN -i carotenox.profile.in -b DZVP -c caroteno.xyz -v 

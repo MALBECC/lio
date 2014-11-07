f2858019324ef7ff11d1c337cb8241a5c6c99793
Juan Pablo Darago <jpdarago@gmail.com>
2014-11-07 13:02:12 -0300

    Total en nanosegundos

--- g2g/timer.cpp
+++ g2g/timer.cpp
@@ -96,7 +96,7 @@ unsigned long Timer::getSec(void) const {
 }
 
 double Timer::getTotal(void) const {
-    return res.tv_nsec / 1000.0 + res.tv_sec * 1000000.0;
+    return res.tv_nsec + res.tv_sec * 1000000000.0;
 }
 
 bool Timer::operator<(const Timer& other) const {

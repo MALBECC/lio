9df4719c43f390331acf473adffef9d8b178349c
Juan Pablo Darago <jpdarago@gmail.com>
2014-11-12 20:17:25 -0300

    Nuevo script de corridas para el xeon phi

--- test/2_hemoCPU/correr-xeon-phi.sh
+++ test/2_hemoCPU/correr-xeon-phi.sh
@@ -3,15 +3,14 @@ if [ -z "$LIOBIN" ] ; then
   LIOBIN=../lio/liosolo
 fi
 
-
-export OMP_NUM_THREADS=240
-export MKL_NUM_THREADS=240
+ulimit -s unlimited
+export OMP_NUM_THREADS=60
+export MKL_NUM_THREADS=60
 export KMP_AFFINITY=explicit,granularity=fine,proclist=[1-239:1,0]
 export OMP_NESTED=false
 export MKL_DYNAMIC=false
-export LIO_OUTER_THREADS=1
-export LIO_INNER_THREADS=240
+export LIO_MINCOST_OFFSET=100000
+export LIO_SPLIT_THRESHOLD=20
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/xeonphifs/lio
-ulimit -s unlimited
 
 $LIOBIN -i hemo.in -b DZVP  -c hem.xyz -v | tee salida

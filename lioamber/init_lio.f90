189939f31c4d4b8eff462337595686a600dc884a
fedepedron <fedekbe@gmail.com>
2016-12-22 22:16:51 -0300

    Cleaned init_lio_amber. Now also accepts input from an optional "lio.in" file.

--- lioamber/init_lio.f90
+++ lioamber/init_lio.f90
@@ -214,13 +214,7 @@ subroutine init_lio_amber(natomin, Izin, nclatom, charge, basis_i              &
            , DENS_i , IGRID_i, IGRID2_i , timedep_i , tdstep_i                 &
            , ntdstep_i, field_i, exter_i, a0_i, epsilon_i, Fx_i, Fy_i          &
            , Fz_i, NBCH_i, propagator_i, writedens_i, tdrestart_i              &
-#ifdef MOD_AMBER
-           , basis_set_i, fitting_set_i, int_basis_i, cubegen_only_i           &
-           , cuberes_i, cubedens_i, cubedensfile_i, cubeorb_i, cubesel_i       & 
-           , cubeorbfile_i, restart_freq_i, energy_freq_i)
-#else
            )
-#endif
 
     use garcha_mod, only : basis, output, fmulliken, fcoord, OPEN, NMAX,     &
                            basis_set, fitting_set, int_basis, DIIS, ndiis,   &
@@ -242,33 +236,24 @@ subroutine init_lio_amber(natomin, Izin, nclatom, charge, basis_i              &
 
     implicit none
     integer , intent(in) :: charge, nclatom, natomin, Izin(natomin)
-#ifdef MOD_AMBER
-    character(len=40) :: basis_set_i, fitting_set_i
-    character(len=20) :: cubedensfile_i,cubeorbfile_i
-    logical           :: int_basis_i, cubegen_only_i, cubedens_i, cubeorb_i
-    integer           :: cuberes_i, cubesel_i, restart_freq_i, energy_freq_i
-#endif
     character(len=20) :: basis_i, output_i, fcoord_i, fmulliken_i, frestart_i, &
-                         frestartin_i
+                         frestartin_i, inputFile
     logical           :: verbose_i, OPEN_i, VCINP_i, predcoef_i, writexyz_i,   &
                          intsoldouble_i, DIIS_i, integ_i, DENS_i, field_i,     &
                          exter_i, writedens_i, tdrestart_i
     integer           :: NMAX_i, NUNP_i, idip_i, ndiis_i, Iexch_i, IGRID_i,    &
-                         IGRID2_i, timedep_i, ntdstep_i, NBCH_i, propagator_i
+                         IGRID2_i, timedep_i, ntdstep_i, NBCH_i, propagator_i, &
+                         dummy
     real*8            :: GOLD_i, told_i, rmax_i, rmaxs_i, dgtrig_i, tdstep_i,  &
                          a0_i, epsilon_i, Fx_i, Fy_i, Fz_i
 
-!   Gives default values to variables.       
+    ! Gives default values to variables.       
     call lio_defaults()
 
-#ifdef MOD_AMBER
-    basis_set      = basis_set_i    ; fitting_set   = fitting_set_i  ;
-    int_basis      = int_basis_i    ; cubegen_only  = cubegen_only_i ;
-    cube_res       = cuberes_i      ; cube_dens     = cubedens_i     ;
-    cube_dens_file = cubedensfile_i ; cube_orb      = cubeorb_i      ;
-    cube_sel       = cubesel_i      ; cube_orb_file = cubeorbfile_i  ;
-    restart_freq   = restart_freq_i ; energy_freq   = energy_freq_i  ;
-#endif
+    ! Checks if input file exists and writes data to namelist variables.
+    inputFile = 'lio.in'
+    call read_options(inputFile, dummy)
+
     basis          = basis_i        ; output        = output_i       ;
     fcoord         = fcoord_i       ; fmulliken     = fmulliken_i    ;
     frestart       = frestart_i     ; frestartin    = frestartin_i   ;
@@ -291,7 +276,7 @@ subroutine init_lio_amber(natomin, Izin, nclatom, charge, basis_i              &
     propagator     = propagator_i   ; writedens     = writedens_i    ;
     tdrestart      = tdrestart_i
 
-!   Initializes LIO. The last argument indicates LIO is not being used alone.
+    ! Initializes LIO. The last argument indicates LIO is not being used alone.
     call init_lio_common(natomin, Izin, nclatom, charge, 1) 
 
     return

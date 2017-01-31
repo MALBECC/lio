c37183a0a032227cf6810c33bd8286e27f93a1a1
fedepedron <fedekbe@gmail.com>
2017-01-31 11:27:10 -0300

    Finished cleaning dipnew in the remaining loops.

--- lioamber/dipnew.f90
+++ lioamber/dipnew.f90
@@ -68,7 +68,7 @@ subroutine dipnew(uDip)
            ss   = pi32 * exp(-alf*dd) / (zij*sqrt(zij))
 
            do icrd = 1, 3
-               Q(icrd)   = (a(i,ni) *r(Nuc(i),icrd) &
+               Q(icrd)   = (a(i,ni) *r(Nuc(i),icrd)     &
                          +  a(j,nj) *r(Nuc(j),icrd)) /zij
                srs(icrd) = Q(icrd)*ss
            enddo
@@ -97,16 +97,16 @@ subroutine dipnew(uDip)
        do ni=1, ncont(i)
        do nj=1, ncont(j)
            zij = a(i,ni) + a(j,nj)
-           z2  = 2.D0 * zij
+           t0  = a(i,ni) * a(j,nj)
+           alf = t0 /zij
            ss  = pi32 * exp(-alf*dd) /(zij*sqrt(zij))
            do icrd = 1, 3
-               Q(icrd)   = (a(i, ni) *r(Nuc(i), icrd) &
+               Q(icrd)   = (a(i, ni) *r(Nuc(i), icrd)     &
                          +  a(j, nj) *r(Nuc(j), icrd)) /zij
                srs(icrd) = Q(icrd)*ss
            enddo
 
-           t0  = a(i,ni) * a(j,nj)
-           alf = t0 /zij
+           z2  = 2.D0 * zij
            ccoef=c(i,ni)*c(j,nj)
 
            do l1 = 1,3
@@ -143,18 +143,18 @@ subroutine dipnew(uDip)
        dd=d(Nuc(i),Nuc(j))
        do ni = 1, ncont(i)
        do nj = 1, ncont(j)
-           zij=a(i,ni)+a(j,nj)
-           z2=2.D0*zij
-           Q(1)=(a(i,ni)*r(Nuc(i),1)+a(j,nj)*r(Nuc(j),1))/zij
-           Q(2)=(a(i,ni)*r(Nuc(i),2)+a(j,nj)*r(Nuc(j),2))/zij
-           Q(3)=(a(i,ni)*r(Nuc(i),3)+a(j,nj)*r(Nuc(j),3))/zij
+           zij = a(i,ni) + a(j,nj)
            alf=a(i,ni)*a(j,nj)/zij
-           alf2=2.D0*alf
            ss=pi32*exp(-alf*dd)/(zij*sqrt(zij))
-           srs(1)=Q(1)*ss
-           srs(2)=Q(2)*ss
-           srs(3)=Q(3)*ss
 
+           do icrd = 1, 3
+               Q(icrd)   = (a(i, ni) *r(Nuc(i), icrd)     &
+                         +  a(j, nj) *r(Nuc(j), icrd)) /zij
+               srs(icrd) = Q(icrd)*ss
+           enddo
+
+           z2 = 2.D0*zij
+           alf2=2.D0*alf
            ccoef=c(i,ni)*c(j,nj)
 
            do l1 = 1, 3
@@ -166,32 +166,25 @@ subroutine dipnew(uDip)
                aux(l1)=aux(l1)+ss/z2
 
                do l2 = 1, l1
-                   t1=Q(l2)-r(Nuc(i),l2)
-                   aux1(1)=aux(1)*t1
-                   aux1(2)=aux(2)*t1
-                   aux1(3)=aux(3)*t1
-                   f1=1.D0
+                   t1   = Q(l2) - r(Nuc(i),l2)
+                   aux1 = aux*t1
+                   f1   = 1.D0
                
                    if (l1.eq.l2) then
-                       aux1(1)=aux1(1)+srs(1)/z2
-                       aux1(2)=aux1(2)+srs(2)/z2
-                       aux1(3)=aux1(3)+srs(3)/z2
-                       f1=sq3
+                       aux1 = aux1 + srs/z2
+                       f1   = sq3
                    endif
 
-                   aux1(l2)=aux1(l2)+ps/z2
-                   l12=l1*(l1-1)/2+l2
+                   aux1(l2) = aux1(l2) + ps /z2
+                   l12      = l1*(l1-1) /2 + l2
 
                    ! The order of the d-shell should be as follows:
                    ! xx, yx, yy, zx, zy, zz (11, 21, 22, 31, 32, 33)
+                   ii = i  + l12 - 1
+                   k  = ii + ((M2-j)*(j-1))/2
+                   cc = RMM(k) * ccoef /f1
 
-                   ii=i+l12-1
-                   k=ii+((M2-j)*(j-1))/2
-                   cc = RMM(k)*ccoef/f1
-
-                   uDip(1)=uDip(1)+cc*aux1(1)
-                   uDip(2)=uDip(2)+cc*aux1(2)
-                   uDip(3)=uDip(3)+cc*aux1(3)
+                   uDip = uDip + cc *aux1
                enddo
            enddo
        enddo
@@ -206,88 +199,72 @@ subroutine dipnew(uDip)
        dd=d(Nuc(i),Nuc(j))
        do ni=1,ncont(i)
        do nj=1,ncont(j)
-           zij=a(i,ni)+a(j,nj)
-           z2=2.D0*zij
-           Q(1)=(a(i,ni)*r(Nuc(i),1)+a(j,nj)*r(Nuc(j),1))/zij
-           Q(2)=(a(i,ni)*r(Nuc(i),2)+a(j,nj)*r(Nuc(j),2))/zij
-           Q(3)=(a(i,ni)*r(Nuc(i),3)+a(j,nj)*r(Nuc(j),3))/zij
-           alf=a(i,ni)*a(j,nj)/zij
-           ss=pi32*exp(-alf*dd)/(zij*sqrt(zij))
-           srs(1)=Q(1)*ss
-           srs(2)=Q(2)*ss
-           srs(3)=Q(3)*ss
+           zij = a(i,ni) + a(j,nj)
+           alf = a(i,ni) *a(j,nj) /zij
+           ss  = pi32*exp(-alf*dd)/(zij*sqrt(zij))
 
+           do icrd = 1, 3
+               Q(icrd)   = (a(i, ni) *r(Nuc(i), icrd)     &
+                         +  a(j, nj) *r(Nuc(j), icrd)) /zij
+               srs(icrd) = Q(icrd)*ss
+           enddo
+
+           z2  = 2.D0*zij
            ccoef=c(i,ni)*c(j,nj)
 
            do l1 = 1, 3
-               t1=Q(l1)-r(Nuc(i),l1)
-               pis=ss*t1
+               t1  = Q(l1) - r(Nuc(i),l1)
+               pis = ss * t1
 
                ! aux : <pi|r|s>
-               aux(1)=t1*srs(1)
-               aux(2)=t1*srs(2)
-               aux(3)=t1*srs(3)
-               aux(l1)=aux(l1)+ss/z2
+               aux    = t1 *srs
+               aux(l1)= aux(l1) + ss /z2
 
                do l2 = 1, l1
-                   t1=Q(l2)-r(Nuc(i),l2)
-                   pjs=ss*t1 
-                   dijs=t1*pis
+                   t1   = Q(l2) - r(Nuc(i),l2)
+                   pjs  = ss *t1 
+                   dijs = t1 *pis
 
                    ! aux1 : <pj|r|s>
-                   aux1(1)=t1*srs(1)
-                   aux1(2)=t1*srs(2)
-                   aux1(3)=t1*srs(3)
-                   aux1(l2)=aux1(l2)+ss/z2
+                   aux1     = t1 *srs
+                   aux1(l2) = aux1(l2) + ss /z2
                 
                    ! aux2 : <dij|r|s>
-                   aux2(1)=t1*aux(1)
-                   aux2(2)=t1*aux(2)
-                   aux2(3)=t1*aux(3)
-                   aux2(l2)=aux2(l2)+pis/z2
-                   f1=1.D0
+                   aux2     = t1 *aux
+                   aux2(l2) = aux2(l2) + pis /z2
+                   f1 = 1.D0
 
                    if (l1.eq.l2) then
-                       f1=sq3
-                       aux2(1)=aux2(1)+srs(1)/z2
-                       aux2(2)=aux2(2)+srs(2)/z2
-                       aux2(3)=aux2(3)+srs(3)/z2
-                       dijs=dijs+ss/z2
+                       f1   = sq3
+                       aux2 = aux2 + srs /z2
+                       dijs = dijs + ss /z2
                    endif
 
                    do l3 = 1, 3
-                       t1=Q(l3)-r(Nuc(j),l3)
-                       aux3(1)=aux2(1)*t1
-                       aux3(2)=aux2(2)*t1
-                       aux3(3)=aux2(3)*t1
+                       t1   = Q(l3) - r(Nuc(j),l3)
+                       aux3 = aux2  * t1
 
                        if (l1.eq.l3) then
-                            aux3(1)=aux3(1)+aux1(1)/z2
-                            aux3(2)=aux3(2)+aux1(2)/z2
-                            aux3(3)=aux3(3)+aux1(3)/z2
+                            aux3 = aux3 + aux1 /z2
                        endif
 
                        if (l2.eq.l3) then
-                            aux3(1)=aux3(1)+aux(1)/z2
-                            aux3(2)=aux3(2)+aux(2)/z2
-                            aux3(3)=aux3(3)+aux(3)/z2
+                            aux3 = aux3 + aux  /z2
                        endif
  
-                       aux3(l3)=aux3(l3)+dijs/z2
+                       aux3(l3) = aux3(l3) + dijs /z2
  
-                       l12=l1*(l1-1)/2+l2
-                       ii=i+l12-1
-                       jj=j+l3-1
+                       l12 = l1*(l1-1) /2 + l2
+                       ii  = i + l12 - 1
+                       jj  = j + l3  - 1
            
-                       k=ii+((M2-jj)*(jj-1))/2
-                       cc=ccoef/f1*RMM(k)
+                       k  = ii + ((M2-jj)*(jj-1)) /2
+                       cc = ccoef /f1 *RMM(k)
 
-                       uDip(1)=uDip(1)+cc*aux3(1)
-                       uDip(2)=uDip(2)+cc*aux3(2)
-                       uDip(3)=uDip(3)+cc*aux3(3)
-                   enddo ! l1 loop
-               enddo     ! l2 loop
-           enddo         ! l3 loop
+                       uDip = uDip + cc *aux3
+                   enddo
+               enddo    
+           enddo        
        enddo
        enddo
    enddo
@@ -300,138 +277,106 @@ subroutine dipnew(uDip)
        dd=d(Nuc(i),Nuc(j))
        do ni=1,ncont(i)
        do nj=1,ncont(j)
-           zij=a(i,ni)+a(j,nj)
-           z2=2.D0*zij
-           Q(1)=(a(i,ni)*r(Nuc(i),1)+a(j,nj)*r(Nuc(j),1))/zij
-           Q(2)=(a(i,ni)*r(Nuc(i),2)+a(j,nj)*r(Nuc(j),2))/zij
-           Q(3)=(a(i,ni)*r(Nuc(i),3)+a(j,nj)*r(Nuc(j),3))/zij
-           alf=a(i,ni)*a(j,nj)/zij
-           alf2=2.D0*alf
-           ss=pi32*exp(-alf*dd)/(zij*sqrt(zij))
-           srs(1)=Q(1)*ss
-           srs(2)=Q(2)*ss
-           srs(3)=Q(3)*ss
+           zij = a(i,ni) + a(j,nj)
+           alf = a(i,ni) * a(j,nj) /zij
+           ss  = pi32*exp(-alf*dd)/(zij*sqrt(zij))
 
-           ccoef=c(i,ni)*c(j,nj)
+           do icrd = 1, 3
+               Q(icrd)   = (a(i, ni) *r(Nuc(i), icrd)     &
+                         +  a(j, nj) *r(Nuc(j), icrd)) /zij
+               srs(icrd) = Q(icrd)*ss
+           enddo
+
+           alf2  = 2.D0 *alf
+           z2    = 2.D0 *zij
+           ccoef = c(i,ni)*c(j,nj)
 
            do l1=1,3
-               t1=Q(l1)-r(Nuc(i),l1)
-               pis=ss*t1
+               t1  = Q(l1) - r(Nuc(i),l1)
+               pis = ss*t1
           
-               ! Aux : <pi|r|s>
-               aux(1)=t1*srs(1)
-               aux(2)=t1*srs(2)
-               aux(3)=t1*srs(3)
-               aux(l1)=aux(l1)+ss/z2
+               ! aux : <pi|r|s>
+               aux     = t1*srs
+               aux(l1) = aux(l1) + ss /z2
 
                do l2=1,l1
-                   t1=Q(l2)-r(Nuc(i),l2)
-                   pjs=ss*t1
-                   dijs=t1*pis
+                   t1   = Q(l2) - r(Nuc(i),l2)
+                   pjs  = ss *t1
+                   dijs = t1 *pis
                 
                    ! aux1 : <pj|r|s>
-                   aux1(1)=t1*srs(1)
-                   aux1(2)=t1*srs(2)
-                   aux1(3)=t1*srs(3)
-                   aux1(l2)=aux1(l2)+ss/z2
+                   aux1     = t1*srs
+                   aux1(l2) = aux1(l2)+ss/z2
  
                    ! Aux2 : (dij|r|s)
-                   aux2(1)=t1*aux(1)
-                   aux2(2)=t1*aux(2)
-                   aux2(3)=t1*aux(3)
-                   aux2(l2)=aux2(l2)+pis/z2
-                   f1=1.D0
+                   aux2     = t1*aux
+                   aux2(l2) = aux2(l2)+pis/z2
+                   f1 = 1.D0
  
                    if (l1.eq.l2) then
-                        f1=sq3
-                        aux2(1)=aux2(1)+srs(1)/z2
-                        aux2(2)=aux2(2)+srs(2)/z2
-                        aux2(3)=aux2(3)+srs(3)/z2
-                        dijs=dijs+ss/z2
+                        f1 = sq3
+                        aux2 = aux2 + srs /z2
+                        dijs = dijs + ss  /z2
                    endif
 
                    do l3=1,3
-                       t1=Q(l3)-r(Nuc(j),l3)
-                       dp=t1*dijs
+                       t1 = Q(l3) - r(Nuc(j),l3)
+                       dp = t1 *dijs
               
                        ! aux3 : (dij|r|pk)
-                       aux3(1)=aux2(1)*t1
-                       aux3(2)=aux2(2)*t1
-                       aux3(3)=aux2(3)*t1
+                       aux3 = aux2 * t1
 
                        ! aux4 : (pi|r|pk)
-                       aux4(1)=aux(1)*t1
-                       aux4(2)=aux(2)*t1
-                       aux4(3)=aux(3)*t1
+                       aux4 = aux  * t1
  
                        ! aux5 : (pj|r|pk)
-                       aux5(1)=aux1(1)*t1
-                       aux5(2)=aux1(2)*t1
-                       aux5(3)=aux1(3)*t1
+                       aux5 = aux1 * t1
 
                        if (l1.eq.l3) then
-                           dp=dp+pjs/z2
-                           aux3(1)=aux3(1)+aux1(1)/z2
-                           aux3(2)=aux3(2)+aux1(2)/z2
-                           aux3(3)=aux3(3)+aux1(3)/z2
-                           aux4(1)=aux4(1)+srs(1)/z2
-                           aux4(2)=aux4(2)+srs(2)/z2
-                           aux4(3)=aux4(3)+srs(3)/z2
+                           dp = dp + pjs /z2
+                           aux3 = aux3 + aux1 /z2
+                           aux4 = aux4 + srs  /z2
                        endif
                        if (l2.eq.l3) then
-                           dp=dp+pis/z2
-                           aux3(1)=aux3(1)+aux(1)/z2
-                           aux3(2)=aux3(2)+aux(2)/z2
-                           aux3(3)=aux3(3)+aux(3)/z2
-                           aux5(1)=aux5(1)+srs(1)/z2
-                           aux5(2)=aux5(2)+srs(2)/z2
-                           aux5(3)=aux5(3)+srs(3)/z2
+                           dp = dp + pis /z2
+                           aux3 = aux3 + aux /z2
+                           aux5 = aux5 + srs /z2
                        endif
 
-                       aux3(l3)=aux3(l3)+dijs/z2
-                       aux4(l3)=aux4(l3)+pis/z2
-                       aux5(l3)=aux5(l3)+pjs/z2
+                       aux3(l3) = aux3(l3) + dijs /z2
+                       aux4(l3) = aux4(l3) + pis  /z2
+                       aux5(l3) = aux5(l3) + pjs  /z2
 
                        do l4=1,l3
-                           t1=Q(l4)-r(Nuc(j),l4)
+                           t1 = Q(l4) - r(Nuc(j),l4)
  
                            ! aux3 : used here for (d|r|d)
-                           aux6(1)=aux3(1)*t1
-                           aux6(2)=aux3(2)*t1
-                           aux6(3)=aux3(3)*t1
+                           aux6 = aux3 * t1
 
                            if (l1.eq.l4) then
-                               aux6(1)=aux6(1)+aux5(1)/z2
-                               aux6(2)=aux6(2)+aux5(2)/z2
-                               aux6(3)=aux6(3)+aux5(3)/z2
+                               aux6 = aux6 + aux5 /z2
                            endif
 
                            if (l2.eq.l4) then
-                               aux6(1)=aux6(1)+aux4(1)/z2
-                               aux6(2)=aux6(2)+aux4(2)/z2
-                               aux6(3)=aux6(3)+aux4(3)/z2
+                               aux6 = aux6 + aux4 /z2
                            endif
 
                            f2=1.D0
                            if (l3.eq.l4) then
-                               f2=sq3
-                               aux6(1)=aux6(1)+aux2(1)/z2
-                               aux6(2)=aux6(2)+aux2(2)/z2
-                               aux6(3)=aux6(3)+aux2(3)/z2
+                               f2   = sq3
+                               aux6 = aux6 + aux2 /z2
                            endif
 
-                           aux6(l4)=aux6(l4)+dp/z2
-                           l12=l1*(l1-1)/2+l2
-                           l34=l3*(l3-1)/2+l4
-                           ii=i+l12-1
-                           jj=j+l34-1
+                           aux6(l4) = aux6(l4) + dp /z2
+                           l12 = l1 *(l1-1) /2 + l2
+                           l34 = l3 *(l3-1) /2 + l4
+                           ii  = i + l12 -1
+                           jj  = j + l34 -1
 
                            if (ii.ge.jj) then
-                               k=ii+((M2-jj)*(jj-1))/2
-                               cc=ccoef/(f1*f2)*RMM(k)
-                               uDip(1)=uDip(1)+cc*aux6(1)
-                               uDip(2)=uDip(2)+cc*aux6(2)
-                               uDip(3)=uDip(3)+cc*aux6(3)
+                               k    = ii + ((M2-jj)*(jj-1)) /2
+                               cc   = ccoef / (f1*f2) * RMM(k)
+                               uDip = uDip + cc*aux6
                            endif
                        enddo
                    enddo

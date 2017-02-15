4b90219f9d011bf97fe3f7dc6a675d889b8dece5
fedepedron <fedekbe@gmail.com>
2017-02-15 17:11:03 -0300

    Repaired the fterm bias potential test.

--- lioamber/utests/test-ftermbias.f90
+++ lioamber/utests/test-ftermbias.f90
@@ -1,166 +1,94 @@
 !%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%!
-       include "../vvfterm.f"
-       program vvfterm_test01
-       implicit none
-       real*8   :: sqsmat(3,3),outmat(3,3)
-       integer  :: vector(3)
-       real*8   :: weight
+include "../fterm_biaspot.f"
+program test_ftermbias
+    implicit none
+    real*8       :: sqsmat(3,3),outmat(3,3), weight, criteria
+    integer      :: vector(3)
+    logical      :: testCond
+    character*20 :: testResult
 
-       write(*,*) ''
-       write(*,*) ''
+    criteria = 0.000000001d0
+    write(*,*) '- FTerm Bias Potential Tests -'
 
+    ! Tests for null SQS matrix.
+    sqsmat = 0.0d0
+    vector = 1
+    weight = 1.0d0
+    outmat = 0.0d0
+    testResult = "FAILED"
+    
+    call fterm_biaspot(3, sqsmat, vector, weight, outmat)
+    testCond = (maxval(abs(outmat)) < criteria)
+    if (testCond) testResult = "PASSED"
+    write(*,*) testResult, ' - Obtain null matrix from null SQS.'
+  
+    ! Tests for null vector.
+    sqsmat(1,1) = 1.0d0 ; sqsmat(1,2) = 3.0d0 ; sqsmat(1,3) = 2.0d0
+    sqsmat(2,1) = 5.0d0 ; sqsmat(2,2) = 6.0d0 ; sqsmat(2,3) = 3.0d0
+    sqsmat(3,1) = 4.0d0 ; sqsmat(3,2) = 2.0d0 ; sqsmat(3,3) = 1.0d0
 
-!------------------------------------------------------------------------------!
-       sqsmat(1,1)=0.0d0
-       sqsmat(1,2)=0.0d0
-       sqsmat(1,3)=0.0d0
-       sqsmat(2,1)=0.0d0
-       sqsmat(2,2)=0.0d0
-       sqsmat(2,3)=0.0d0
-       sqsmat(3,1)=0.0d0
-       sqsmat(3,2)=0.0d0
-       sqsmat(3,3)=0.0d0
+    vector = 0
+    outmat = 0.0d0
+    testResult = "FAILED"
 
-       vector(1)=1
-       vector(2)=1
-       vector(3)=1
-       weight=1.0d0
+    call fterm_biaspot(3, sqsmat, vector, weight, outmat)
+    testCond = (maxval(abs(outmat)) < criteria)
+    if (testCond) testResult = "PASSED"
+    write(*,*) testResult, ' - Obtain null matrix from null vector.'
 
-       outmat(:,:)=0.0d0
-       call vvfterm(3,sqsmat,vector,weight,outmat)
-       write(*,*) 'null matrix from null mat:'
-       write(*,*) outmat(1,1),outmat(1,2),outmat(1,3)
-       write(*,*) outmat(2,1),outmat(2,2),outmat(2,3)
-       write(*,*) outmat(3,1),outmat(3,2),outmat(3,3)
-       write(*,*) ''
+    ! Tests for null weight.
+    vector = 1
+    weight = 0.0d0
+    outmat = 0.0d0
+    testResult = "FAILED"
 
+    call fterm_biaspot(3, sqsmat, vector, weight, outmat)
+    testCond = (maxval(abs(outmat)) < criteria)
+    if (testCond) testResult = "PASSED"
+    write(*,*) testResult, ' - Obtain null matrix from null weight.'
 
-!------------------------------------------------------------------------------!
-       sqsmat(1,1)=1.0d0
-       sqsmat(1,2)=3.0d0
-       sqsmat(1,3)=2.0d0
-       sqsmat(2,1)=5.0d0
-       sqsmat(2,2)=6.0d0
-       sqsmat(2,3)=3.0d0
-       sqsmat(3,1)=4.0d0
-       sqsmat(3,2)=2.0d0
-       sqsmat(3,3)=1.0d0
+    ! Tests for diagonal SQS matrix.
+    sqsmat      = 0.0d0
+    sqsmat(1,1) = 2.0d0
+    sqsmat(2,2) = 3.0d0
+    sqsmat(3,3) = 4.0d0
 
-       vector(1)=0
-       vector(2)=0
-       vector(3)=0
-       weight=1.0d0
+    weight = 1.0d0
+    outmat = 0.0d0
 
-       outmat(:,:)=0.0d0
-       call vvfterm(3,sqsmat,vector,weight,outmat)
-       write(*,*) 'null matrix from null vec:'
-       write(*,*) outmat(1,1),outmat(1,2),outmat(1,3)
-       write(*,*) outmat(2,1),outmat(2,2),outmat(2,3)
-       write(*,*) outmat(3,1),outmat(3,2),outmat(3,3)
-       write(*,*) ''
+    call fterm_biaspot(3, sqsmat, vector, weight, outmat)
+    testCond = (abs(outmat(1,1) - 4 ) < criteria).and. &
+               (abs(outmat(2,2) - 9 ) < criteria).and. &
+               (abs(outmat(3,3) - 16) < criteria).and. &
+               (minval(abs(outmat)) < criteria)
+    if (testCond) testResult = "PASSED"
+    write(*,*) testResult, ' - Test for diagonalized SQS matrix.'
 
+    ! Tests diagonal SQS with a 0 in vector.
+    vector    = 1
+    vector(3) = 0
+    weight    = 2.0d0
+    outmat    = 0.0d0
+   
+    call fterm_biaspot(3, sqsmat, vector, weight, outmat)
+    testCond = (abs(outmat(1,1) - 8 ) < criteria).and. &
+               (abs(outmat(2,2) - 18) < criteria).and. &
+               (minval(abs(outmat)) < criteria)
+    if (testCond) testResult = "PASSED"
+    write(*,*) testResult, ' - Test for (1, 1, 0) vector.'
 
-!------------------------------------------------------------------------------!
-       sqsmat(1,1)=1.0d0
-       sqsmat(1,2)=3.0d0
-       sqsmat(1,3)=2.0d0
-       sqsmat(2,1)=5.0d0
-       sqsmat(2,2)=6.0d0
-       sqsmat(2,3)=3.0d0
-       sqsmat(3,1)=4.0d0
-       sqsmat(3,2)=2.0d0
-       sqsmat(3,3)=1.0d0
+    ! Tests for all 1.
+    sqsmat = 1.0d0
+    vector = 1
+    weight = 1.0d0
 
-       vector(1)=1
-       vector(2)=1
-       vector(3)=1
-       weight=0.0d0
+    call fterm_biaspot(3, sqsmat, vector, weight, outmat)
+        testCond = (abs(outmat(1,1) - 11) < criteria).and. &
+                   (abs(outmat(2,2) - 21) < criteria).and. &
+                   (minval(abs(outmat-3)) < criteria)
+    if (testCond) testResult = "PASSED"
+    write(*,*) testResult, ' - Test for all 1 SQS, vector, and weight.'
 
-       outmat(:,:)=0.0d0
-       call vvfterm(3,sqsmat,vector,weight,outmat)
-       write(*,*) 'null matrix from null weight:'
-       write(*,*) outmat(1,1),outmat(1,2),outmat(1,3)
-       write(*,*) outmat(2,1),outmat(2,2),outmat(2,3)
-       write(*,*) outmat(3,1),outmat(3,2),outmat(3,3)
-       write(*,*) ''
-
-
-!------------------------------------------------------------------------------!
-       sqsmat(1,1)=2.0d0
-       sqsmat(1,2)=0.0d0
-       sqsmat(1,3)=0.0d0
-       sqsmat(2,1)=0.0d0
-       sqsmat(2,2)=3.0d0
-       sqsmat(2,3)=0.0d0
-       sqsmat(3,1)=0.0d0
-       sqsmat(3,2)=0.0d0
-       sqsmat(3,3)=4.0d0
-
-       vector(1)=1
-       vector(2)=1
-       vector(3)=1
-       weight=1.0d0
-
-       outmat(:,:)=0.0d0
-       call vvfterm(3,sqsmat,vector,weight,outmat)
-       write(*,*) '4,9,16 id matrix:'
-       write(*,*) outmat(1,1),outmat(1,2),outmat(1,3)
-       write(*,*) outmat(2,1),outmat(2,2),outmat(2,3)
-       write(*,*) outmat(3,1),outmat(3,2),outmat(3,3)
-       write(*,*) ''
-
-
-!------------------------------------------------------------------------------!
-       sqsmat(1,1)=2.0d0
-       sqsmat(1,2)=0.0d0
-       sqsmat(1,3)=0.0d0
-       sqsmat(2,1)=0.0d0
-       sqsmat(2,2)=3.0d0
-       sqsmat(2,3)=0.0d0
-       sqsmat(3,1)=0.0d0
-       sqsmat(3,2)=0.0d0
-       sqsmat(3,3)=4.0d0
-
-       vector(1)=1
-       vector(2)=1
-       vector(3)=0
-       weight=2.0d0
-
-       outmat(:,:)=0.0d0
-       call vvfterm(3,sqsmat,vector,weight,outmat)
-       write(*,*) '2*4,2*9,0 id matrix:'
-       write(*,*) outmat(1,1),outmat(1,2),outmat(1,3)
-       write(*,*) outmat(2,1),outmat(2,2),outmat(2,3)
-       write(*,*) outmat(3,1),outmat(3,2),outmat(3,3)
-       write(*,*) ''
-
-
-!------------------------------------------------------------------------------!
-       sqsmat(1,1)=1.0d0
-       sqsmat(1,2)=1.0d0
-       sqsmat(1,3)=1.0d0
-       sqsmat(2,1)=1.0d0
-       sqsmat(2,2)=1.0d0
-       sqsmat(2,3)=1.0d0
-       sqsmat(3,1)=1.0d0
-       sqsmat(3,2)=1.0d0
-       sqsmat(3,3)=1.0d0
-
-       vector(1)=1
-       vector(2)=1
-       vector(3)=1
-       weight=1.0d0
-
-       call vvfterm(3,sqsmat,vector,weight,outmat)
-       write(*,*) 'previows + 3 matrix:'
-       write(*,*) outmat(1,1),outmat(1,2),outmat(1,3)
-       write(*,*) outmat(2,1),outmat(2,2),outmat(2,3)
-       write(*,*) outmat(3,1),outmat(3,2),outmat(3,3)
-       write(*,*) ''
-
-
-!------------------------------------------------------------------------------!
-       write(*,*) ''
-       write(*,*) ''
-       return; end program
+    return
+end program test_ftermbias
 !%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%!

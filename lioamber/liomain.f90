dcc3893c7d30bdbe56316a6398f9fc957f0b0b5b
Federico N. Pedron <fedekbe@gmail.com>
2017-02-15 15:58:17 -0300

    Changes to properties file. (#82)
    * Moved the do_XXX routines from properties.f90 to liomain.f90.
    
    * Fixed a small error in the do_population routine.
    
    * Modified do_fukui to add necessary documentation.

--- lioamber/liomain.f90
+++ lioamber/liomain.f90
@@ -5,6 +5,11 @@
 ! performing in tantem with AMBER/GROMACS. Currently the routine:              !
 ! * Allocates matrices needed for SCF calculations.                            !
 ! * Calls SCF or SCFOP for closed/open shell calculations.                     !
+! Other interfacial routines included are:                                     !
+! * do_forces        (calculates forces/gradients)                             !
+! * do_dipole        (calculates dipole moment)                                !
+! * do_population_analysis (performs the analysis required)                    !
+! * do_fukui         (performs Fukui function calculation and printing)        !
 !%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%!
  
 subroutine liomain(E, dipxyz)
@@ -49,3 +54,133 @@ subroutine liomain(E, dipxyz)
 
     return
 end subroutine liomain
+
+
+!%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%!
+!%% DO_FORCES %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%!
+! Calculates forces for QM and MM regions and writes them to output.           !
+!%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%!
+subroutine do_forces(uid)
+
+    use garcha_mod, only : natom, nsol
+
+    implicit none
+    integer, intent(in) :: uid
+    integer             :: k
+    real*8, allocatable :: dxyzqm(:,:), dxyzcl(:,:)
+
+    open(unit=uid, file='forces')
+
+    allocate ( dxyzqm(3, natom) )
+    dxyzqm = 0.0
+
+    call dft_get_qm_forces(dxyzqm)
+    if (nsol.gt.0) then
+        allocate ( dxyzcl(3, natom+nsol) )
+        dxyzcl = 0.0
+        call dft_get_mm_forces(dxyzcl, dxyzqm)
+    endif
+
+    call write_forces(dxyzqm, natom, 0, uid)
+    deallocate (dxyzqm)
+
+    if(nsol.gt.0) then
+        call write_forces(dxyzcl, nsol, natom, uid)
+        deallocate (dxyzcl)
+    endif
+
+    return
+end subroutine do_forces
+!%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%!
+
+!%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%!
+!%% DO_DIPOLE %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%!
+! Sets variables up and calls dipole calculation.                              !
+!%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%!
+subroutine do_dipole(dipxyz, uid)
+    implicit none
+    integer, intent(in)    :: uid
+    real*8 , intent(inout) :: dipxyz(3)
+    real*8                 :: u
+
+    call g2g_timer_start('dipole')
+    call dip(dipxyz)
+    u = sqrt(dipxyz(1)**2 + dipxyz(2)**2 + dipxyz(3)**2)
+
+    call write_dipole(dipxyz, u, uid)
+    call g2g_timer_stop('dipole')
+
+    return
+end subroutine do_dipole
+!%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%!
+
+!%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%!
+!%% DO_POPULATION_ANALYSIS %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%!
+! Performs the different population analyisis available.                       !
+!%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%!
+subroutine do_population_analysis()
+   use garcha_mod, only : RMM, Smat, RealRho, M, Enucl, Nuc, Iz, natom, &
+                          mulliken, lowdin, sqsm
+   use ECP_mod   , only : ecpmode, IzECP
+
+   implicit none
+   integer :: M1, M5, IzUsed(natom), kk
+   real*8  :: q(natom)
+
+   ! Needed until we dispose of RMM.
+   M1=1 ; M5=1+M*(M+1)
+
+   ! Iz used to write the population file.
+   IzUsed = Iz
+   if (ecpmode) IzUsed = IzECP
+
+   ! Decompresses and fixes S and RealRho matrixes, which are needed for
+   ! population analysis.
+   call int1(Enucl)
+   call spunpack('L',M,RMM(M5),Smat)
+   call spunpack('L',M,RMM(M1),RealRho)
+   call fixrho(M,RealRho)
+
+   ! Performs Mulliken Population Analysis if required.
+   if (mulliken) then
+       call mulliken_calc(natom,M,RealRho,Smat,Nuc,Iz,q)
+       call write_population(85,natom,IzUsed,q,0)
+   endif
+   ! Performs Löwdin Population Analysis if required.
+   if (lowdin) then
+       do kk=1,natom
+           q(kk) = real(IzUsed(kk))
+       enddo
+       call lowdin_calc(M,natom,RealRho,sqsm,Nuc,q)
+       call write_population(85,natom,IzUsed,q,1)
+   endif
+
+   return
+endsubroutine do_population_analysis
+!%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%!
+
+!%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%!
+!%% DO_FUKUI %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%!
+! Performs Fukui function calls and printing.                                  !
+!%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%!
+subroutine do_fukui()
+    use garcha_mod, only : X, NCO, M, natom, Nuc, Smat, Eorbs, Iz, OPEN
+
+    implicit none
+    real*8  :: fukuim(natom), fukuin(natom), fukuip(natom), softness
+
+    if (OPEN) then
+        ! TO-DO
+        ! Add call fukui_calc_OS when openshell is available.
+    else
+        call fukui_calc(X(1,M*2+1), NCO, M, natom, Nuc, Smat, fukuim, fukuip, &
+                        fukuin, Eorbs)
+        call get_softness(Eorbs(NCO-1), Eorbs(NCO), Eorbs(NCO-1), Eorbs(NCO), &
+                          softness)
+        call write_fukui(fukuim, fukuip, fukuin, natom, Iz, softness)
+    endif
+
+    return
+end subroutine do_fukui
+!%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%!
+

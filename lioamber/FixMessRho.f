bd7ca1e7bb34152c7c06f03681e6d76a02c927a3
fedepedron <fedekbe@gmail.com>
2017-02-21 13:32:20 -0300

    Edited and cleaned fixmessrho.f

--- lioamber/FixMessRho.f
+++ lioamber/FixMessRho.f
@@ -1,50 +1,43 @@
 !%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%!
-!  FixMessRho
-!------------------------------------------------------------------------------!
-!
-! In the RMM vector, Rho is stored in packed storage but
-! each non diagonal position has dobule the real value.
-! So before putting a density matrix in RMM, non diagonal
-! positions need to be x2 (messrho) and when taking it
-! out of RMM, the non diagonal positions need to be
-! divided by two (fixrho).
-!
-! Note that fixrho modifies the matrix, whereas mess rho
-! modifies the vector.
-!
-!
+!%% fixMessRho.F90 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%!
+! In the RMM vector, Rho is stored in packed storage but each non diagonal     !
+! position has dobule the real value. So before putting a density matrix in    !
+! RMM, non diagonal positions need to be x2 (messrho) and when taking it! out  !
+! of RMM, the non diagonal positions need to be divided by two (fixrho).       !
+! Routines included:                                                           !
+! * fixrho (modifies the matrix)                                               !
+! * messrho (modifies the vector)                                              !
 !%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%!
-       SUBROUTINE fixrho(M,Matrix)
-!------------------------------------------------------------------------------!
-       implicit none
-       integer,intent(in)   :: M
-       real*8,intent(inout) :: Matrix(M,M)
-       integer              :: ii,jj
-!
-       do ii=1,M
-       do jj=1,M
-         if (ii.ne.jj) then
-           Matrix(ii,jj)=Matrix(ii,jj)/(2.0d0)
-         endif
-       enddo
-       enddo
-!
-       return;end subroutine
-!
-!%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%!
-       SUBROUTINE messrho(M,Vector)
-!------------------------------------------------------------------------------!
-       implicit none
-       integer,intent(in)   :: M
-       real*8,intent(inout) :: Vector((M*(M+1))/2)
-       integer              :: ii,jj,kk
-!
-       do ii=1,M
-       do jj=1,ii
-         kk=ii+(2*M-jj)*(jj-1)/2
-         if (ii.ne.jj) Vector(kk)=Vector(kk)*(2.0d0)
-       enddo
-       enddo
+subroutine fixrho(M, Matrix)
+    
+    implicit none
+    integer,intent(in)    :: M
+    real*8 ,intent(inout) :: Matrix(M, M)
+    integer               :: ii,jj
 
-       return;end subroutine
+    do ii=1, M
+    do jj=1, M
+        if (ii.ne.jj) Matrix(ii, jj) = Matrix(ii, jj) / (2.0d0)
+    enddo
+    enddo
+
+    return
+end subroutine fixrho
+
+subroutine messrho(M,Vector)
+ 
+    implicit none
+    integer,intent(in)    :: M
+    real*8 ,intent(inout) :: Vector((M*(M+1))/2)
+    integer               :: ii, jj, kk
+
+    do ii=1,M
+    do jj=1,ii
+        kk = ii + (2*M-jj)*(jj-1) / 2
+        if (ii.ne.jj) Vector(kk) = Vector(kk) * (2.0d0)
+    enddo
+    enddo
+
+    return
+end subroutine
 !%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%!

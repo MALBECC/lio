fe6f3106222e779d7be0f356e3e3793f01fa8115
fedepedron <fedekbe@gmail.com>
2017-02-21 15:02:55 -0300

    Edited and cleaned fterm_biaspot.

--- lioamber/fterm_biaspot.f
+++ lioamber/fterm_biaspot.f
@@ -1,26 +1,25 @@
 !%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%!
-       subroutine fterm_biaspot(M,sqsmat,vector,weight,outmat)
-       implicit none
-       integer,intent(in)   :: M
-       real*8,intent(in)    :: sqsmat(M,M)
-       integer,intent(in)   :: vector(M)
-       real*8,intent(in)    :: weight
-       real*8,intent(inout) :: outmat(M,M)
+!%% FTERM_BIASPOT.F90 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%!
+! TO-DO: EXPLANATION.
+!%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%!
+subroutine fterm_biaspot(M,sqsmat,vector,weight,outmat)
+    implicit none
+    integer, intent(in)    :: M, vector(M)
+    real*8 , intent(in)    :: weight, sqsmat(M,M)
+    real*8 , intent(inout) :: outmat(M,M)
 
-       real*8  :: newterm,foruri
-       integer :: ii,jj,kk
+    real*8  :: newterm, foruri
+    integer :: ii, jj, kk
 
-       do ii=1,M
-       do jj=1,M
-!         foruri=0.0d0
-         do kk=1,M
-           newterm=sqsmat(ii,kk)*weight*sqsmat(kk,jj)
-           outmat(ii,jj)=outmat(ii,jj)+newterm*real(vector(kk))
-!           foruri=foruri+sqsmat(ii,kk)*real(vector(kk))*sqsmat(kk,jj)
-         enddo
-!         write(666,*) ii,jj,foruri
-       enddo
-       enddo
+    do ii = 1, M
+    do jj = 1, M
+    do kk = 1, M
+        newterm       = sqsmat(ii,kk) * weight * sqsmat(kk,jj)
+        outmat(ii,jj) = outmat(ii,jj) + newterm*real(vector(kk))
+    enddo
+    enddo
+    enddo
 
-       return; end subroutine
+    return
+end subroutine fterm_biaspot
 !%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%!

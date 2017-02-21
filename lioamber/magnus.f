bfa4bdfe35d7678e43c5cd38fe68e3bd95127e0e
fedepedron <fedekbe@gmail.com>
2017-02-21 15:31:14 -0300

    Edited and cleaned magnus.

--- lioamber/magnus.f
+++ lioamber/magnus.f
@@ -1,36 +1,39 @@
 !%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%!
-       SUBROUTINE magnus(Fock,RhoOld,RhoNew,M,N,dt,factorial)
-!------------------------------------------------------------------------------!
-! Magnus propagator (N order)
-! Entrada: Fock(t+(deltat/2)), rho(t)
-! Salida:  rho6=rho(t+deltat)
+!%% MAGNUS.F90 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%!
+! This file contains the routine for an N-order magnus propagation.            !
 !%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%!
-       IMPLICIT NONE
-       INTEGER,INTENT(IN)                   :: M,N
-       REAL*8,INTENT(IN)                    :: Fock(M,M),dt
-       COMPLEX*8,INTENT(IN)                 :: RhoOld(M,M)
-       COMPLEX*8,INTENT(OUT)                :: RhoNew(M,M)
-       COMPLEX*8,ALLOCATABLE,DIMENSION(:,:) :: ConmPrev,ConmNext,Omega1
-       COMPLEX*8,ALLOCATABLE,DIMENSION(:,:) :: Scratch
-       INTEGER                              :: ii
-       COMPLEX*8,PARAMETER                  :: icmplx=CMPLX(0.0D0,1.0D0)
-       REAL*8, INTENT(IN)                   :: factorial(N)
-!------------------------------------------------------------------------------!
-! Variable initializations
-       ALLOCATE(ConmPrev(M,M),ConmNext(M,M),Omega1(M,M))
-       ALLOCATE(Scratch(M,M))
-       ConmPrev=RhoOld
-       RhoNew=RhoOld
-! Omega1 (=W) instantiation
-       Omega1=(-1)*(icmplx)*(Fock)*(dt)
-! Density matrix propagation
-       DO ii=1,N
-         ConmNext=MATMUL(Omega1,ConmPrev)
-         Scratch=MATMUL(ConmPrev,Omega1)
-         ConmNext=ConmNext-Scratch
-         RhoNew=RhoNew+factorial(ii)*ConmNext
-         ConmPrev=ConmNext
-       ENDDO
-       DEALLOCATE(ConmPrev,ConmNext,Omega1,Scratch)
-       RETURN;END
+subroutine magnus(fock, rhoOld, rhoNew, M, N, dt, factorial)
+    ! Input:  Fock(t+(deltat/2)), rho(t)
+    ! Output: rho6 = rho(t+deltat)
+    implicit none
+    integer  , intent(in)  :: M, N
+    real*8   , intent(in)  :: Fock(M,M), dt, factorial(N)
+    complex*8, intent(in)  :: RhoOld(M,M)
+    complex*8, intent(out) :: RhoNew(M,M)
+
+    complex*8, allocatable :: Scratch(:,:), ConmPrev(:,:), ConmNext(:,:), &
+                              Omega1(:,:)
+    integer                :: ii
+    complex*8, parameter   :: icmplx=CMPLX(0.0D0,1.0D0)
+
+    ! Variable initializations.
+    allocate(ConmPrev(M,M), ConmNext(M,M), Omega1(M,M), Scratch(M,M))
+    ConmPrev = RhoOld
+    RhoNew   = RhoOld
+
+    ! Omega1 (=W) instantiation.
+    ,Omega1 = (-1)*(icmplx)*(Fock)*(dt)
+
+    ! Density matrix propagation
+    do ii=1, N
+        ConmNext = MATMUL(Omega1, ConmPrev)
+        Scratch  = MATMUL(ConmPrev, Omega1)
+        ConmNext = ConmNext - Scratch
+        RhoNew   = RhoNew + factorial(ii)*ConmNext
+        ConmPrev = ConmNext
+    enddo
+
+    deallocate(ConmPrev, ConmNext, Omega1, Scratch)
+    return
+end
 !%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%!

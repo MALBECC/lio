535268219700f8a097b5af857d0d8ef160ce0c0f
fedepedron <fedekbe@gmail.com>
2017-02-24 10:09:32 -0300

    Edited and cleaned read_groups. Should this really be kept separate?

--- lioamber/read_groups.f
+++ lioamber/read_groups.f
@@ -1,18 +1,20 @@
 !%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%!
-       subroutine read_groups(N,file_name,atom_group)
-       implicit none
-       integer,intent(in)   :: N
-       character(len=*)     :: file_name
-       integer,intent(out)  :: atom_group(N)
+!%% PROPERTIES.F90 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%!
+! TO-DO: Explanation
+!%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%!
+subroutine read_groups(N, file_name, atom_group)
+    implicit none
+    integer,intent(in)   :: N
+    character(len=*)     :: file_name
+    integer,intent(out)  :: atom_group(N)
 
-       integer :: kk
-!------------------------------------------------------------------------------!
+    integer :: kk
 
-       open(unit=1001,file=file_name)
-       do kk=1,N
-         read(unit=1001,fmt=*) atom_group(kk)
-       enddo
+    open(unit=1001, file=file_name)
+    do kk=1,N
+        read(unit=1001, fmt=*) atom_group(kk)
+    enddo
 
-!------------------------------------------------------------------------------!
-       return; end subroutine
+    return
+end subroutine
 !%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%!

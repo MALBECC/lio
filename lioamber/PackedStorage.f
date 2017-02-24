8c523f0fea49fb4fccd60abc0caa55a6909c3a96
fedepedron <fedekbe@gmail.com>
2017-02-24 09:53:06 -0300

    Cleaned and edited PackedStorage.

--- lioamber/PackedStorage.f
+++ lioamber/PackedStorage.f
@@ -1,246 +1,217 @@
 !%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%!
-! PACKED STORAGE
-!------------------------------------------------------------------------------!
-!
-! This two subroutines are intended to facilitate the passage
-! from a matrix and a vector with the same information (the
-! vector has it in what is known as "packed storage" or SP).
-! The matrix must be symetric for packed storage to be used,
-! and this routines only support real values.
-!
-! NOTE: If Vector is originally of size > NM*(NM+1)/2, then
-! calling the subroutine with "Vector(i0)" will make it
-! work with the positions of the original vector that go
-! from i0 to i0+NM*(NM+1)/2-1.
-! (something similar may work with the variable 'Matrix'
-! but it will probably be more intrincated)
-!
-!-----------------------!
-! STORAGE SCHEME UPLO=U
-!-----------------------!
-!  | 1  7  8 |
-!  |(7) 2  9 |   <=>   ( 1 , 7 , 2 , 8 , 9 , 3 )
-!  |(8)(9) 3 |
-!
-!-----------------------!
-! STORAGE SCHEME UPLO=L
-!-----------------------!
-!  | 1 (7)(8)|
-!  | 7  2 (9)|   <=>   ( 1 , 7 , 8 , 2 , 9 , 3 )
-!  | 8  9  3 |
-!
-!----------------------------------------------------------!
-! For more information, visit:
-!   (*) http://www.netlib.org/lapack/lug/node123.html
-!   (*) http://www.netlib.org/lapack/lug/node24.html
-!
-! 04/2014 || F.F.R
+!%% PACKED_STORAGE.F90 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%!
+! This two subroutines are intended to facilitate the passage from a matrix and!
+! a vector with the same information (the vector has it in what is known as    !
+! "packed storage" or SP). The matrix must be symetric for packed storage to   !
+! be used, and this routines only support real values.                         !
+! Subroutines included are:                                                    !
+! * spunpack                                                                   !
+! * spunpack_rho                                                               !
+! * sprepack                                                                   !
+! * spunpack_rtc                                                               !
+! * sprepack_ctr                                                               !
+!                                                                              !
+! NOTE: If Vector is originally of size > NM*(NM+1)/2, then calling the        !
+! subroutine with "Vector(i0)" will make it work with the positions of the     !
+! original vector that go from i0 to i0+NM*(NM+1)/2-1. (something similar may  !
+! work with the variable 'Matrix' but it will probably be more intrincated).   !
+!                                                                              !
+! STORAGE SCHEME UPLO=U                                                        !
+!  | 1  7  8 |                                                                 !
+!  |(7) 2  9 |   <=>   ( 1 , 7 , 2 , 8 , 9 , 3 )                               !
+!  |(8)(9) 3 |                                                                 !
+! STORAGE SCHEME UPLO=L                                                        !
+!  | 1 (7)(8)|                                                                 !
+!  | 7  2 (9)|   <=>   ( 1 , 7 , 8 , 2 , 9 , 3 )                               !
+!  | 8  9  3 |                                                                 !
+!                                                                              !
+! For more information, visit:                                                 !
+!   (*) http://www.netlib.org/lapack/lug/node123.html                          ! 
+!   (*) http://www.netlib.org/lapack/lug/node24.html                           !
+! 04/2014 || F.F.R                                                             !
 !%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%!
-       SUBROUTINE spunpack(UPLO,NM,Vector,Matrix)
-       IMPLICIT NONE
-       CHARACTER(LEN=1)   :: UPLO
-       INTEGER,INTENT(IN) :: NM
-       REAL*8,INTENT(IN)  :: Vector(NM*(NM+1)/2)
-       REAL*8,INTENT(OUT) :: Matrix(NM,NM)
-       INTEGER            :: ii,jj,idx
-!
-!------------------------------------------------------------------------------!
-!
-       IF (UPLO.EQ.'U') THEN
-         DO jj=1,NM
-         DO ii=1,jj
-           idx=ii+(jj*(jj-1)/2)
-           Matrix(ii,jj)=Vector(idx)
-           Matrix(jj,ii)=Vector(idx)
-         ENDDO
-         ENDDO
+subroutine spunpack(UPLO, NM, Vector, Matrix)
+    implicit none
+    character(len=1)    :: UPLO
+    integer,intent(in)  :: NM
+    real*8 ,intent(in)  :: Vector(NM*(NM+1)/2)
+    real*8 ,intent(out) :: Matrix(NM,NM)
+    integer             :: ii, jj, idx
 
-       ELSE IF (UPLO.EQ.'L') THEN
-         DO ii=1,NM
-         DO jj=1,ii
-           idx=ii+(2*NM-jj)*(jj-1)/2
-           Matrix(ii,jj)=Vector(idx)
-           Matrix(jj,ii)=Vector(idx)
-         ENDDO
-         ENDDO
+    if (UPLO.eq.'U') then
+        do jj = 1, NM
+        do ii = 1, jj
+            idx = ii + (jj*(jj-1)/2)
+            Matrix(ii,jj) = Vector(idx)
+            Matrix(jj,ii) = Vector(idx)
+        enddo
+        enddo
 
-       ELSE
-         PRINT*,'NOT GOOD INPUT FOR UPLO'
+    else if (UPLO.eq.'L') then
+        do ii = 1, NM
+        do jj = 1, ii
+            idx = ii + (2*NM-jj)*(jj-1)/2
+            Matrix(ii,jj) = Vector(idx)
+            Matrix(jj,ii) = Vector(idx)
+        enddo
+        enddo
 
-       ENDIF
-!
-!------------------------------------------------------------------------------!
-       RETURN;END SUBROUTINE
+    else
+        write(*,*) 'spunpack: Wrong value in UPLO.'
+    endif
+    
+    return
+end subroutine spunpack
 
+subroutine spunpack_rho(UPLO, NM, Vector, Matrix)
+    implicit none
+    character(len=1)     :: UPLO
+    integer, intent(in)  :: NM
+    real*8 , intent(in)  :: Vector(NM*(NM+1)/2)
+    real*8 , intent(out) :: Matrix(NM,NM)
+    integer              :: ii, jj, idx
 
-!%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%!
-       SUBROUTINE spunpack_rho(UPLO,NM,Vector,Matrix)
-       IMPLICIT NONE
-       CHARACTER(LEN=1)   :: UPLO
-       INTEGER,INTENT(IN) :: NM
-       REAL*8,INTENT(IN)  :: Vector(NM*(NM+1)/2)
-       REAL*8,INTENT(OUT) :: Matrix(NM,NM)
-       INTEGER            :: ii,jj,idx
-!
-!------------------------------------------------------------------------------!
-!
-       IF (UPLO.EQ.'U') THEN
-         DO jj=1,NM
-            DO ii=1,jj-1
-               idx=ii+(jj*(jj-1)/2)
-               Matrix(ii,jj)=Vector(idx)/2
-               Matrix(jj,ii)=Vector(idx)/2
-            ENDDO
-               idx=jj+(jj*(jj-1)/2)
-               Matrix(jj,jj)=Vector(idx)
-         ENDDO
+    if (UPLO.eq.'U') then
+        do jj = 1, NM
+            do ii = 1, jj - 1
+               idx = ii + (jj*(jj-1)/2)
+               Matrix(ii,jj) = Vector(idx)/2
+               Matrix(jj,ii) = Vector(idx)/2
+            enddo
+               idx = jj + (jj*(jj-1)/2)
+               Matrix(jj,jj) = Vector(idx)
+        enddo
+    else if (UPLO.eq.'L') then
+        do ii = 1, NM
+            do jj = 1, ii - 1
+                idx = ii+(2*NM-jj)*(jj-1)/2
+                Matrix(ii,jj) = Vector(idx)/2
+                Matrix(jj,ii) = Vector(idx)/2
+            enddo
+            idx = jj + (2*NM-jj)*(jj-1)/2
+            Matrix(jj,jj) = Vector(idx)
+        enddo
+    else
+        write(*,*) 'spunpack_rho: Wrong value in UPLO.'
+    endif
+    
+    return
+end subroutine spunpack_rho
 
-       ELSE IF (UPLO.EQ.'L') THEN
-         DO ii=1,NM
-            DO jj=1,ii-1
-               idx=ii+(2*NM-jj)*(jj-1)/2
-               Matrix(ii,jj)=Vector(idx)/2
-               Matrix(jj,ii)=Vector(idx)/2
-            ENDDO
-               idx=jj+(2*NM-jj)*(jj-1)/2
-               Matrix(jj,jj)=Vector(idx)
-         ENDDO
+subroutine sprepack(UPLO, NM, Vector, Matrix)
+    implicit none
+    character(len=1)    :: UPLO
+    integer,intent(in)  :: NM
+    real*8 ,intent(in)  :: Matrix(NM, NM)
+    real*8 ,intent(out) :: Vector(NM*(NM+1)/2)
+    integer             :: ii, jj, idx
 
-       ELSE
-         PRINT*,'NOT GOOD INPUT FOR UPLO'
-       ENDIF
-!
-!------------------------------------------------------------------------------!
-       RETURN;END SUBROUTINE
+    if (UPLO.eq.'U') then
+        do jj = 1, NM
+        do ii = 1, jj
+            aidx = ii + (jj*(jj-1)/2)
+            Vector(idx) = Matrix(ii,jj)
+        enddo
+        enddo
+    else if (UPLO.eq.'L') then
+        do ii = 1, NM
+        do jj = 1, ii
+           idx = ii + (2*NM-jj)*(jj-1)/2
+           Vector(idx) = Matrix(ii,jj)
+        enddo
+        enddo
+    else
+         write(*,*) 'sprepack: Wrong value in UPLO.'    
+    endif
 
+    return 
+end subroutine sprepack
 
-!%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%!
-       SUBROUTINE sprepack(UPLO,NM,Vector,Matrix)
-       IMPLICIT NONE
-       CHARACTER(LEN=1)   :: UPLO
-       INTEGER,INTENT(IN) :: NM
-       REAL*8,INTENT(OUT) :: Vector(NM*(NM+1)/2)
-       REAL*8,INTENT(IN)  :: Matrix(NM,NM)
-       INTEGER            :: ii,jj,idx
-!
-!------------------------------------------------------------------------------!
-!
-       IF (UPLO.EQ.'U') THEN
-         DO jj=1,NM
-         DO ii=1,jj
-           idx=ii+(jj*(jj-1)/2)
-           Vector(idx)=Matrix(ii,jj)
-         ENDDO
-         ENDDO
-
-       ELSE IF (UPLO.EQ.'L') THEN
-         DO ii=1,NM
-         DO jj=1,ii
-           idx=ii+(2*NM-jj)*(jj-1)/2
-           Vector(idx)=Matrix(ii,jj)
-         ENDDO
-         ENDDO
-
-       ELSE
-         PRINT*,'NOT GOOD INPUT FOR UPLO'
-
-       ENDIF
-!
-!------------------------------------------------------------------------------!
-       RETURN;END SUBROUTINE
-
-
-!%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%!
-       SUBROUTINE spunpack_rtc(UPLO,NM,Vector,Matrix)
-       IMPLICIT NONE
-       CHARACTER(LEN=1)   :: UPLO
-       INTEGER,INTENT(IN) :: NM
-       REAL*8,INTENT(IN)  :: Vector(NM*(NM+1)/2)
+subroutine spunpack_rtc(UPLO, NM, Vector, Matrix)
+    implicit none
+    character(len=1)        :: UPLO
+    integer   , intent(in)  :: NM
+    real*8    , intent(in)  :: Vector(NM*(NM+1)/2)
 #ifdef TD_SIMPLE
-       COMPLEX*8,INTENT(OUT) :: Matrix(NM,NM)
+    complex*8 , intent(out) :: Matrix(NM,NM)
 #else
-       COMPLEX*16,INTENT(OUT) :: Matrix(NM,NM)
+    complex*16, intent(out) :: Matrix(NM,NM)
 #endif
-       INTEGER            :: ii,jj,idx
-!
-!------------------------------------------------------------------------------!
-!
-       IF (UPLO.EQ.'U') THEN
-         DO jj=1,NM
-            DO ii=1,jj-1
-               idx=ii+(jj*(jj-1)/2)
-                  Matrix(ii,jj)=cmplx(Vector(idx),0.0D0)
-                  Matrix(ii,jj)=Matrix(ii,jj)*0.50D0      ! RHO CASE
-                  Matrix(jj,ii)=Matrix(ii,jj)
-             ENDDO
-             idx=ii+(ii*(ii-1)/2)
-             Matrix(ii,ii)=cmplx(Vector(idx),0.0D0)
-          ENDDO
-       ELSE IF (UPLO.EQ.'L') THEN
-         DO ii=1,NM
-            DO jj=1,ii-1
-               idx=ii+(2*NM-jj)*(jj-1)/2
-               Matrix(ii,jj)=cmplx(Vector(idx),0.0D0)
-               Matrix(ii,jj)=Matrix(ii,jj)*0.50D0
-            ENDDO     
-            Matrix(ii,ii)=Vector(ii+(2*NM-ii)*(ii-1)/2)
-            DO jj=ii+1,NM
-               idx=jj+(2*NM-ii)*(ii-1)/2
-               Matrix(ii,jj)=cmplx(Vector(idx),0.0D0)
-               Matrix(ii,jj)=Matrix(ii,jj)*0.50D0
-            ENDDO
-         ENDDO
-       ELSE
-         PRINT*,'NOT GOOD INPUT FOR UPLO'
-       ENDIF
-!
-!------------------------------------------------------------------------------!
-       RETURN;END SUBROUTINE
-!%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%!
-       SUBROUTINE sprepack_ctr(UPLO,NM,Vector,Matrix)
-       IMPLICIT NONE
-       CHARACTER(LEN=1)   :: UPLO
-       INTEGER,INTENT(IN) :: NM
-       REAL*8,INTENT(OUT) :: Vector(NM*(NM+1)/2)
+    integer                 :: ii, jj, idx
+
+    if (UPLO.eq.'U') then
+        do jj = 1, NM
+            do ii = 1, jj - 1
+                idx = ii + (jj*(jj-1)/2)
+                Matrix(ii,jj) = cmplx(Vector(idx),0.0D0)
+                Matrix(ii,jj) = Matrix(ii,jj)*0.50D0
+                Matrix(jj,ii) = Matrix(ii,jj)
+            enddo
+            idx = ii + (ii*(ii-1)/2)
+            Matrix(ii,ii) = cmplx(Vector(idx),0.0D0)
+        enddo
+    else if (UPLO.eq.'L') then
+        do ii=1,NM
+            do jj = 1, ii - 1
+                idx = ii + (2*NM-jj)*(jj-1)/2
+                Matrix(ii,jj) = cmplx(Vector(idx),0.0D0)
+                Matrix(ii,jj) = Matrix(ii,jj)*0.50D0
+            enddo     
+            Matrix(ii,ii) = Vector(ii+(2*NM-ii)*(ii-1)/2)
+            do jj = ii + 1, NM
+                idx = jj + (2*NM-ii)*(ii-1)/2
+                Matrix(ii,jj) = cmplx(Vector(idx),0.0D0)
+                Matrix(ii,jj) = Matrix(ii,jj)*0.50D0
+            enddo
+        enddo
+    else
+        write(*,*) 'spunpack_rtc: Wrong value in UPLO.'
+    endif
+
+    return
+end subroutine spunpack_rtc
+
+subroutine sprepack_ctr(UPLO,NM,Vector,Matrix)
+    implicit none
+    character(len=1)        :: UPLO
+    integer   ,intent(in)   :: NM
+    real*8    , intent(out) :: Vector(NM*(NM+1)/2)
 #ifdef TD_SIMPLE
-       COMPLEX*8,INTENT(IN) :: Matrix(NM,NM)
+    complex*8 , intent(in)  :: Matrix(NM,NM)
 #else
-       COMPLEX*16,INTENT(IN) :: Matrix(NM,NM)
+    complex*16, intent(in)  :: Matrix(NM,NM)
 #endif
-       INTEGER            :: ii,jj,idx
-!
-!------------------------------------------------------------------------------!
-!
-       IF (UPLO.EQ.'U') THEN
-         DO jj=1,NM
-         DO ii=1,jj
-           idx=ii+(jj*(jj-1)/2)
+    integer                 :: ii, jj, idx
+
+    if (UPLO.eq.'U') then
+        do jj = 1, NM
+        do ii = 1, jj
+           idx = ii + (jj*(jj-1)/2)
            if(ii.eq.jj) then
-              Vector(idx)=REAL(Matrix(ii,jj))
+              Vector(idx) = real(Matrix(ii,jj))
            else
-              Vector(idx)=REAL(Matrix(ii,jj))
-              Vector(idx)=Vector(idx)*2.0D0
+              Vector(idx) = real(Matrix(ii,jj))
+              Vector(idx) = Vector(idx)*2.0D0
            endif
-         ENDDO
-         ENDDO
-       ELSE IF (UPLO.EQ.'L') THEN
-         DO ii=1,NM
-         DO jj=1,ii
-           idx=ii+(2*NM-jj)*(jj-1)/2
-           IF(ii.eq.jj) THEN
-             Vector(idx)=REAL(Matrix(ii,jj))
-           ELSE
-             Vector(idx)=REAL(Matrix(ii,jj))
-             Vector(idx)=Vector(idx)*2.0D0
-           ENDIF
-         ENDDO
-         ENDDO
+        enddo
+        enddo
+    else if (UPLO.eq.'L') then
+        do ii = 1, NM
+        do jj = 1, ii
+            idx = ii + (2*NM-jj)*(jj-1)/2
+            if(ii.eq.jj) then
+                Vector(idx) = real(Matrix(ii,jj))
+            else
+                Vector(idx) = real(Matrix(ii,jj))
+                Vector(idx) = Vector(idx)*2.0D0
+           endif
+        enddo
+        enddo
+    else
+        write(*,*) 'spunpack_rtc: Wrong value in UPLO.'
+    endif
 
-       ELSE
-         PRINT*,'NOT GOOD INPUT FOR UPLO'
-       ENDIF
-!
-!------------------------------------------------------------------------------!
-       RETURN;END SUBROUTINE
+    return
+end subroutine sprepack_ctr
 !%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%!
 

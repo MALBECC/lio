387a6be0cc4171740664a17cb48b87cbf7443fb0
fedepedron <fedekbe@gmail.com>
2017-03-08 14:59:23 -0300

    Started editing func.f, removing all implicits and unused single-precision routines.

--- lioamber/func.f
+++ lioamber/func.f
@@ -1,25 +1,33 @@
-c--------------------------------------------------------------
-c this small program is taken from old version of gdfmol
-c it is the implementation of the Obara-Saika method for
-c the evaluation of F(m,T), using a 2 branch calculation
-c DEBUGGING VERSION, this is the attempt to generalize
-c and improve previous version ( up to F16 )
+!%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%!
+!%% FUNC.F90 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%!
+! Dario Original - 11/March/1992                                               !
+! This small program is taken from old version of gdfmol. It is the            !
+! implementation of the Obara-Saika method for the evaluation of F(m,T), using !
+! a 2 branch calculation.
+! DEBUGGING VERSION, this is the attempt to generalize and improve previous    !
+! version ( up to F16 ) 
 c Ref: JCP 84 3963 (1986)
 c it seems to work
 c This is the version that should be included in definitive
 c program
-c 11 March 1992 -----------------------------------------------
-c
-      FUNCTION FUNCT(N,T)                                               
-      USE garcha_mod
-      IMPLICIT REAL*8 (A-H,O-Z)                                         
-!      COMMON /TABLE/ STR(880,0:21),FAC(0:16)
-C
+!%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%!
+
+      real*8 function FUNCT(N, T)                                               
+      use garcha_mod, only : STR, FAC
+
+      implicit none
+      real*8, intent(inout)  :: T
+      integer, intent(in) :: N
+
+      integer :: it
+      real*8  :: delt, delt2, delt3, delt4, delt5, fcap, ti 
+      real*8  :: tf0, tf1, tf2, tf3, tf4, tf5
+     
       if (T.lt.0.0D0) then
        write(*,*) 'Problems',T
        T=abs(T)
       endif
-C                                                                       
+                                                                       
       IF (T.LE.43.975D0)   THEN                          
        IT = 20.0D0 * (T + 0.025D0)                                        
        TI = DFLOAT(IT)                                                   
@@ -29,92 +37,48 @@ C
        DELT4 = 0.25D0 * DELT                                             
        DELT2 = DELT4 + DELT4                                             
        DELT5 = 0.20D0 * DELT                                             
-c
-c
-c
+
+
+
        TF0 = STR(IT,N)                                                    
        TF1 = STR(IT,N+1)                                                    
        TF2 = STR(IT,N+2)                                                    
        TF3 = STR(IT,N+3)                                                    
        TF4 = STR(IT,N+4)                                                    
        TF5 = STR(IT,N+5)                                                    
-c
+
        FCAP=TF0-DELT*( TF1-DELT2*(TF2-DELT3*(TF3-DELT4*               
      >    (TF4-DELT5*TF5))))                                           
        FUNCT = FCAP                                                   
        RETURN                                                            
-C                                                                       
+                                                                       
        
        ELSE
-      FUNCT=FAC(N)*1.D0/(T**N*dsqrt(T))
+      FUNCT = FAC(N)*1.D0/(T**N*dsqrt(T))
      
       ENDIF
-c
+
       END            
   
-
-      FUNCTION FUNCTS(M,T)
-      IMPLICIT REAL*4 (A-H,O-Z)
-      COMMON /TABLES/ STRS(880,0:21),FACS(0:16)
-C
-      if (T.lt.0.0E0) then
-       write(*,*) 'Problems',T
-       T=abs(T)
-      endif
-C                                                                       
-      IF (T.LE.43.975E0)   THEN
-       IT = 20.0E0 * (T + 0.025E0)                                        
-       TI = FLOAT(IT)                                                   
-       IT=IT + 1                                                         
-       DELT = T - 0.05E0 * TI                                             
-       DELT3 = DELT * 0.3333333333E0                                
-       DELT4 = 0.25E0 * DELT                                             
-       DELT2 = DELT4 + DELT4                                             
-       DELT5 = 0.20E0 * DELT                                             
-c
-c
-c
-       TF0 = STRS(IT,M)                                                    
-       TF1 = STRS(IT,M+1)                                                    
-       TF2 = STRS(IT,M+2)                                                    
-       TF3 = STRS(IT,M+3)                                                    
-       TF4 = STRS(IT,M+4)                                                    
-       TF5 = STRS(IT,M+5)                                                    
-c
-       FCAP=TF0-DELT*( TF1-DELT2*(TF2-DELT3*(TF3-DELT4*
-     >    (TF4-DELT5*TF5))))
-       FUNCTS = FCAP
-       RETURN                                                            
-C                                                                       
-
-       ELSE
-      FUNCTS=FACS(M)*1.E0/(T**M*sqrt(T))
-      ENDIF
-c
-      END
-
-                                                   
 C ----------------------------------------------                        
 c subroutine for generating tables, used later on in the Taylor expansion
 c for the incomplete Gamma functions
 c-----------------------------------------------
       SUBROUTINE GENERF
-      USE garcha_mod
-      IMPLICIT REAL*8 (A-H,O-Z)
-      parameter (sqpi=1.77245385090551588D0)
-!      COMMON /TABLE/ STR(880,0:21),FAC(0:16)
-c
+      USE garcha_mod, only : STR, FAC
+      IMPLICIT none
+      real*8, parameter :: sqpi=1.77245385090551588D0
+      real*8 :: t, u, w, y, FMCH, f
+      integer :: i, n, m
 c loop over T values in the table ( 0. to 43.95 , interval 0.05)
-c
+
       DO 799 I=1,880
        T = 0.05D0 * DFLOAT(I-1)
        Y = DEXP(-T)
        U = T + T
        F = FMCH(22,T)
-c
 c loop over order of incomple Gamma functions ( 0 to 21, the ones
 c necessary for evaluating orders 0-16)
-c
       DO 99 M=21,0,-1
        W=2.D0*DFLOAT(M)+1.D0
        STR(I,M) = (Y + U * F)/W
@@ -132,45 +96,19 @@ c
       RETURN
       END
 
-      SUBROUTINE GENERFS
-      IMPLICIT REAL*4 (A-H,O-Z)
-      parameter (sqpi=1.772453850905E0)
-      COMMON /TABLES/ STRS(880,0:21),FACS(0:16)
-c
-c loop over T values in the table ( 0. to 43.95 , interval 0.05)
-c
-      DO 799 I=1,880
-       T = 0.05E0 * FLOAT(I-1)
-       Y = EXP(-T)
-       U = T + T
-       F = FMCHS(22,T)
-c
-c loop over order of incomple Gamma functions ( 0 to 21, the ones
-c necessary for evaluating orders 0-16)
-c
-      DO 99 M=21,0,-1
-       W=2.E0*FLOAT(M)+1.E0
-       STRS(I,M) = (Y + U * F)/W
-       F=STRS(I,M)
-  99  CONTINUE
-c
-  799 CONTINUE
-c calculation of the function [(v+1/2)/2**v+1*sqrt(pi) ]
-c
-      FACS(0)=sqpi/2.E0
-      do n=1,16
-       FACS(n)=FACS(n-1)*(2*n-1)/2
-      enddo
-c
-      RETURN
-      END
-
 c-------------------
 c same version as in old version of GDFMOL
-      FUNCTION FMCH(M,X)
-      IMPLICIT REAL*8 (A-H,O-Z)
-      parameter (sqpi=1.77245385090551588D0)
-c
+      real*8 FUNCTION FMCH(M,X)
+      implicit none
+      real*8, intent(in) :: X
+      integer, intent(in) :: M
+
+      real*8, parameter :: sqpi=1.77245385090551588D0
+     
+      real*8 ::  Y, A, B, APPROX, FIMULT, FIPROP, PTLSUM
+      real*8 :: TERM, XD
+      integer :: i, iw, notrms
+
       Y=DEXP(-X)
       IF (X.GT.10.D0) GO TO 20
       A = DFLOAT(M)
@@ -212,49 +150,3 @@ c
   999 FORMAT ('23H0FMCH DOES NOT CONVERGE',I6,1PE16.8)
       END
 C     -----------------------
-      FUNCTION FMCHS(M,X)
-      IMPLICIT REAL*4 (A-H,O-Z)
-      parameter (sqpi=1.77245385090E0)
-c
-      Y=EXP(-X)
-      IF (X.GT.10.E0) GO TO 20
-      A = FLOAT(M)
-      A = A + 0.5E0
-      TERM = 1.0E0 / A
-      PTLSUM = TERM
-      DO 11 I=2,50
-      TERM = TERM * X / (A + FLOAT(I-1))
-      PTLSUM = PTLSUM + TERM
-      IF(TERM/PTLSUM.LT.1.0E-12)GO TO 12
-   11 CONTINUE
-      STOP
-   12 FMCHS = 0.5E0 * PTLSUM * Y
-      RETURN
-c
-   20 A = FLOAT(M)
-      B = A + 0.5E0
-      XD = 1.0E0 / X
-      APPROX = 0.886226925452758E0 * SQRT(XD) * XD**M
-      DO 22 I=1,M
-   22 APPROX = APPROX * (B - FLOAT(I))
-      FIMULT = 0.5E0 * Y * XD
-      PTLSUM = 0.0E0
-      IF(FIMULT.EQ.0.0E0)GO TO 25
-      FIPROP = FIMULT/APPROX
-      TERM = 1.0E0
-      PTLSUM = TERM
-      NOTRMS = X
-      NOTRMS = NOTRMS + M
-      DO 24 I=2,NOTRMS
-      A = B - FLOAT(I-1)
-      TERM = TERM * A * XD
-      PTLSUM = PTLSUM + TERM
-      IF(ABS(TERM*FIPROP/PTLSUM).LE.1.0E-10)GO TO 25
-   24 CONTINUE
-      WRITE (IW,999) M,X
-   25 FMCHS = APPROX - FIMULT * PTLSUM
-      RETURN
-  999 FORMAT (24H0FMCHS DOES NOT CONVERGE,I6,1PE16.8)
-      END
-C     -----------------------
-

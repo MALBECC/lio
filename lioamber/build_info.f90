b5a3d6d6a23f97a8543e8d7fd9b0f73646e16a84
Francisco <francisco@eneas-qimail.com>
2017-04-04 11:47:44 -0300

    Finished with new makefile general structure for lioamber.

--- lioamber/build_info.f90
+++ lioamber/build_info.f90
@@ -6,64 +6,65 @@ module build_info
 ! version of lio was used and how was it compiled, in order to improve and
 ! facilitate reproducibility of results.
 !
-! Information is passed into local constants ( parameters named BUILD_XXX )
-! through variables defined at compilation ( BPASS_XXX ).
+! Information is passed into local constants ( parameters named BDATA_XXX )
+! through variables defined during compilation ( BPASS_XXX ).
 !
 !%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%!
 #  ifdef BPASS_VERSION
-      character(len=10), parameter :: BUILD_VERSION   = BPASS_VERSION
+      character(len=40), parameter :: BDATA_VERSION   = BPASS_VERSION
 #  else
-      character(len=10), parameter :: BUILD_VERSION   = 'NO DATA'
-#     define NON_REFERABLE_BUILD
+      character(len=40), parameter :: BDATA_VERSION   = 'NO DATA'
+#     define INCOMPLETE_BDATA
 #  endif
 
-!------------------------------------------------------------------------------!
+
 #  ifdef BPASS_GITHASH
-      character(len=40), parameter :: BUILD_GITHASH   = BPASS_GITHASH
+      character(len=40), parameter :: BDATA_GITHASH   = BPASS_GITHASH
 #  else
-      character(len=40), parameter :: BUILD_GITHASH   = 'NO DATA'
-#     define NON_REFERABLE_BUILD
+      character(len=40), parameter :: BDATA_GITHASH   = 'NO DATA'
+#     define INCOMPLETE_BDATA
 #  endif
 
-!------------------------------------------------------------------------------!
+
 #  ifdef BPASS_CUDA
-      integer,           parameter :: BUILD_CUDA      = BPASS_CUDA
+      integer,           parameter :: BDATA_CUDA      = BPASS_CUDA
 #  else
-      integer,           parameter :: BUILD_CUDA      = -1
-#     define NON_REFERABLE_BUILD
+      integer,           parameter :: BDATA_CUDA      = -666
+#     define INCOMPLETE_BDATA
 #  endif
 
-!------------------------------------------------------------------------------!
+
 #  ifdef BPASS_INTEL
-      integer,           parameter :: BUILD_INTEL     = BPASS_INTEL
+      integer,           parameter :: BDATA_INTEL     = BPASS_INTEL
 #  else
-      integer,           parameter :: BUILD_INTEL     = -1
-#     define NON_REFERABLE_BUILD
+      integer,           parameter :: BDATA_INTEL     = -666
+#     define INCOMPLETE_BDATA
 #  endif
 
-!------------------------------------------------------------------------------!
-#  ifdef BPASS_PROFDEB
-      integer,           parameter :: BUILD_PROFDEB   = BPASS_PROFDEB
-#  else
-      integer,           parameter :: BUILD_PROFDEB   = -1
-#     define NON_REFERABLE_BUILD
-#  endif
 
-!------------------------------------------------------------------------------!
 #  ifdef BPASS_PARALLEL
-      integer,           parameter :: BUILD_PARALLEL  = BPASS_PARALLEL
+      integer,           parameter :: BDATA_PARALLEL  = BPASS_PARALLEL
 #  else
-      integer,           parameter :: BUILD_PARALLEL  = -1
-#     define NON_REFERABLE_BUILD
+      integer,           parameter :: BDATA_PARALLEL  = -666
+#     define INCOMPLETE_BDATA
 #  endif
 
-!------------------------------------------------------------------------------!
+
 #  ifdef BPASS_PRECISION
-      integer,           parameter :: BUILD_PRECISION = BPASS_PRECISION
+      integer,           parameter :: BDATA_PRECISION = BPASS_PRECISION
 #  else
-      integer,           parameter :: BUILD_PRECISION = -1
-#     define NON_REFERABLE_BUILD
+      integer,           parameter :: BDATA_PRECISION = -666
+#     define INCOMPLETE_BDATA
 #  endif
+
+
+#  ifdef BPASS_ANALYTICS
+      integer,           parameter :: BDATA_ANALYTICS = BPASS_ANALYTICS
+#  else
+      integer,           parameter :: BDATA_ANALYTICS = -666
+#     define INCOMPLETE_BDATA
+#  endif
+
 
 !%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%!
 contains
@@ -71,25 +72,43 @@ contains
    subroutine print_build_info( outunit )
       implicit none
       integer, intent(in) :: outunit
-      write( unit=outunit, fmt= '(A)') &
-      '============================================================'
-      write( unit=outunit, fmt= '(2A)')   '  LIO VERSION  : ', BUILD_VERSION
-      write( unit=outunit, fmt= '(2A)')   '  GIT HASHTAG  : ', BUILD_GITHASH
+      write( unit=outunit, fmt= '(A)') ''
+      write( unit=outunit, fmt= '(2A)')   '  LIO VERSION  : ', BDATA_VERSION
+      write( unit=outunit, fmt= '(A)') ''
+      write( unit=outunit, fmt= '(2A)')   '  GIT HASHTAG  : ', BDATA_GITHASH
       write( unit=outunit, fmt= '(A)')    ''
       write( unit=outunit, fmt= '(2A)')   '  COMPILATION OPTIONS '
-      write( unit=outunit, fmt= '(A,I2)') '  *  cuda      = ', BUILD_CUDA
-      write( unit=outunit, fmt= '(A,I2)') '  *  intel     = ', BUILD_INTEL
-      write( unit=outunit, fmt= '(A,I2)') '  *  profdeb   = ', BUILD_PROFDEB
-      write( unit=outunit, fmt= '(A,I2)') '  *  parallel  = ', BUILD_PARALLEL
-      write( unit=outunit, fmt= '(A,I2)') '  *  precision = ', BUILD_PRECISION
-#     ifdef NON_REFERABLE_BUILD
-         write( unit=outunit, fmt= '(A)') ''
+      write( unit=outunit, fmt= '(A,I2)') '  *  cuda      = ', BDATA_CUDA
+      write( unit=outunit, fmt= '(A,I2)') '  *  intel     = ', BDATA_INTEL
+      write( unit=outunit, fmt= '(A,I2)') '  *  parallel  = ', BDATA_PARALLEL
+      write( unit=outunit, fmt= '(A,I2)') '  *  precision = ', BDATA_PRECISION
+      write( unit=outunit, fmt= '(A,I2)') '  *  analytics = ', BDATA_ANALYTICS
+      write( unit=outunit, fmt= '(A)') ''
+#     ifdef INCONSISTENT_BUILD
          write( unit=outunit, fmt= '(A)') &
-         '  WARNING! NON REFERABLE COMPILATION'
+         '  WARNING! INCONSISTENT COMPILATION:'
+         write( unit=outunit, fmt= '(A)') &
+         '  Part of the code has been compiled using a certain set'
+         write( unit=outunit, fmt= '(A)') &
+         '  of options, while another part has been compiled with'
+         write( unit=outunit, fmt= '(A)') &
+         '  a different set. Please make clean between compilations'
+         write( unit=outunit, fmt= '(A)') &
+         '  with different options to have a more reliable run.'
+         write( unit=outunit, fmt= '(A)') ''
+#     endif
+#     ifdef INCOMPLETE_BDATA
+         write( unit=outunit, fmt= '(A)') &
+         '  WARNING! INCOMPLETE DATA:'
+         write( unit=outunit, fmt= '(A)') &
+         '  There has been a problem with the data passed during'
+         write( unit=outunit, fmt= '(A)') &
+         '  compilation. This is a bug and needs to be reported.'
          write( unit=outunit, fmt= '(A)') ''
 #     endif
       write( unit=outunit, fmt= '(A)')  &
       '============================================================'
+
    end subroutine
 
 end module

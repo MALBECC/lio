b9f3f0cadab0e003de683c605c251509774cb80a
charly <carlosmbqca@gmail.com>
2017-07-27 15:19:46 -0300

    no change

--- lioamber/cublasmath/cublasmath.f
+++ lioamber/cublasmath/cublasmath.f
@@ -40,7 +40,6 @@ c       implicit none
        include 'commutator_cublas.f'
        include 'magnus_cublas.f'
        include 'cu_fock_commuts.f'
-       include 'cumsp_r.f'
        end module
 !
 !

4f79364e613affbe3c3c0b4199811fe60b80079a
Francisco <ramirezfranciscof@users.noreply.github.com>
2017-08-10 10:42:57 -0300

    (1) Nuclear velocities now updated in every electronic step. (2) Electronic step number counter added.

--- lioamber/ehrendata.f90
+++ lioamber/ehrendata.f90
@@ -3,7 +3,8 @@ module ehrendata
 !------------------------------------------------------------------------------!
    implicit none
    real*8  :: StoredEnergy = 0.0d0
-   integer :: step_number  = 0
+   integer :: qmn_nstep = 0
+   integer :: qme_nstep = 0
    integer :: rsti_funit  = 654321
    integer :: rsto_funit  = 123456
 

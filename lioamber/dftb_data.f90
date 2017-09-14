51ceb561571c008e3946324aa804ff45452b52da
charly <carlosmbqca@gmail.com>
2017-09-14 13:27:34 -0300

    Implementation of DFTB inside SCF. Normal SCF calculations seem to be working. Need to test new DFTB functionality.

--- lioamber/dftb_data.f90
+++ lioamber/dftb_data.f90
@@ -1,18 +1,20 @@
 !%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 module dftb_data
    implicit none
+   
 
-   integer      :: MTB    ! Size of the two tight-binding subatrices
-   integer      :: MTBQM  ! Size of the DFT-TB matrix
+   logical      :: dftb_calc =.false. !logical indicator for dftb calculation
+   integer      :: MTB   = 0 ! Size of the two tight-binding subatrices
+   integer      :: MDFTB = 0 ! Size of the DFT-TB matrix
 
-   integer      :: nend              ! Index matrix size
+   integer      :: end_basis              ! Index matrix size
    integer, allocatable :: Iend(:,:) ! Index matrix
 
 
-   real*8       :: alfaTB  ! Diagonal tight binding param
+   real*8       :: alfaTB  ! Diagonal tight binding param (fermi energy)
    real*8       :: betaTB  ! Offdiagonal tight binding param
    real*8       :: gammaTB ! DFT-TB terms
-   
+   real*8       :: Vbias   ! Bias potential
       
 !#ifdef TD_SIMPLE
    real*8,allocatable   :: chimerafock (:, :) !allocate in the central code

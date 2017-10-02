216ffb362fbcccbf086f1165448be13f37f74b2c
fedepedron <fedekbe@gmail.com>
2017-10-02 17:45:20 -0300

    Fixed Openshell not using CPU.

--- g2g/init.cpp
+++ g2g/init.cpp
@@ -140,10 +140,6 @@ extern "C" void g2g_parameter_init_(const unsigned int& norm, const unsigned int
     cout << "Number of MO(UP): "<<fortran_vars.nco<<endl;
     cout << "Number of MO(DOWN): "<<fortran_vars.nco+fortran_vars.nunp<<endl;
 
-    // To be removed in full-open shell
-    G2G::cpu_threads = 0; // FF-Temp
-    cout << "Openshell currently not supported with CPU multithreading.\n"; // FF-Temp
-
     fortran_vars.rmm_dens_a = FortranMatrix<double>(rhoalpha, fortran_vars.m, fortran_vars.m, fortran_vars.m);
     fortran_vars.rmm_dens_b = FortranMatrix<double>(rhobeta,  fortran_vars.m, fortran_vars.m, fortran_vars.m);
     fortran_vars.rmm_input_ndens1 = FortranMatrix<double>(RMM, fortran_vars.m, fortran_vars.m, fortran_vars.m);

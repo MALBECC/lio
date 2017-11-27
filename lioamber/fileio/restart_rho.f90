ce59fee5abe129813842a8b0a44c653d772dbd69
fedepedron <fedekbe@gmail.com>
2017-11-27 17:22:15 -0300

    Wrote Rho restart subroutines.

--- lioamber/fileio/restart_rho.f90
+++ lioamber/fileio/restart_rho.f90
@@ -1,14 +1,255 @@
 !%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%!
-!%% WRITE_OUTPUT.F90 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%!
-! * write_restart    (prints an orbital coefficient restart file)              !
+!%% RESTART_RHO.F90 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%!
+! * read_rho_restart_ccd  (reads Rho restart for closed shell - cmplx double)  !
+! * read_rho_restart_ccs  (reads Rho restart for closed shell - cmplx single)  !
+! * read_rho_restart_cd   (reads Rho restart for closed shell - double)        !
+! * read_rho_restart_cs   (reads Rho restart for closed shell - single)        !
+! * read_rho_restart_ocd  (reads Rho restart for open shell - cmplx double)    !
+! * read_rho_restart_ocs  (reads Rho restart for open shell - cmplx single)    !
+! * read_rho_restart_od   (reads Rho restart for open shell - double)          !
+! * read_rho_restart_os   (reads Rho restart for open shell - single)          !
+! * write_rho_restart_ccd (writes Rho restart for closed shell - cmplx double) !
+! * write_rho_restart_ccs (writes Rho restart for closed shell - cmplx single) !
+! * write_rho_restart_cd  (writes Rho restart for closed shell - double)       !
+! * write_rho_restart_cs  (writes Rho restart for closed shell - single)       !
+! * write_rho_restart_ocd (writes Rho restart for open shell - cmplx double)   !
+! * write_rho_restart_ocs (writes Rho restart for open shell - cmplx single)   !
+! * write_rho_restart_od  (writes Rho restart for open shell - double)         !
+! * write_rho_restart_os  (writes Rho restart for open shell - single)         !
 !%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%!
 
-subroutine write_rho_restart_c(rho, rho_size, UID)
+
+!% READ_RHO_XX %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%!
+! Reads the Rho matrix from restart, rearranging it if needed, for open and    !
+! closed shell cases in both single and double precision, real and complex.    !
+!%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%!
+subroutine read_rho_ccd(rho, M, UID)
+   ! rho : Rho matrix.
+   ! M   : Rho matrix size.
+   ! UID : Input file unit ID.
+   implicit none
+   integer   , intent(in)  :: M, UID
+   complex*16, intent(out) :: rho(M,M)
+
+   rewind(UID)
+   call read_sqmatrix(rho, M, UID)
+   return
+end subroutine read_rho_ccd
+
+subroutine read_rho_ccs(rho, M, UID)
+   ! rho : Rho matrix.
+   ! M   : Rho matrix size.
+   ! UID : Input file unit ID.
+   implicit none
+   integer  , intent(in)  :: M, UID
+   complex*8, intent(out) :: rho(M,M)
+
+   rewind(UID)
+   call read_sqmatrix(rho, M, UID)
+   return
+end subroutine read_rho_ccs
+
+subroutine read_rho_cd(rho, M, UID)
+   ! rho : Rho matrix.
+   ! M   : Rho matrix size.
+   ! UID : Input file unit ID.
+   implicit none
+   integer, intent(in)  :: M, UID
+   real*8 , intent(out) :: rho(M,M)
+ 
+   rewind(UID)
+   call read_sqmatrix(rho, M, UID)
+   return
+end subroutine read_rho_cd
+
+subroutine read_rho_cs(rho, M, UID)
+   ! rho : Rho matrix.
+   ! M   : Rho matrix size.
+   ! UID : Input file unit ID.
+   implicit none
+   integer, intent(in)  :: M, UID
+   real*4 , intent(out) :: rho(M,M)
+
+   rewind(UID)
+   call read_sqmatrix(rho, M, UID)
+   return
+end subroutine read_rho_cs
+
+subroutine read_rho_ocd(rho_a, rho_b, M, UID)
+   ! rho_a : Rho matrix Alpha.
+   ! rho_b : Rho matrix Beta.
+   ! M     : Rho matrix size.
+   ! UID   : Input file unit ID.
+   implicit none
+   integer   , intent(in)  :: M, UID
+   complex*16, intent(out) :: rho_a(M,M), rho_b(M,M)
+
+   rewind(UID)
+   call read_sqmatrix(rho_a, M, UID)
+   call read_sqmatrix(rho_b, M, UID)
+   return
+end subroutine read_rho_ocd
+
+subroutine read_rho_ocs(rho_a, rho_b, M, UID)
+   ! rho_a : Rho matrix Alpha.
+   ! rho_b : Rho matrix Beta.
+   ! M     : Rho matrix size.
+   ! UID   : Input file unit ID.
+   implicit none
+   integer  , intent(in)  :: M, UID
+   complex*8, intent(out) :: rho_a(M,M), rho_b(M,M)
+
+   rewind(UID)
+   call read_sqmatrix(rho_a, M, UID)
+   call read_sqmatrix(rho_b, M, UID)
+   return
+end subroutine read_rho_ocs
+
+subroutine read_rho_od(rho_a, rho_b, M, UID)
+   ! rho_a : Rho matrix Alpha.
+   ! rho_b : Rho matrix Beta.
+   ! M     : Rho matrix size.
+   ! UID   : Input file unit ID.
+   implicit none
+   integer, intent(in)  :: M, UID
+   real*8 , intent(out) :: rho_a(M,M), rho_b(M,M)
+
+   rewind(UID)
+   call read_sqmatrix(rho_a, M, UID)
+   call read_sqmatrix(rho_b, M, UID)
+   return
+end subroutine read_rho_od
+
+subroutine read_rho_os(rho_a, rho_b, M, UID)
+   ! rho_a : Rho matrix Alpha.
+   ! rho_b : Rho matrix Beta.
+   ! M     : Rho matrix size.
+   ! UID   : Input file unit ID.
+   implicit none
+   integer, intent(in)  :: M, UID
+   real*4 , intent(out) :: rho_a(M,M), rho_b(M,M)
+
+   rewind(UID)
+   call read_sqmatrix(rho_a, M, UID)
+   call read_sqmatrix(rho_b, M, UID)
+   return
+end subroutine read_rho_os
+!%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%!
 
 
-end subroutine 
+!% WRITE_RHO_XX %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%!
+! Rearranges and prints Rho matrix for open and closed shell cases in both     !
+! single and double precision, real and complex                                !
+!%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%!
+subroutine write_rho_ccd(rho, M, UID)
+   ! rho : Rho matrix.
+   ! M   : Rho matrix size.
+   ! UID : Output file unit ID.
+   implicit none
+   integer   , intent(in) :: M, UID
+   complex*16, intent(in) :: rho(M,M)
 
-subroutine write_rho_restart_o(rho_a, rho_b, rho_size, UID)
+   rewind(UID)
+   call write_sqmatrix(rho, M, UID)
+   return
+end subroutine write_rho_ccd
 
+subroutine write_rho_ccs(rho, M, UID)
+   ! rho : Rho matrix.
+   ! M   : Rho matrix size.
+   ! UID : Output file unit ID.
+   implicit none
+   integer  , intent(in) :: M, UID 
+   complex*8, intent(in) :: rho(M,M)
 
-end subroutine
+   rewind(UID)
+   call write_sqmatrix(rho, M, UID)
+   return
+end subroutine write_rho_ccs
+
+subroutine write_rho_cd(rho, M, UID)
+   ! rho : Rho matrix.
+   ! M   : Rho matrix size.
+   ! UID : Output file unit ID.
+   implicit none
+   integer, intent(in) :: M, UID
+   real*8 , intent(in) :: rho(M,M)
+
+   rewind(UID)
+   call write_sqmatrix(rho, M, UID)
+   return
+end subroutine write_rho_cd
+
+subroutine write_rho_cs(rho, M, UID)
+   ! rho : Rho matrix.
+   ! M   : Rho matrix size.
+   ! UID : Output file unit ID.
+   implicit none
+   integer, intent(in) :: M, UID
+   real*4 , intent(in) :: rho(M,M)
+
+   rewind(UID)
+   call write_sqmatrix(rho, M, UID)
+   return
+end subroutine write_rho_cs
+
+subroutine write_rho_ocd(rho_a, rho_b, M, UID)
+   ! rho_a : Rho matrix Alpha.
+   ! rho_b : Rho matrix Beta.
+   ! M     : Rho matrix size.
+   ! UID   : Output file unit ID.
+   implicit none
+   integer   , intent(in) :: M, UID
+   complex*16, intent(in) :: rho_a(M,M), rho_b(M,M)
+
+   rewind(UID)
+   call write_sqmatrix(rho_a, M, UID)
+   call write_sqmatrix(rho_b, M, UID)
+   return
+end subroutine write_rho_ocd
+
+subroutine write_rho_ocs(rho_a, rho_b, M, UID)
+   ! rho_a : Rho matrix Alpha.
+   ! rho_b : Rho matrix Beta.
+   ! M     : Rho matrix size.
+   ! UID   : Output file unit ID.
+   implicit none
+   integer  , intent(in) :: M, UID
+   complex*8, intent(in) :: rho_a(M,M), rho_b(M,M)
+
+   rewind(UID)
+   call write_sqmatrix(rho_a, M, UID)
+   call write_sqmatrix(rho_b, M, UID)
+   return
+end subroutine write_rho_ocs
+
+subroutine write_rho_od(rho_a, rho_b, M, UID)
+   ! rho_a : Rho matrix Alpha.
+   ! rho_b : Rho matrix Beta.
+   ! M     : Rho matrix size.
+   ! UID   : Output file unit ID.
+   implicit none
+   integer, intent(in) :: M, UID
+   real*8 , intent(in) :: rho_a(M,M), rho_b(M,M)
+
+   rewind(UID)
+   call write_sqmatrix(rho_a, M, UID)
+   call write_sqmatrix(rho_b, M, UID)
+   return
+end subroutine write_rho_od
+
+subroutine write_rho_os(rho_a, rho_b, M, UID)
+   ! rho_a : Rho matrix Alpha.
+   ! rho_b : Rho matrix Beta.
+   ! M     : Rho matrix size.
+   ! UID   : Output file unit ID.
+   implicit none
+   integer, intent(in) :: M, UID
+   real*4 , intent(in) :: rho_a(M,M), rho_b(M,M)
+
+   rewind(UID)
+   call write_sqmatrix(rho_a, M, UID)
+   call write_sqmatrix(rho_b, M, UID)
+   return
+end subroutine write_rho_os
+!%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%!

69dec2d96550c0d98daf1cf28920d685fe77a55e
fedepedron <fedekbe@gmail.com>
2017-11-27 17:16:20 -0300

    Edited the module's main file.

--- lioamber/fileio/fileio.f90
+++ lioamber/fileio/fileio.f90
@@ -1,8 +1,10 @@
 module fileio
    implicit none
+   include 'restart_commons_h.f90'
    include 'restart_rho_h.f90'
    include 'restart_fock_h.f90'
 contains
+   include 'restart_commons.f90'
    include 'restart_rho.f90'
    include 'restart_fock.f90'
 end module fileio

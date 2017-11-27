2b92a40e486255d12e4f01ee644bdf6394c856b3
fedepedron <fedekbe@gmail.com>
2017-11-27 16:36:34 -0300

    Created Fock restart subroutines.

--- lioamber/fileio/restart_fock.f90
+++ lioamber/fileio/restart_fock.f90
@@ -1,79 +1,135 @@
 !%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%!
 !%% RESTART_FOCK.F90 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%!
-! * read_fock_restart_c   (reads Fock restart for closed shell)                !
-! * read_fock_restart_o   (reads Fock restart for open shell)                  !
-! * write_fock_restart_c  (writes Fock restart for closed shell)               !
-! * write_fock_restart_o  (writes Fock restart for open shell)                 !
+! * read_fock_restart_cd   (reads Fock restart for closed shell - double)      !
+! * read_fock_restart_cs   (reads Fock restart for closed shell - single)      !
+! * read_fock_restart_od   (reads Fock restart for open shell - double)        !
+! * read_fock_restart_os   (reads Fock restart for open shell - single)        !
+! * write_fock_restart_cd  (writes Fock restart for closed shell - double)     !
+! * write_fock_restart_cs  (writes Fock restart for closed shell - single)     !
+! * write_fock_restart_od  (writes Fock restart for open shell - double)       !
+! * write_fock_restart_os  (writes Fock restart for open shell - single)       !
 !%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%!
 
-subroutine write_fock_restart_c(fock, M, n_occ, ndx, UID)
-   ! fock  : Coefficient matrix.
-   ! M     : Total number of basis functions.
-   ! n_occ : Total number of occupied orbitals.
-   ! ndx   : Basis functions index.
-   ! UID   : Output file unit.
+
+!% READ_FOCK_XX %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%!
+! Reads the Fock matrix from restart, rearranging it if needed, for open and   !
+! closed shell cases in both single and double precision.                      !
+!%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%!
+subroutine read_fock_cd(fock, M, UID)
+   ! fock : Fock matrix.
+   ! M    : Fock matrix size.
+   ! UID  : Input file unit ID.
    implicit none
-   integer, intent(in) :: M, n_occ, UID, ndx(M)
-   real*8 , intent(in) :: fock(M,4*M)
-
-   integer :: icount, jcount
-
-   do icount = 1, M
-   do jcount = 1, M
-      fock(ndx(icount), M+jcount) = fock(icount, 2*M+jcount)      
-   enddo
-   enddo
-
-   rewind UID
-   do icount = 1, M
-   do jcount = 1, n_occ
-      write(UID, 400) fock(icount, M+jcount)
-   enddo
-   enddo   
-
+   integer, intent(in)  :: M, UID
+   real*8 , intent(out) :: fock(M,M)
+ 
+   rewind(UID)
+   call read_sqmatrix(fock, M, UID)
    return
+end subroutine read_fock_cd
 
-400 format(4(E14.7E2, 2x))
-end subroutine write_fock_restart_c
-
-
-subroutine write_fock_restart_o(fock_a, fock_b, M, n_occ_a, n_occ_b, ndx, UID)
-   ! fock_a  : Alpha coefficient matrix.
-   ! fock_b  : Beta coefficient matrix.
-   ! M       : Total number of basis functions.
-   ! n_occ_a : Total number of occupied alpha orbitals.
-   ! n_occ_a : Total number of occupied beta orbitals.
-   ! ndx     : Basis functions index.
-   ! UID     : Output file unit.
+subroutine read_fock_cs(fock, M, UID)
+   ! fock : Fock matrix.
+   ! M    : Fock matrix size.
+   ! UID  : Input file unit ID.
    implicit none
-   integer, intent(in) :: M, n_occ_a, n_occ_b, UID, ndx(M)
-   real*8 , intent(in) :: fock_a(M,4*M), fock_b(M, 4*M)
+   integer, intent(in)  :: M, UID
+   real*4 , intent(out) :: fock(M,M)
 
-   integer :: icount, jcount
-
-   do icount=1, M
-   do jcount=1, M
-      fock_a(ndx(icount), M+jcount) = fock_a(icount, 2*M+jcount)
-      fock_b(ndx(icount), M+jcount) = fock_b(icount, 2*M+jcount)
-   enddo
-   enddo
-
-   rewind UID
-   do icount = 1, M
-   do jcount = 1, n_occ_a
-      write(UID, 400) fock_a(icount, M+jcount)
-   enddo
-   enddo
-
-   do icount = 1, M
-   do jcount = 1, n_occ_b
-      write(UID, 400) fock_b(icount, M+jcount)
-   enddo
-   enddo
-
+   rewind(UID)
+   call read_sqmatrix(fock, M, UID)
    return
+end subroutine read_fock_cs
 
-400 format(4(E14.7E2, 2x))
-end subroutine write_fock_restart_o
+subroutine read_fock_od(fock_a, fock_b, M, UID)
+   ! fock_a : Fock matrix Alpha.
+   ! fock_b : Fock matrix Beta.
+   ! M      : Fock matrix size.
+   ! UID    : Input file unit ID.
+   implicit none
+   integer, intent(in)  :: M, UID
+   real*8 , intent(out) :: fock_a(M,M), fock_b(M,M)
+
+   rewind(UID)
+   call read_sqmatrix(fock_a, M, UID)
+   call read_sqmatrix(fock_b, M, UID)
+   return
+end subroutine read_fock_od
+
+subroutine read_fock_os(fock_a, fock_b, M, UID)
+   ! fock_a : Fock matrix Alpha.
+   ! fock_b : Fock matrix Beta.
+   ! M      : Fock matrix size.
+   ! UID    : Input file unit ID.
+   implicit none
+   integer, intent(in)  :: M, UID
+   real*4 , intent(out) :: fock_a(M,M), fock_b(M,M)
+
+   rewind(UID)
+   call read_sqmatrix(fock_a, M, UID)
+   call read_sqmatrix(fock_b, M, UID)
+   return
+end subroutine read_fock_os
+!%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%!
 
 
+!% WRITE_FOCK_XX %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%!
+! Rearranges and prints Fock matrix for open and closed shell cases in both    !
+! single and double precision.                                                 !
+!%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%!
+subroutine write_fock_cd(fock, M, UID)
+   ! fock : Fock matrix.
+   ! M    : Fock matrix size.
+   ! UID  : Output file unit ID.
+   implicit none
+   integer, intent(in) :: M, UID
+   real*8 , intent(in) :: fock(M,M)
+
+   rewind(UID)
+   call write_sqmatrix(fock, M, UID)
+   return
+end subroutine write_fock_cd
+
+subroutine write_fock_cs(fock, M, UID)
+   ! fock : Fock matrix.
+   ! M    : Fock matrix size.
+   ! UID  : Output file unit ID.
+   implicit none
+   integer, intent(in) :: M, UID
+   real*4 , intent(in) :: fock(M,M)
+
+   rewind(UID)
+   call write_sqmatrix(fock, M, UID)
+   return
+end subroutine write_fock_cs
+
+subroutine write_fock_od(fock_a, fock_b, M, UID)
+   ! fock_a : Fock matrix Alpha.
+   ! fock_b : Fock matrix Beta.
+   ! M      : Fock matrix size.
+   ! UID    : Output file unit ID.
+   implicit none
+   integer, intent(in) :: M, UID
+   real*8 , intent(in) :: fock_a(M,M), fock_b(M,M)
+
+   rewind(UID)
+   call write_sqmatrix(fock_a, M, UID)
+   call write_sqmatrix(fock_b, M, UID)
+   return
+end subroutine write_fock_od
+
+subroutine write_fock_os(fock_a, fock_b, M, UID)
+   ! fock_a : Fock matrix Alpha.
+   ! fock_b : Fock matrix Beta.
+   ! M      : Fock matrix size.
+   ! UID    : Output file unit ID.
+   implicit none
+   integer, intent(in) :: M, UID
+   real*4 , intent(in) :: fock_a(M,M), fock_b(M,M)
+
+   rewind(UID)
+   call write_sqmatrix(fock_a, M, UID)
+   call write_sqmatrix(fock_b, M, UID)
+   return
+end subroutine write_fock_os
+!%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%!

ad91109c14b2af3f974cfbe1cc7009d65bfbc660
fedepedron <fedekbe@gmail.com>
2018-01-29 12:00:22 -0300

    Moved part of the subs to a generic shape module.

--- lioamber/field_subs.f90
+++ lioamber/field_subs.f90
@@ -1,18 +1,105 @@
-
+! This module contains routines for Field setups and calculations.
+! Field shapes are as follows:
+! 0 - Constant, 1 - Gaussian, 2 - Half Gaussian Up, 3 - Half Gaussian Down
 module field_subs
+   use shape_data, only: shape_iso, shape_aniso
    implicit none
+   integer :: nfields_iso = 0, nfields_aniso = 0
+
+   type, extends(shape_iso) :: field_iso
+      real*8 :: amplitude = 0.0D0
+   end type field_iso
+
+   type, extends(shape_aniso) :: field_aniso
+      real*8 :: amplitude(3) = 0.0D0
+   end type field_aniso
+
+   type(field_aniso), allocatable :: fields_aniso(:)
+   type(field_iso)  , allocatable :: fields_iso(:)
+
+   interface field_setup_iso
+      module procedure field_setup_iso_full
+      module procedure field_setup_iso_easy
+   end interface field_setup_iso
 
 contains
-   ! Specifies field shape and parameters.
-   ! Called from the outside.
-   subroutine field_setup_sym(nfields)
-   use field_data, only: nfields
-   implicit none
 
+   ! Specifies field shape and parameters. the "easy" setup asumes default
+   ! relations for some of the shape parameters. Both subroutines are mutually
+   ! exclusive. In easy, the decay is applied arbtirarily.
+   subroutine field_setup_iso_full(nfields_i, fld_shape, periodic, time_decay, &
+                                   time_start, time_end, center, coord, amplit)
+      implicit none
+      integer, intent(in) :: nfields_i, fld_shape(nfields_i)
+      logical, intent(in) :: periodic(nfields_i)
+      real*8 , intent(in) :: time_decay(nfields_i), time_start(nfields_i), &
+                             time_end(nfields_i), center(nfields_i),       &
+                             coord(nfields_i,3), amplit
+      integer :: icount
 
-   end subroutine field_setup_sym
+      nfields_iso = nfields_i
+      if (allocated(fields_iso)) deallocate(fields_iso)
+      allocate (fields_iso(nfields_iso))
+
+      do icount = 1, nfields_iso
+         fields_iso(icount)%shape_type = fld_shape(icount)
+         fields_iso(icount)%periodic   = periodic(icount)
+         fields_iso(icount)%time_decay = time_decay(icount)
+         fields_iso(icount)%time_start = time_start(icount)
+         fields_iso(icount)%time_end   = time_end(icount)
+         fields_iso(icount)%center     = center(icount)
+         fields_iso(icount)%coord(1:3) = coord(icount,1:3)
+         fields_iso(icount)%amplitude  = amplit
+      enddo
+      return
+   end subroutine field_setup_iso_full
+
+   subroutine field_setup_iso_easy(nfields_i, fld_shape, time_end, coord)
+      implicit none
+      integer, intent(in) :: nfields_i, fld_shape(nfields_i)
+      real*8 , intent(in) :: time_end(nfields_i), coord(nfields_i,3)
+      integer :: icount
+
+      nfields_iso = nfields_i
+      if (allocated(fields_iso)) deallocate(fields_iso)
+      allocate (fields_iso(nfields_iso))
+
+      do icount = 1, nfields_iso
+         fields_iso(icount)%shape_type = fld_shape(icount)
+         fields_iso(icount)%periodic   = .false.
+         fields_iso(icount)%time_start = 0.0D0
+         fields_iso(icount)%time_end   = time_end(icount)
+         fields_iso(icount)%coord(1:3) = coord(icount,1:3)
+
+         select case (fld_shape(icount))
+         case (0)
+            fields_iso(icount)%time_decay = 0.0D0
+            fields_iso(icount)%center     = 0.0D0
+         case (1)
+            fields_iso(icount)%time_decay = time_end(icount) /50.0D0
+            fields_iso(icount)%center     = time_end(icount) / 2.0D0
+         case (2)
+            fields_iso(icount)%time_decay = time_end(icount) /25.0D0
+            fields_iso(icount)%center     = time_end(icount)
+         case (3)
+            fields_iso(icount)%time_decay = time_end(icount) /25.0D0
+            fields_iso(icount)%center     = 0.0D0
+         case default
+            write(*,*) "ERROR - Field: Wrong shape type for field ", icount, "."
+            stop
+         end select
+      enddo
+      return
+   end subroutine field_setup_iso_easy
 
    subroutine field_calc()
 
    end subroutine field_calc
+
+   subroutine field_finalize()
+      implicit none
+      if (allocated(fields_iso)) deallocate(fields_iso)
+      if (allocated(fields_aniso)) deallocate(fields_aniso)
+      return
+   end subroutine field_finalize
 end module field_subs
--- lioamber/shape_data.f90
+++ lioamber/shape_data.f90
@@ -1,70 +1,61 @@
-! This module contains shared variables for field calculations using intfld.
-! Shape indicates the field shape in time: 0 is constant, 1 is gaussian,  2 is
-! half gaussian up, 3 is half gaussian down.
-module field_data
+! This module contains shared variables for shape calculations.
+! shape_type indicates the shape in time: 0 is constant, 1 is gaussian.
+module shape_data
    private
-   public :: field, field_asym
-   public :: nfields, nfields_asym, all_fields, all_asym_fields
+   public :: shape_iso, shape_aniso
 
-   type field
-      integer :: fld_shape  = 0
+   type shape_iso
+      integer :: shape_type = 0
       logical :: periodic   = .false.
       real*8  :: time_decay = 1.0D0
       real*8  :: time_start = 0.0D0
       real*8  :: time_end   = 0.0D0
       real*8  :: center     = 1.0D0
-      real*8  :: amplitude  = 1.0D0
       real*8  :: coord(3)   = 0.0D0
       contains
          procedure, nopass :: calc_g
-         procedure, pass   :: calculate => calc_sym
-   end type field
+         procedure, pass   :: calculate => calc_iso
+   end type shape_iso
 
-   type field_asym
-      integer :: fld_shape(3)  = 0
+   type shape_aniso
+      integer :: shape_type(3) = 0
       logical :: periodic(3)   = .false.
       real*8  :: time_decay(3) = 1.0D0
       real*8  :: time_start(3) = 0.0D0
       real*8  :: time_end(3)   = 0.0D0
       real*8  :: center(3)     = 0.0D0
-      real*8  :: amplitude(3)  = 1.0D0
       real*8  :: coord(3)      = 0.0D0
       contains
          procedure, nopass :: calc_g
-         procedure, pass   :: calculate => calc_asym
-   end type field_asym
-
-   integer :: nfields = 0
-   integer :: nfields_asym = 0
-   type(field)     , allocatable :: all_fields(:)
-   type(field_asym), allocatable :: all_asym_fields(:)
+         procedure, pass   :: calculate => calc_aniso
+   end type shape_aniso
 contains
 
-   ! Calculates the field in one coordinate.
-   function calc_g(in_time, f_coord, decay, center) result(field_mag)
+   ! Calculates the shape in one coordinate.
+   function calc_g(in_time, f_coord, decay, center) result(shape_mag)
       implicit none
       real*8 , intent(in) :: in_time, f_coord, decay, center
-      real*8  :: field_mag, exponent, period
+      real*8  :: shape_mag, exponent, period
 
-      field_mag = 0.0D0
+      shape_mag = 0.0D0
       exponent  = 0.0D0
 
       exponent  = - ( (in_time - center) / decay )**2
-      field_mag = f_coord * exp(exponent)
+      shape_mag = f_coord * exp(exponent)
 
       return
    end function calc_g
 
-   subroutine calc_sym(this, time, field_mag)
+   subroutine calc_iso(this, time, shape_mag)
       implicit none
-      class(field), intent(in)  :: this
-      real*8     , intent(in)  :: time
-      real*8     , intent(out) :: field_mag(3)
+      class(shape_iso), intent(in)  :: this
+      real*8          , intent(in)  :: time
+      real*8          , intent(out) :: shape_mag(3)
       real*8  :: in_time, period
       integer :: icount, calc_coord_g
 
       if ( (time.gt.this%time_end).and.(.not.(this%periodic)) ) then
-         field_mag = 0.0D0
+         shape_mag = 0.0D0
          return
       elseif (this%periodic) then
          period  = this%time_end - this%time_start
@@ -73,33 +64,33 @@ contains
          in_time = time
       endif
 
-      select case (this%fld_shape)
+      select case (this%shape_type)
          case (0)
-            field_mag = this%coord
+            shape_mag = this%coord
          case (1, 2, 3)
             do icount = 1, 3
-               field_mag(icount) = this%calc_g(in_time, this%coord(icount), &
+               shape_mag(icount) = this%calc_g(in_time, this%coord(icount), &
                                                 this%time_decay, this%center)
             enddo
          case default
-            write(*,*) "ERROR - Field. Wrong field shape (calc_coord)."
+            write(*,*) "ERROR - shape. Wrong shape shape (calc_coord)."
       end select
 
       return
-   end subroutine calc_sym
+   end subroutine calc_iso
 
-   subroutine calc_asym(this, time, field_mag)
+   subroutine calc_aniso(this, time, shape_mag)
       implicit none
-      class(field_asym), intent(in)  :: this
-      real*8          , intent(in)  :: time
-      real*8          , intent(out) :: field_mag(3)
+      class(shape_aniso), intent(in)  :: this
+      real*8            , intent(in)  :: time
+      real*8            , intent(out) :: shape_mag(3)
       real*8  :: in_time(3), period
       integer :: icount, calc_coord_g
 
       do icount = 1, 3
          if((time.gt.this%time_end(icount)).and.(.not.(this%periodic(icount))))&
          then
-            field_mag(icount) = 0.0D0
+            shape_mag(icount) = 0.0D0
          elseif (this%periodic(icount)) then
             period          = this%time_end(icount) - this%time_start(icount)
             in_time(icount) = time  - floor(time/period) * period
@@ -107,22 +98,22 @@ contains
             in_time(icount) = time
          endif
 
-         select case (this%fld_shape(icount))
+         select case (this%shape_type(icount))
          case (0)
             if (time.le.this%time_end(icount)) then
-               field_mag(icount) = this%coord(icount)
+               shape_mag(icount) = this%coord(icount)
             endif
          case (1, 2, 3)
             if (time.le.this%time_end(icount)) then
-               field_mag(icount) = this%calc_g(in_time(icount), &
+               shape_mag(icount) = this%calc_g(in_time(icount), &
                                    this%coord(icount), this%time_decay(icount),&
                                    this%center(icount))
             endif
          case default
-            write(*,*) "ERROR - Field. Wrong field shape (calc_coord)."
+            write(*,*) "ERROR - shape. Wrong shape shape (calc_coord)."
          end select
       enddo
 
       return
-   end subroutine calc_asym
-end module field_data
+   end subroutine calc_aniso
+end module shape_data

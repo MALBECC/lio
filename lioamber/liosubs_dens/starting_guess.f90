014f579e9eae2dbeecd286165c66109bf2931dee
Francisco <ramirezfranciscof@users.noreply.github.com>
2018-02-05 11:04:55 -0300

    Simple clean up. Improvement in how to handle the treatment of overlap matrix still pending (inputs used, passing only the sop object to subroutines, including cublas inside sop, etc).

--- lioamber/liosubs_dens/starting_guess.f90
+++ lioamber/liosubs_dens/starting_guess.f90
@@ -3,114 +3,73 @@
 !     FCe = SCe; (X^T)SX = 1
 !     F' = (X^T)FX
 !     => (X^-1*C)^-1 * F' * (X^-1*C) = e
+!
+! This subroutine takes the Hmat as a vector, transforms it into a matrix, 
+! diagonalizes it, and builds the density from the resulting orbitals.
+!
+! TODO:
+!
+! (*) The starting guess should take care of calculating everything, it
+!     shouldn't need to eat Hmat. I do understand the convenience since
+!     it is the same for the whole SCF loop. Perhaps make it optional??
+!
+! (*) Diagonalizing fock and building the density matrix are things that
+!     are or should be performed by dedicated subroutines, and these
+!     should be called both here and in the SCF cycle.
+!
+!
 !%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%!
-subroutine starting_guess( Nmat, Nvec, hmat_vec, Xmat, densat_vec )
-   use garcha_mod, ONLY:  M, X, Md, NCO, MO_coef_at
-   use liosubs_math, only: transform
+subroutine starting_guess( Nmat, Nvec, NCO, hmat_vec, Xmat, densat_vec )
+   use liosubs_math  , only: transform
+
    implicit none
    integer, intent(in)    :: Nmat
    integer, intent(in)    :: Nvec
+   integer, intent(in)    :: NCO
    real*8 , intent(in)    :: Xmat(Nmat,Nmat)
    real*8 , intent(inout) :: hmat_vec(Nvec)
    real*8 , intent(inout) :: densat_vec(Nvec)
    
-   real*8, allocatable   :: morb_coefat(:,:)
+   real*8, allocatable   :: morb_energy(:)
    real*8, allocatable   :: morb_coefon(:,:)
-   real*8, allocatable   :: rmm5(:), rmm15(:)
-   real*8, allocatable   :: hmat(:,:), fockon(:,:), fockon_vec(:)
-   real*8, allocatable   :: eigval_vec(:)
-   integer :: ii,jj,i,j,k,kk
-   integer :: M1, M2, info
-   real*8  :: ff
+   real*8, allocatable   :: morb_coefat(:,:)
+   real*8, allocatable   :: morb_coefoc(:,:)
+   real*8, allocatable   :: dens_mao(:,:)
+   real*8, allocatable   :: hmat(:,:)
+   real*8, allocatable   :: WORK(:)
+   integer               :: LWORK
+   integer               :: info
 
    call g2g_timer_start('initial guess')
    call g2g_timer_sum_start('initial guess')
 
-
-   allocate( rmm5(Nvec), rmm15(Nvec), fockon_vec(Nvec) )
-   allocate( eigval_vec(Nvec) )
-   allocate( morb_coefat(Nmat, Nmat), morb_coefon(Nmat, Nmat) )
-
-!  Calculate F' in RMM(M5)
+   allocate( morb_energy(Nvec) )
+   allocate( morb_coefon(Nmat, Nmat) )
+   allocate( morb_coefat(Nmat, Nmat) )
+   allocate( morb_coefoc(Nmat, NCO) )
    allocate( hmat(Nmat,Nmat) )
-   allocate( fockon(Nmat, Nmat) )
+   allocate( dens_mao(Nmat, Nmat) )
+
    call spunpack('L', Nmat, hmat_vec, hmat )
-   fockon = transform( hmat, Xmat )
-   call sprepack('L', Nmat, fockon_vec, fockon )
- 
+   morb_coefon(:,:) = transform( hmat, Xmat )
+   morb_energy(:)   = 0.0d0
 
-! F' diagonalization now
-! xnano will contain (X^-1)*C
-! FFR: NO IT DOES NOT; it contains C...
-   do i=1,M
-      rmm15(i)=0.D0
-      eigval_vec(i) = 0.0d0
-   enddo
+   LWORK = -1
+   if ( allocated(WORK) ) deallocate(WORK)
+   allocate( WORK( 1 ) )
+   call dsyev( 'V', 'L', Nmat, morb_coefon, Nmat, morb_energy, WORK, LWORK, info )
 
-   do i=1,Nvec
-      rmm5(i)=fockon_vec(i)
-   enddo
+   LWORK = NINT( WORK(1) )
+   if ( allocated(WORK) ) deallocate(WORK)
+   allocate( WORK( LWORK ) )
+   call dsyev( 'V', 'L', Nmat, morb_coefon, Nmat, morb_energy, WORK, LWORK, info )
 
-   morb_coefon(:,:) = 0.0d0
-
-!  Inputs and outputs are the same
-   call dspev( 'V', 'L', M, RMM5, eigval_vec, morb_coefon, M, RMM15, info )
-
-
-! Recover C from (X^-1)*C
-   do i=1,M
-   do j=1,M
-      morb_coefat(i,j) = 0.0d0
-      do k=1,M
-         morb_coefat(i,j) = morb_coefat(i,j) + X(i,k) * morb_coefon(k,j)
-      enddo
-   enddo
-   enddo
-
-! Density Matrix
-   kk=0
-   do k=1,NCO
-   do i=1,M
-      kk=kk+1
-      MO_coef_at(kk)=morb_coefat(i,k)
-   enddo
-   enddo
-
+   morb_coefat = matmul( Xmat, morb_coefon )
+   morb_coefoc(1:Nmat,1:NCO) = morb_coefat(1:Nmat,1:NCO)
+   dens_mao = (2.0d0) * matmul( morb_coefoc, transpose(morb_coefoc) )
+   call messup_densmat( dens_mao )
+   call sprepack( 'L', Nmat, densat_vec, dens_mao)
    
-   kk=0
-   do j=1,M
-   do i=j,M
-   
-      kk=kk+1
-      densat_vec(kk)=0.D0
-      
-      if (i.eq.j) then
-!        one factor of 2 for alpha+beta
-         ff=2.D0
-      else
-!        another factor of 2 for direct triangular sum (j>i) w/real basis
-         ff=4.D0
-      endif
-!
-      do k=1,NCO
-         densat_vec(kk)=densat_vec(kk)+ff*morb_coefat(i,k)*morb_coefat(j,k)
-      enddo
-      
-   enddo
-   enddo
- 
-   M1 = Nmat
-   M2 = 2*Nmat
-   do jj = 1, M
-   do ii = 1, M
-      X(ii,M1+jj)       = morb_coefon(ii,jj)
-      X(ii,M2+jj)       = morb_coefat(ii,jj)
-   enddo
-   enddo
-
-
-   deallocate(rmm5,rmm15)
-
    call g2g_timer_stop('initial guess')
    call g2g_timer_sum_stop('initial guess')
 end subroutine starting_guess

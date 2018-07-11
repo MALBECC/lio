35655a32594d5417533472777ca2449c7eca2f38
eduarditoperez <eduardoperezleale@gmail.com>
2018-07-11 18:17:50 -0300

    - Fixed some compilation issues for Libxc after the rebase.

--- g2g/fix_compile.h
+++ g2g/fix_compile.h
@@ -1,6 +1,33 @@
+#ifndef __FAKECOMPILE_H__
+#define __FAKECOMPILE_H__
+
 #if !GPU_KERNELS
 #define __host__
 #define __device__
 #else
 #include <cuda_runtime.h>
+#endif
+
+#ifdef __CUDACC__
+    // Nothing
+#else
+    // Fake definitions for the kernels so they'll
+    // compile under gcc or cc
+    struct fake_blockDim {
+	int x;
+    };
+
+    struct fake_blockIdx {
+	int x;
+    };
+
+    struct fake_threadIdx {
+	int x;
+    };
+
+    extern fake_blockDim blockDim;
+    extern fake_blockIdx blockIdx;
+    extern fake_threadIdx threadIdx;
+#endif
+
 #endif
--- g2g/libxc/libxcproxy.h
+++ g2g/libxc/libxcproxy.h
@@ -8,7 +8,8 @@
 #include <vector>
 #include "../scalar_vector_types.h"
 #include "print_utils.h"
-#include <cuda_runtime.h>
+#include "../fix_compile.h"
+//#include <cuda_runtime.h>
 #include "../timer.h"
 
 extern "C" void g2g_timer_sum_start_(const char* timer_name, unsigned int length_arg);
@@ -247,25 +248,25 @@ void LibxcProxy <T, width>::doGGA(T dens,
 {
     //printf("LibxcProxy::doGGA cpu simple(...) \n");
 
-    T rho[1] = {dens};
+    const double rho[1] = {dens};
     // Libxc needs the 'contracted gradient'
-    T sigma[1] = {(grad.x * grad.x) + (grad.y * grad.y) + (grad.z * grad.z)};
-    T exchange[1];
-    T correlation[1];
+    double sigma[1] = {(grad.x * grad.x) + (grad.y * grad.y) + (grad.z * grad.z)};
+    double exchange[1];
+    double correlation[1];
 
     // The outputs for exchange
-    T vrho [1];
-    T vsigma [1];
-    T v2rho [1];
-    T v2rhosigma[1];
-    T v2sigma [1];
+    double vrho [1];
+    double vsigma [1];
+    double v2rho [1];
+    double v2rhosigma[1];
+    double v2sigma [1];
 
     // The outputs for correlation
-    T vrhoC [1];
-    T vsigmaC [1];
-    T v2rhoC [1];
-    T v2rhosigmaC [1];
-    T v2sigmaC [1];
+    double vrhoC [1];
+    double vsigmaC [1];
+    double v2rhoC [1];
+    double v2rhosigmaC [1];
+    double v2sigmaC [1];
 
     // The exchange values
     xc_gga (&funcForExchange, 1,
@@ -291,12 +292,9 @@ void LibxcProxy <T, width>::doGGA(T dens,
                 v2sigmaC,
                 NULL, NULL, NULL, NULL);
 
-    // TODO: tener el cuenta el tema del FULL_DOUBLE
-    // a la vuelta del calculo en libxc. Si FULL_DOUBLE=1 -> son doubles
-    // sino son floats y hay que castear antes de hacer las cuentas
-    // sino da todo cero.
     ex = exchange[0];
     ec = correlation[0];
+
     // Merge the results for the derivatives.
     vrho[0] += vrhoC[0];
     vsigma[0] += vsigmaC[0];
@@ -389,10 +387,6 @@ void LibxcProxy <T, width>::doGGA(T* dens,
                 v2sigmaC,
                 NULL, NULL, NULL, NULL);
 
-    // TODO: tener el cuenta el tema del FULL_DOUBLE
-    // a la vuelta del calculo en libxc. Si FULL_DOUBLE=1 -> son doubles
-    // sino son floats y hay que castear antes de hacer las cuentas
-    // sino da todo cero.
     for (int i=0; i<number_of_points; i++) {
 	ex[i] = exchange[i];
         ec[i] = correlation[i];
@@ -529,33 +523,11 @@ __global__ void joinResults(
 		    const G2G::vec_type<double, width>* hess1,
 		    const G2G::vec_type<double, width>* hess2,
 		    int numElements)
-/*
-template <class T, int width>
-__global__ void joinResults(
-		    T* ex, T* exchange,
-		    T* ec, T* correlation,
-		    T* vrho, T* vrhoC,
-		    T* vsigma, T* vsigmaC,
-		    T* v2rho, T* v2rhoC,
-		    T* v2rhosigma, T* v2rhosigmaC,
-		    T* v2sigma, T* v2sigmaC,
-		    T* y2a,
-		    const T* sigma,
-		    const G2G::vec_type<T, width>* grad,
-		    const G2G::vec_type<T, width>* hess1,
-		    const G2G::vec_type<T, width>* hess2,
-		    int numElements)*/
 {
     int i = blockDim.x * blockIdx.x + threadIdx.x;
 
     if (i < numElements)
     {
-/*
-	printf("%i %lf %lf %lf %lf %lf %lf %lf %lf %lf\n",i,
-	    hess1[i].x, hess1[i].y, hess1[i].z,
-	    hess2[i].x, hess2[i].y, hess2[i].z,
-	    grad[i].x, grad[i].y, grad[i].z);
-*/
 	ex[i] = exchange[i];
 	ec[i] = correlation[i];
 
@@ -633,7 +605,6 @@ __global__ void convertFloatToDouble(const G2G::vec_type<double,4>* input, G2G::
     return;
 }
 
-
 __global__ void convertDoubleToFloat(const G2G::vec_type<double,4>* input, G2G::vec_type<float,4>* output, int numElements)
 {
     int i = blockDim.x * blockIdx.x + threadIdx.x;
@@ -647,19 +618,19 @@ __global__ void convertDoubleToFloat(const G2G::vec_type<double,4>* input, G2G::
     }
 }
 
-//
+// end Conversion KERNELS
 ////////////////////////////////
 
 #endif
 
 //////////////////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////
-// LibxcProxy::doGGA
+// LibxcProxy::doGGA - GPU version
 // Calls the xc_gga function from libxc
 // dens: pointer for the density array
 // number_of_points: the size of all the input arrays
 // contracted_grad: the contracted grad for libxc
-// grad:
+// grad: 
 // hess1:
 // hess2:
 // ex: here goes the results after calling xc_gga from libxc for the exchange functional
@@ -1050,399 +1021,6 @@ void LibxcProxy <T, width>::doGGA(T* dens,
 #endif
     return;
 }
-
-
-/*
-template <class T, int width>
-void LibxcProxy <T, width>::doGGA(T* dens,
-    const int number_of_points,
-    const T* contracted_grad,
-    const G2G::vec_type<T, width>* grad,
-    const G2G::vec_type<T, width>* hess1,
-    const G2G::vec_type<T, width>* hess2,
-    T* ex,
-    T* ec,
-    T* y2a)
-{
-    //printf("LibxcProxy::doGGA cuda template version (...) \n");
-    print_proxy_input<T> (dens, number_of_points, contracted_grad, grad, hess1, hess2);
-
-    // The size of scalar type.
-    bool full_double = (sizeof(T) == 8);
-    if (full_double) {
-        printf("Is full double: TRUE\n");
-    } else {
-	printf("Is full double: FALSE\n");
-    }
-
-    // Variables for the Kernels
-    int threadsPerBlock = 256;
-    int blocksPerGrid = (number_of_points + threadsPerBlock - 1) / threadsPerBlock;
-
-    cudaError_t err = cudaSuccess;
-
-    // All the arrays for libxc must be of T*
-    int array_size = sizeof(T) * number_of_points;
-    //int vec_size = sizeof(G2G::vec_type<T,width>) * number_of_points;
-
-    T* rho = NULL;
-    T* sigma = NULL;
-
-    T* ex_double = NULL;
-    T* ec_double = NULL;
-    T* y2a_double = NULL;
-    const G2G::vec_type<T, width>* grad_double = NULL;
-    const G2G::vec_type<T, width>* hess1_double = NULL;
-    const G2G::vec_type<T, width>* hess2_double = NULL;
-
-    // Alloc memory for rho
-    err = cudaMalloc((void**)&rho, array_size);
-    if (err != cudaSuccess)
-    {
-        fprintf(stderr, "Failed to allocate device rho!\n");
-        exit(EXIT_FAILURE);
-    }
-
-    // Alloc memory for sigma
-    err = cudaMalloc((void**)&sigma, array_size);
-    if (err != cudaSuccess)
-    {
-        fprintf(stderr, "Failed to allocate device sigma!\n");
-	exit(EXIT_FAILURE);
-    }
-
-    // Si el tipo de datos es float, creamos los arrays para copiar
-    // los inputs y convertirlos a floats.
-
-    if (!full_double) {
-	err = cudaMalloc((void**)&ex_double, array_size);
-        if (err != cudaSuccess)
-        {
-	    fprintf(stderr, "Failed to allocate device ex_double!\n");
-    	    exit(EXIT_FAILURE);
-        }
-	cudaMemset(ex_double,0,array_size);
-
-        err = cudaMalloc((void**)&ec_double, array_size);
-	if (err != cudaSuccess)
-        {
-	    fprintf(stderr, "Failed to allocate device ec_double!\n");
-	    exit(EXIT_FAILURE);
-        }
-	cudaMemset(ec_double,0,array_size);
-
-        err = cudaMalloc((void**)&y2a_double, array_size);
-	if (err != cudaSuccess)
-        {
-    	    fprintf(stderr, "Failed to allocate device y2a_double!\n");
-	    exit(EXIT_FAILURE);
-        }
-	cudaMemset(y2a_double,0,array_size);
-
-        err = cudaMalloc((void**)&grad_double, vec_size);
-	if (err != cudaSuccess)
-	{
-	    fprintf(stderr, "Failed to allocate device grad_double!\n");
-    	    exit(EXIT_FAILURE);
-        }
-
-	err = cudaMalloc((void**)&hess1_double, vec_size);
-        if (err != cudaSuccess)
-	{
-	    fprintf(stderr, "Failed to allocate device hess1_double!\n");
-	    exit(EXIT_FAILURE);
-        }
-
-	err = cudaMalloc((void**)&hess2_double, vec_size);
-        if (err != cudaSuccess)
-	{
-	    fprintf(stderr, "Failed to allocate device hess2_double!\n");
-	    exit(EXIT_FAILURE);
-        }
-    }
-
-    // Preparamos los datos.
-    //if (full_double) {
-	err = cudaMemcpy(rho, dens, array_size, cudaMemcpyDeviceToDevice);
-        if (err != cudaSuccess)
-	{
-	    fprintf(stderr, "Failed to copy data from dens->rho\n");
-        }
-
-	err = cudaMemcpy(sigma, contracted_grad, array_size, cudaMemcpyDeviceToDevice);
-        if (err != cudaSuccess)
-	{
-	    fprintf(stderr, "Failed to copy data from contracted_grad->sigma\n");
-	}
-
-        // Usamos los datos como vienen ya que son todos doubles.
-	ex_double = ex;
-	ec_double = ec;
-        y2a_double = y2a;
-	grad_double = grad;
-        hess1_double = hess1;
-        hess2_double = hess2;
-
-//    } else {
-	// Como los inputs son float, los convertimos para libxc
-	//convertFloatToDouble<<<blocksPerGrid, threadsPerBlock>>>(dens, rho, number_of_points);
-	//convertFloatToDouble<<<blocksPerGrid, threadsPerBlock>>>(contracted_grad, sigma, number_of_points);
-        //convertFloatToDouble<<<blocksPerGrid, threadsPerBlock>>>(grad, grad_double, number_of_points);
-	//convertFloatToDouble<<<blocksPerGrid, threadsPerBlock>>>(hess1, hess1_double, number_of_points);
-        //convertFloatToDouble<<<blocksPerGrid, threadsPerBlock>>>(hess2, hess2_double, number_of_points);
-//    }
-
-    // Preparamos los arrays de salida.
-    T* exchange = NULL;
-    err = cudaMalloc((void **)&exchange, array_size);
-    if (err != cudaSuccess)
-    {
-        fprintf(stderr, "Failed to allocate device exchange!\n");
-        exit(EXIT_FAILURE);
-    }
-
-    T* correlation = NULL;
-    err = cudaMalloc((void **)&correlation, array_size);
-    if (err != cudaSuccess)
-    {
-        fprintf(stderr, "Failed to allocate device correlation!\n");
-        exit(EXIT_FAILURE);
-    }
-
-    cudaMemset(exchange,0,array_size);
-    cudaMemset(correlation,0,array_size);
-
-    // The outputs for exchange
-    T* vrho = NULL;
-    T* vsigma = NULL;
-    T* v2rho = NULL;
-    T* v2rhosigma = NULL;
-    T* v2sigma = NULL;
-
-    err = cudaMalloc((void **)&vrho, array_size);
-    if (err != cudaSuccess)
-    {
-        fprintf(stderr, "Failed to allocate device vrho!\n");
-        exit(EXIT_FAILURE);
-    }
-
-    err = cudaMalloc((void **)&vsigma, array_size);
-    if (err != cudaSuccess)
-    {
-        fprintf(stderr, "Failed to allocate device vsigma!\n");
-        exit(EXIT_FAILURE);
-    }
-
-    err = cudaMalloc((void **)&v2rho, array_size);
-    if (err != cudaSuccess)
-    {
-        fprintf(stderr, "Failed to allocate device v2rho!\n");
-        exit(EXIT_FAILURE);
-    }
-
-    err = cudaMalloc((void **)&v2rhosigma, array_size);
-    if (err != cudaSuccess)
-    {
-        fprintf(stderr, "Failed to allocate device v2rhosigma!\n");
-        exit(EXIT_FAILURE);
-    }
-
-    err = cudaMalloc((void **)&v2sigma, array_size);
-    if (err != cudaSuccess)
-    {
-        fprintf(stderr, "Failed to allocate device v2sigma!\n");
-        exit(EXIT_FAILURE);
-    }
-
-    // Clear arrays
-    cudaMemset(vrho, 0, array_size);
-    cudaMemset(vsigma, 0, array_size);
-    cudaMemset(v2rho, 0, array_size);
-    cudaMemset(v2rhosigma, 0, array_size);
-    cudaMemset(v2sigma, 0, array_size);
-
-    // The outputs for correlation
-    T* vrhoC = NULL;
-    T* vsigmaC = NULL;
-    T* v2rhoC = NULL;
-    T* v2rhosigmaC = NULL;
-    T* v2sigmaC = NULL;
-
-    err = cudaMalloc((void **)&vrhoC, array_size);
-    if (err != cudaSuccess)
-    {
-        fprintf(stderr, "Failed to allocate device vrhoC!\n");
-        exit(EXIT_FAILURE);
-    }
-
-    err = cudaMalloc((void **)&vsigmaC, array_size);
-    if (err != cudaSuccess)
-    {
-        fprintf(stderr, "Failed to allocate device vsigmaC!\n");
-        exit(EXIT_FAILURE);
-    }
-
-    err = cudaMalloc((void **)&v2rhoC, array_size);
-    if (err != cudaSuccess)
-    {
-        fprintf(stderr, "Failed to allocate device v2rhoC!\n");
-        exit(EXIT_FAILURE);
-    }
-
-    err = cudaMalloc((void **)&v2rhosigmaC, array_size);
-    if (err != cudaSuccess)
-    {
-        fprintf(stderr, "Failed to allocate device v2rhosigmaC!\n");
-        exit(EXIT_FAILURE);
-    }
-
-    err = cudaMalloc((void **)&v2sigmaC, array_size);
-    if (err != cudaSuccess)
-    {
-        fprintf(stderr, "Failed to allocate device v2sigmaC!\n");
-        exit(EXIT_FAILURE);
-    }
-    ///////////////////////////////////
-    // Clear arrays for correlation
-    cudaMemset(vrhoC, 0, array_size);
-    cudaMemset(vsigmaC, 0, array_size);
-    cudaMemset(v2rhoC, 0, array_size);
-    cudaMemset(v2rhosigmaC, 0, array_size);
-    cudaMemset(v2sigmaC, 0, array_size);
-
-    /////////////////////////////
-    // Call LIBXC for exchange
-    try {
-        xc_gga (&funcForExchange, number_of_points,
-                rho,
-                sigma,
-                exchange,
-                vrho,
-                vsigma,
-                v2rho,
-                v2rhosigma,
-                v2sigma,
-                NULL, NULL, NULL, NULL);
-    } catch (int exception) {
-        fprintf (stderr, "Exception ocurred calling xc_gga for Exchange '%d' \n", exception);
-        return;
-    }
-
-    ////////////////////////////////
-    // Call LIBXC for correlation
-    try {
-        // Now the correlation value.
-        xc_gga (&funcForCorrelation, number_of_points,
-                rho,
-                sigma,
-                correlation,
-                vrhoC,
-                vsigmaC,
-                v2rhoC,
-                v2rhosigmaC,
-                v2sigmaC,
-                NULL, NULL, NULL, NULL);
-    } catch (int exception) {
-        fprintf (stderr, "Exception ocurred calling xc_gga for Correlation '%d' \n", exception);
-        return;
-    }
-
-    ////////////////////////
-    // Gather the results
-    joinResults<T, width><<<blocksPerGrid, threadsPerBlock>>>(
-	ex_double, exchange,
-	ec_double, correlation,
-	vrho, vrhoC,
-	vsigma, vsigmaC,
-	v2rho, v2rhoC,
-	v2rhosigma, v2rhosigmaC,
-	v2sigma, v2sigmaC,
-	y2a_double,
-	sigma,
-	grad_double,
-	hess1_double,
-	hess2_double,
-	number_of_points);
-
-    //////////////////////////
-    // Convert if necessary
-//    if (!full_double) {
-	//convertDoubleToFloat<<<blocksPerGrid, threadsPerBlock>>> (ex_double, ex, number_of_points);
-	//convertDoubleToFloat<<<blocksPerGrid, threadsPerBlock>>> (ec_double, ec, number_of_points);
-        //convertDoubleToFloat<<<blocksPerGrid, threadsPerBlock>>> (y2a_double, y2a, number_of_points);
-//    }
-
-    /////////////////////////
-    // Free device memory
-    if (rho != NULL) {
-	cudaFree(rho);
-    }
-    if (sigma != NULL) {
-	cudaFree(sigma);
-    }
-    if (exchange != NULL) {
-	cudaFree(exchange);
-    }
-    if (correlation != NULL) {
-	cudaFree(correlation);
-    }
-    if (vrho != NULL) {
-        cudaFree(vrho);
-    }
-    if (vsigma != NULL) {
-	cudaFree(vsigma);
-    }
-    if (v2rho != NULL) {
-	cudaFree(v2rho);
-    }
-    if (v2rhosigma != NULL) {
-	cudaFree(v2rhosigma);
-    }
-    if (v2sigma != NULL) {
-	cudaFree(v2sigma);
-    }
-    if (vrhoC != NULL) {
-        cudaFree(vrhoC);
-    }
-    if (vsigmaC != NULL) {
-	cudaFree(vsigmaC);
-    }
-    if (v2rhoC != NULL) {
-	cudaFree(v2rhoC);
-    }
-    if (v2rhosigmaC != NULL) {
-	cudaFree(v2rhosigmaC);
-    }
-    if (v2sigmaC != NULL) {
-	cudaFree(v2sigmaC);
-    }
-
-
-    if (!full_double) {
-        if (ex_double != NULL) {
-            cudaFree(ex_double);
-        }
-        if (ec_double != NULL) {
-            cudaFree(ec_double);
-        }
-        if (y2a_double != NULL) {
-        cudaFree(y2a_double);
-        }
-        if (grad_double != NULL) {
-            cudaFree((void*)grad_double);
-    	}
-    	if (hess1_double != NULL) {
-        cudaFree((void*)hess1_double);
-    	}
-        if (hess2_double != NULL) {
-    	    cudaFree((void*)hess2_double);
-        }
-    }
-
-    return;
-}
-*/
-
 
 //////////////////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////

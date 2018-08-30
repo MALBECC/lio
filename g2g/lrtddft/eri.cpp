961d7156542ede4b1f9d7d562f2c1b32f427bc2c
gonzalodm <gonzalodm20@gmail.com>
2018-08-30 19:53:42 -0300

    Cleanup commits squashed.
    Delete float dependences
    
    Add Makefiles to compile LR subroutines
    
    Fixed Bugs in lrtddft
    
    bugfixed in basis funtions - libint
    
    modifications in lrtddft/
    
    Add fake subroutine
    
    add new function to libxcproxy
    
    lrtddft call libxcproxy
    
    bug fixed in compilation
    
    Now compile with cuda /= 0
    
    Test for linear response
    
    Bug fixed in compilation
    
    Delete one matrix 4DIM
    
    I've included FCA into LR
    
    Delete some undesirable variables
    
    Corrected UnDensExc subroutine
    
    Bugfix when you compiled without LR options
    
    Reorder loops in coupling
    
    I changed the loops for a matrix multiplication

--- g2g/lrtddft/eri.cpp
+++ g2g/lrtddft/eri.cpp
@@ -11,95 +11,84 @@ using namespace std;
 
 typedef unsigned int uint;
 
-#if FULL_DOUBLE
 std::vector<Atom> libint_geom(double*,int);
-std::vector<libint2::Shell> make_basis(const std::vector<Atom>&,double*,
-                            double*,uint*,uint*,int,int,int,int);
-#else
-std::vector<Atom> libint_geom(float*,int);
-std::vector<libint2::Shell> make_basis(const std::vector<Atom>&,float*,
-                            float*,uint*,uint*,int,int,int,int);
-#endif
+std::vector<libint2::Shell> make_basis(const std::vector<Atom>&,double*,double*,uint*,uint*,
+                            int,int,int,int);
 
 size_t max_nprim(const std::vector<libint2::Shell>&);
+
 int max_l(const std::vector<libint2::Shell>&);
+
 std::vector<int> map_shell(const std::vector<libint2::Shell>&);
 
-//####################################################################
-//####################################################################
-#if FULL_DOUBLE
-void eri(double*Kc_int,int M,int NCO,int dim,uint natoms,
-         uint*ncont,double*cbas,double*a,double*r,uint*nuc,
-         int sfunc,int pfunc, int dfunc)   // Para C
-#else
-void eri(float*Kc_int,int M,int NCO,int dim,uint natoms,
-         uint*ncont,float*cbas,float*a,float*r,uint*nuc,
-         int sfunc,int pfunc, int dfunc)   // Para C
-#endif
+void eri(double*Kc_int,int M,uint natoms,uint*ncont,
+         double*cbas,double*a,double*r,uint*nuc,
+         int sfunc,int pfunc, int dfunc)
 {
-   int M3, M2;
-   M2 = M*M;
-   M3 = M2*M;
-   libint2::initialize();
-   Shell::do_enforce_unit_normalization(false);
-   std::vector<Atom> atoms = libint_geom(r,natoms); // coordinates needed for libint
-   auto obs = make_basis(atoms,a,cbas,ncont,nuc,sfunc,pfunc,dfunc,M);
+  int M3, M2;
+  M3 = M*M*M; M2 = M*M;
+  libint2::initialize();
+  Shell::do_enforce_unit_normalization(false);
+  std::vector<Atom> atoms = libint_geom(r,natoms);
+  auto obs = make_basis(atoms,a,cbas,ncont,nuc,sfunc,pfunc,dfunc,M);
+
+// Check basis normalization with libint
+//TODO: ver como hacer para que libint no normalize
 /*  cout << "BASIS SET LIBINT" << endl; // print SET BASIS
   std::copy(begin(obs), end(obs),
          std::ostream_iterator<Shell>(std::cout, "\n"));
 */
-   Engine eri_engine(Operator::coulomb, max_nprim(obs), max_l(obs), 0);
-   const auto& buf = eri_engine.results();
-   std::vector<int> shell2bf = map_shell(obs);
 
-   for(int s1=0; s1<obs.size(); ++s1) {
-     int bf1_first = shell2bf[s1]; // first basis function in this shell
-     int n1 = obs[s1].size();   // number of basis function in this shell
-     for(int s2=0; s2<=s1; ++s2) {
-       int bf2_first = shell2bf[s2];
-       int n2 = obs[s2].size();
-       for(int s3=0; s3<obs.size(); ++s3) {
-         int bf3_first = shell2bf[s3];
-         int n3 = obs[s3].size();
-         for(int s4=0; s4<=s3; ++s4) {
-           int bf4_first = shell2bf[s4];
-           int n4 = obs[s4].size();
-           eri_engine.compute(obs[s1], obs[s2], obs[s3], obs[s4]);
-           const auto* buf_1234 = buf[0];
-           if(buf_1234 == nullptr) continue;
-           for(int f1=0,f1234=0;f1<n1; ++f1) {
-             const int bf1 = f1 + bf1_first;
-             for(int f2=0; f2<n2; ++f2) {
-               const int bf2 = f2 + bf2_first;
-               for(int f3=0; f3<n3; ++f3) {
-                 const int bf3 = f3 + bf3_first;
-                 for(int f4=0;f4<n4;++f4,++f1234) {
-                   const int bf4 = f4 + bf4_first;
-                   double value = buf_1234[f1234];
-                   Kc_int[bf1*M3+bf2*M2+bf3*M+bf4]=value;
-                   Kc_int[bf2*M3+bf1*M2+bf3*M+bf4]=value;
-                   Kc_int[bf1*M3+bf2*M2+bf4*M+bf3]=value;
-                   Kc_int[bf2*M3+bf1*M2+bf4*M+bf3]=value;
-                 }
-               }
-             }
-           }
-         }
-       }
-     }
-   }
-   libint2::finalize();
+  Engine eri_engine(Operator::coulomb, max_nprim(obs), max_l(obs), 0);
+  const auto& buf = eri_engine.results();
+  std::vector<int> shell2bf = map_shell(obs);
+
+  for(int s1=0; s1<obs.size(); ++s1) {
+    int bf1_first = shell2bf[s1]; // first basis function in this shell
+    int n1 = obs[s1].size();   // number of basis function in this shell
+
+    for(int s2=0; s2<=s1; ++s2) {
+      int bf2_first = shell2bf[s2];
+      int n2 = obs[s2].size();
+
+      for(int s3=0; s3<obs.size(); ++s3) {
+        int bf3_first = shell2bf[s3];
+        int n3 = obs[s3].size();
+
+        for(int s4=0; s4<=s3; ++s4) {
+          int bf4_first = shell2bf[s4];
+          int n4 = obs[s4].size();
+
+          eri_engine.compute(obs[s1], obs[s2], obs[s3], obs[s4]);
+          const auto* buf_1234 = buf[0];
+          if (buf_1234 == nullptr) continue;
+          for(int f1=0, f1234=0; f1<n1; ++f1){
+            const int bf1 = f1 + bf1_first;
+            for(int f2=0; f2<n2; ++f2){
+              const int bf2 = f2 + bf2_first;
+              for(int f3=0; f3<n3; ++f3){
+                const int bf3 = f3 + bf3_first;
+                for(int f4=0; f4<n4; ++f4, ++f1234){
+                  const int bf4 = f4 + bf4_first;
+                  double value = buf_1234[f1234];
+                  
+                  Kc_int[bf1*M3+bf2*M2+bf3*M+bf4]=value;
+                  Kc_int[bf2*M3+bf1*M2+bf3*M+bf4]=value;
+                  Kc_int[bf1*M3+bf2*M2+bf4*M+bf3]=value;
+                  Kc_int[bf2*M3+bf1*M2+bf4*M+bf3]=value;
+                }
+              }
+            }
+          }
+
+        }
+      }
+    }
+  }
+  libint2::finalize();
 }
-//####################################################################
-//####################################################################
 
-//####################################################################
-//####################################################################
-#if FULL_DOUBLE
 std::vector<Atom> libint_geom(double* r,int natoms)
-#else
-std::vector<Atom> libint_geom(float* r,int natoms)
-#endif
 {
    std::vector<Atom> atoms(natoms);
 
@@ -110,26 +99,15 @@ std::vector<Atom> libint_geom(float* r,int natoms)
    }
    return atoms;
 }
-//####################################################################
-//####################################################################
 
-//####################################################################
-//####################################################################
-#if FULL_DOUBLE
 std::vector<libint2::Shell> make_basis(const std::vector<Atom>& atoms,
-          double*a,double*c,uint*ncont,uint*nuc,int s_func,int p_func, 
-          int d_func,int M)
-#else
-std::vector<libint2::Shell> make_basis(const std::vector<Atom>& atoms,
-          float*a,float*c,uint*ncont,uint*nuc,int s_func, int p_func,
-          int d_func,int M)
-#endif
+     double*a,double*c,uint*ncont,uint*nuc,int s_func, int p_func, int d_func,int M)
 {
    std::vector<Shell> obs;
    int from = 0;
    int to = s_func;
 
-   for(int i=from; i<to; i++) {//for s functions
+   for(int i=from; i<to; i++) {  // for s functions
      int centro = nuc[i]-1;
      int tam = ncont[i];
      std::vector<double> exp(tam);
@@ -151,8 +129,8 @@ std::vector<libint2::Shell> make_basis(const std::vector<Atom>& atoms,
 
    from = s_func;
    to = s_func+p_func*3;
-   for(int i=from; i<to; i=i+3) {//for p functions
-      int centro = nuc[i]-1;
+   for(int i=from; i<to; i=i+3) { // for p functions
+      int centro = nuc[i]-1;   
       int tam = ncont[i];
       std::vector<double> exp(tam);
       std::vector<double> coef(tam);
@@ -173,7 +151,7 @@ std::vector<libint2::Shell> make_basis(const std::vector<Atom>& atoms,
 
    from = s_func+p_func*3;
    to = M;
-   for(int i=from; i<to; i=i+6) {//for d functions
+   for(int i=from; i<to; i=i+6) { // for d functions
       int centro = nuc[i]-1;
       int tam = ncont[i];
       std::vector<double> exp(tam);
@@ -195,24 +173,15 @@ std::vector<libint2::Shell> make_basis(const std::vector<Atom>& atoms,
 
    return obs;
 }
-//####################################################################
-//####################################################################
 
-//####################################################################
-//####################################################################
-size_t max_nprim(const std::vector<libint2::Shell>& obs)
-{
+size_t max_nprim(const std::vector<libint2::Shell>& obs) {
   size_t n = 0;
   for (auto shell: obs)
     n = std::max(shell.nprim(), n);
   return n;
 }
 
-//####################################################################
-//####################################################################
-
-int max_l(const std::vector<libint2::Shell>& obs)
-{
+int max_l(const std::vector<libint2::Shell>& obs) {
   int l = 0;
   for (auto shell: obs)
     for (auto c: shell.contr)
@@ -220,12 +189,7 @@ int max_l(const std::vector<libint2::Shell>& obs)
   return l;
 }
 
-//####################################################################
-//####################################################################
-
-std::vector<int> map_shell(const std:: 
-     vector<libint2::Shell>& obs)
-{
+std::vector<int> map_shell(const std::vector<libint2::Shell>& obs) {
   std::vector<int> result;
   result.reserve(obs.size());
 
@@ -236,6 +200,3 @@ std::vector<int> map_shell(const std::
   }
   return result;
 }
-//####################################################################
-//####################################################################
-

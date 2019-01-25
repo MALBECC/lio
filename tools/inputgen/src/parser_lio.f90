6c7bcb25397927cad5717742d523fc759c488bec
Francisco <francisco@logos-qimail.com>
2019-01-25 14:23:35 -0300

    Patch: multiplied non-diagonal elements x2 in restart file of density.
    I fixed this in the input maker for the time being, but I really think
    we shouldn't really keep using the "density-but-not-really" format
    inside the code.

--- tools/inputgen/src/parser_lio.f90
+++ tools/inputgen/src/parser_lio.f90
@@ -27,11 +27,16 @@ subroutine writelio_rst( funit, mdim, densmat )
    integer, intent(in) :: funit
    integer, intent(in) :: mdim
    real*8,  intent(in) :: densmat(mdim, mdim)
+   real*8, allocatable :: temp(:)
    integer             :: ii
 
+   allocate( temp(mdim) )
+
    do ii = 1, mdim
+      temp(:) = densmat(ii,:) * 2.0d0
+      temp(ii) = densmat(ii,ii)
 !      write( unit=funit, fmt='(4(E14.7E2, 2x))' ) densmat(ii,:)
-      write( unit=funit, fmt=* ) densmat(ii,:)
+      write( unit=funit, fmt=* ) temp
    end do
 
 end subroutine writelio_rst

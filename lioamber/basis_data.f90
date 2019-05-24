b813c1170cc6b9e5319d8bb12a5be0dab36f8baa
fedepedron <fedekbe@gmail.com>
2019-05-24 11:26:31 -0300

    Fixed small report when basis are not found.

--- lioamber/basis_data.f90
+++ lioamber/basis_data.f90
@@ -526,14 +526,14 @@ subroutine check_basis(atom_bas_done, atom_fit_done, n_atoms, atom_Z, iostatus)
    iostatus = 0
    do iatom = 1, n_atoms
       if ( .not. atom_bas_done(atom_Z(iatom)) ) then
-         call atom_name(iatom, iatom_name)
+         call atom_name(atom_Z(iatom), iatom_name)
          write(*,'(A)') "  Error: Basis set not found for ", &
                         trim(iatom_name), "."
          iostatus = 1
          return
       endif
       if ( .not. atom_fit_done(atom_Z(iatom)) ) then
-         call atom_name(iatom, iatom_name)
+         call atom_name(atom_Z(iatom), iatom_name)
          write(*,'(A)') "  Error: Fitting set not found for ", &
                         trim(iatom_name), "."
          iostatus = 1

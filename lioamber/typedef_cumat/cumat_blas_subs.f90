a2fa5a4a0c41332c1eae03f9e594625a2f198824
Federico N. Pedron <fedekbe@gmail.com>
2019-06-19 11:52:35 -0300

    (#295) Datatypes: CuMat data structure and complex number precision.
    * Created header for complex types. Further types can be added.
    
    * Started a custom type for CUBLAS matrices. CuMats can be either real or complex, and encapsulate all of the CUBLAS operations in LIO.

---
+++ lioamber/typedef_cumat/cumat_blas_subs.f90
@@ -0,0 +1,94 @@
+! This file contains masks for BLAS and CUBLAS subroutines to be performed
+! with cumats. If possible, all operations are performed in GPU. The calling
+! object is the one to store the result.
+!
+! * add_mat performs A = a*B + A; B being an external CUMAT (see BLAS AXPY)
+! * mat_mul performs A = a*B*C + b*A; B,C being external CUMATs (see BLAS GEMM)
+
+! REAL
+subroutine add_mat_r(this, bmat, alfa, stat)
+   implicit none
+   real(kind=8)  , intent(in)              :: alfa
+   type(cumat_r) , intent(in)              :: bmat
+   class(cumat_r), intent(inout)           :: this
+   integer       , intent(inout), optional :: stat
+
+   integer :: my_stat = 0
+
+#ifdef CUBLAS
+   my_stat = cublas_daxpy(this%mat_size * this%mat_size, alfa, &
+                          bmat%cu_pointer, 1, this%cu_pointer, 1)
+#else
+   call daxpy(this%mat_size * this%mat_size, alfa, bmat%matrix, 1, &
+              this%matrix, 1)
+#endif
+
+   if (present(stat)) stat = my_stat
+end subroutine add_mat_r
+
+subroutine mat_mul_r(this, bmat, cmat, alfa, beta, stat)
+   implicit none
+   real(kind=8)  , intent(in)              :: alfa, beta
+   type(cumat_r) , intent(in)              :: bmat, cmat
+   class(cumat_r), intent(inout)           :: this
+   integer       , intent(inout), optional :: stat
+
+   integer :: my_stat = 0
+
+#ifdef CUBLAS
+   my_stat = cublas_dgemm('N', 'N', this%mat_size, this%mat_size,        &
+                          this%mat_size, alfa, bmat%cu_pointer,          &
+                          bmat%mat_size, cmat%cu_pointer, cmat%mat_size, &
+                          beta, this%cu_pointer, this%mat_size)
+#else
+   call dgemm('N', 'N', this%mat_size, this%mat_size, this%mat_size, alfa, &
+              bmat%matrix, bmat%mat_size, cmat%matrix, cmat%mat_size, beta,&
+              this%matrix, this%mat_size)
+#endif
+
+   if (present(stat)) stat = my_stat
+end subroutine mat_mul_r
+
+! COMPLEX
+subroutine add_mat_x(this, bmat, alfa, stat)
+   implicit none
+   TDCOMPLEX     , intent(in)              :: alfa
+   type(cumat_x) , intent(in)              :: bmat
+   class(cumat_x), intent(inout)           :: this
+   integer       , intent(inout), optional :: stat
+
+   integer :: my_stat = 0
+
+#ifdef CUBLAS
+   my_stat = cublas_xaxpy(this%mat_size * this%mat_size, alfa, &
+                          bmat%cu_pointer, 1, this%cu_pointer, 1)
+#else
+   call xaxpy(this%mat_size * this%mat_size, alfa, bmat%matrix, 1, &
+              this%matrix, 1)
+#endif
+
+   if (present(stat)) stat = my_stat
+end subroutine add_mat_x
+
+subroutine mat_mul_x(this, bmat, cmat, alfa, beta, stat)
+   implicit none
+   TDCOMPLEX     , intent(in)              :: alfa, beta
+   type(cumat_x) , intent(in)              :: bmat, cmat
+   class(cumat_x), intent(inout)           :: this
+   integer       , intent(inout), optional :: stat
+
+   integer :: my_stat = 0
+
+#ifdef CUBLAS
+   my_stat = cublas_xgemm('N', 'N', this%mat_size, this%mat_size,        &
+                          this%mat_size, alfa, bmat%cu_pointer,          &
+                          bmat%mat_size, cmat%cu_pointer, cmat%mat_size, &
+                          beta, this%cu_pointer, this%mat_size)
+#else
+   call xgemm('N', 'N', this%mat_size, this%mat_size, this%mat_size, alfa, &
+              bmat%matrix, bmat%mat_size, cmat%matrix, cmat%mat_size, beta,&
+              this%matrix, this%mat_size)
+#endif
+
+   if (present(stat)) stat = my_stat
+end subroutine mat_mul_x
---
+++ lioamber/typedef_cumat/cumat_multiply.f90
@@ -0,0 +1,47 @@
+! Grabs an input matrix (CPU) and multiplies with current cumat
+! storing the result in mat_result.
+subroutine multiply_r(this, output_matrix, input_matrix)
+#ifdef CUBLAS
+   use cublasmath, only: cumxp_r
+#endif
+   
+   implicit none
+   class(cumat_r), intent(in)    :: this
+   real(kind=8)  , intent(in)    :: input_matrix(:,:)
+   real(kind=8)  , intent(inout) :: output_matrix(:,:)
+
+   integer :: mat_size1, mat_size2
+   
+   mat_size1 = size(input_matrix,1)
+   mat_size2 = size(input_matrix,2)
+   
+#ifdef CUBLAS
+   call cumxp_r(input_matrix, this%cu_pointer, output_matrix, mat_size1)
+#else
+   output_matrix = matmul(this%matrix, input_matrix)
+#endif
+
+end subroutine multiply_r
+
+subroutine multiply_x(this, output_matrix, input_matrix)
+#ifdef CUBLAS
+   use cublasmath, only: cumxp
+#endif
+      
+   implicit none
+   class(cumat_x), intent(in)    :: this
+   TDCOMPLEX     , intent(in)    :: input_matrix(:,:)
+   TDCOMPLEX     , intent(inout) :: output_matrix(:,:)
+
+   integer :: mat_size1, mat_size2
+   
+   mat_size1 = size(input_matrix,1)
+   mat_size2 = size(input_matrix,2)
+
+#ifdef CUBLAS
+   call cumxp(input_matrix, this%cu_pointer, output_matrix, mat_size1)
+#else
+   output_matrix = matmul(this%matrix, input_matrix)
+#endif
+
+end subroutine multiply_x

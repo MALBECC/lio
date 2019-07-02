a92a11d48b5591821d727981b5362bf7dba6c7a5
francisco <francisco@logan.q1.fcen.uba.ar>
2019-07-02 11:31:12 -0300

    Ready to test in TD.

--- lioamber/fileio/fileio.f90
+++ lioamber/fileio/fileio.f90
@@ -3,6 +3,11 @@ module fileio_data
    logical :: style    = .false.
    integer :: verbose  = 3
    integer :: rst_dens = 0
+
+   integer           :: movie_count = 0
+   integer           :: movie_nfreq = 0
+   character(len=40) :: movie_name0 = "liomovie"
+
 contains
    subroutine get_style(style_o)
       implicit none
@@ -15,18 +20,20 @@ end module fileio_data
 
 module fileio
    implicit none
-   include 'restart_commons_h.f90'
-   include 'restart_coef_h.f90'
-   include 'restart_fock_h.f90'
-   include 'restart_rho_h.f90'
-   include 'restart_td_h.f90'
+#   include "restart_commons_h.f90"
+#   include "restart_coef_h.f90"
+#   include "restart_fock_h.f90"
+#   include "restart_rho_h.f90"
+#   include "restart_td_h.f90"
 contains
-   include 'restart_commons.f90'
-   include 'restart_coef.f90'
-   include 'restart_fock.f90'
-   include 'restart_rho.f90'
-   include 'restart_td.f90'
-   include 'output_init.f90'
-   include 'output_others.f90'
-   include 'output_scf.f90'
+#   include "restart_commons.f90"
+#   include "restart_coef.f90"
+#   include "restart_fock.f90"
+#   include "restart_rho.f90"
+#   include "restart_td.f90"
+#   include "output_init.f90"
+#   include "output_others.f90"
+#   include "output_scf.f90"
+#   include "movieprint.f90"
+#   include "movieaux.f90"
 end module fileio

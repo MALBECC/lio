1d291a19a628d6e38532bae9acac264743fe64dc
Federico N. Pedron <fedekbe@gmail.com>
2019-08-01 14:33:08 -0300

    (#301) Fixed CUMAT issues with Magnus/TB
     * Added "inv" option to basechange_gemm in order to give it the same functionality as its CUBLAS counterpart.
     * Fixed complex(kind) declaration in tbdft_data.
     * Fixed bug in cumat_get subroutine (CPU version), which was storing a new matrix instead of returning the one already stored.

--- lioamber/mathsubs/basechange_gemm.f
+++ lioamber/mathsubs/basechange_gemm.f
@@ -7,69 +7,86 @@
 ! (3) Second Product Utrp(nii,nni)*Matm(nni,ndd)
 !
 !%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%!
-       function basechange_d_gemm(M,Mati,Umat) result(Mato)
+       function basechange_d_gemm(M,Mati,Umat,mode) result(Mato)
        implicit none
        integer,intent(in)     :: M
+       character(len=3)       :: mode
        real*8,intent(in)      :: Umat(M,M)
        real*8,intent(in)      :: Mati(M,M)
        real*8,allocatable     :: Matm(:,:)
        real*8,allocatable     :: Mato(:,:)
-       integer                :: i,j
-!
-       allocate(Matm(M,M),Mato(M,M))
-       Matm=DBLE(0)
-       Mato=DBLE(0)
-!
-       call DGEMM('T','N',M,M,M,1.0D0,Umat,M,Mati,M,0.0D0,Matm,M)
-       call DGEMM('N','N',M,M,M,1.0D0,Matm,M,Umat,M,0.0D0,Mato,M)
-!
-       return;end function
-!
-!
-!
+       
+       allocate(Matm(M,M), Mato(M,M))
+       Matm = 0.0D0
+       Mato = 0.0D0
+
+       if (mode == 'inv') then
+          call DGEMM('N','N',M,M,M,1.0D0,Umat,M,Mati,M,0.0D0,Matm,M)
+          call DGEMM('N','T',M,M,M,1.0D0,Matm,M,Umat,M,0.0D0,Mato,M)
+       else
+          call DGEMM('T','N',M,M,M,1.0D0,Umat,M,Mati,M,0.0D0,Matm,M)
+          call DGEMM('N','N',M,M,M,1.0D0,Matm,M,Umat,M,0.0D0,Mato,M)
+       endif
+       deallocate(Matm)
+
+       return
+       end function
+
 !%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%!
        
-       function basechange_cc_gemm(M,Mati,Umat) result(Mato)
+       function basechange_cc_gemm(M,Mati,Umat,mode) result(Mato)
        implicit none
        integer,intent(in)     :: M
+       character(len=3)       :: mode
        complex*8,intent(in)   :: Umat(M,M)
        complex*8,intent(in)   :: Mati(M,M)
        complex*8,allocatable  :: Matm(:,:)
        complex*8,allocatable  :: Mato(:,:)
        COMPLEX*8 :: alpha,beta
-       integer                :: i,j
-!
+
        allocate(Matm(M,M),Mato(M,M))
-       Matm=DCMPLX(0,0)
-       Mato=DCMPLX(0,0)
-       alpha=cmplx(1.0D0,0.0D0)
-       beta=cmplx(0.0D0,0.0D0)
-!
-       call CGEMM('T','N',M,M,M,alpha,Umat,M,Mati,M,beta,Matm,M)
-       call CGEMM('N','N',M,M,M,alpha,Matm,M,Umat,M,beta,Mato,M)
-!
-       return; end function
-!
+       Matm  = cmplx(0.0E0,0.0E0)
+       Mato  = cmplx(0.0E0,0.0E0)
+       alpha = cmplx(1.0E0,0.0E0)
+       beta  = cmplx(0.0E0,0.0E0)
+       if (mode == 'inv') then
+          call CGEMM('N','N',M,M,M,alpha,Umat,M,Mati,M,beta,Matm,M)
+          call CGEMM('N','T',M,M,M,alpha,Matm,M,Umat,M,beta,Mato,M)
+       else
+          call CGEMM('T','N',M,M,M,alpha,Umat,M,Mati,M,beta,Matm,M)
+          call CGEMM('N','N',M,M,M,alpha,Matm,M,Umat,M,beta,Mato,M)
+       endif
+       deallocate(Matm)
+
+       return
+       end function
+
 !%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%!
-!
-       function basechange_zz_gemm(M,Mati,Umat) result(Mato)
+       function basechange_zz_gemm(M,Mati,Umat,mode) result(Mato)
        implicit none
        integer,intent(in)      :: M
+       character(len=3)        :: mode
        complex*16,intent(in)   :: Umat(M,M)
        complex*16,intent(in)   :: Mati(M,M)
        complex*16,allocatable  :: Matm(:,:)
        complex*16,allocatable  :: Mato(:,:)
        complex*16              :: alpha,beta
-       integer                 :: i,j
-!
+
        allocate(Matm(M,M),Mato(M,M))
-       Matm=DCMPLX(0,0)
-       Mato=DCMPLX(0,0)
-       alpha=cmplx(1.0D0,0.0D0)
-       beta=cmplx(0.0D0,0.0D0)
-!
-       call ZGEMM('T','N',M,M,M,alpha,Umat,M,Mati,M,beta,Matm,M)
-       call ZGEMM('N','N',M,M,M,alpha,Matm,M,Umat,M,beta,Mato,M)
-!
-       return;end function
+       Matm  = dcmplx(0.0D0,0.0D0)
+       Mato  = dcmplx(0.0D0,0.0D0)
+       alpha = dcmplx(1.0D0,0.0D0)
+       beta  = dcmplx(0.0D0,0.0D0)
+
+       if (mode == 'inv') then
+          call ZGEMM('N','N',M,M,M,alpha,Umat,M,Mati,M,beta,Matm,M)
+          call ZGEMM('N','T',M,M,M,alpha,Matm,M,Umat,M,beta,Mato,M)
+       else
+          call ZGEMM('T','N',M,M,M,alpha,Umat,M,Mati,M,beta,Matm,M)
+          call ZGEMM('N','N',M,M,M,alpha,Matm,M,Umat,M,beta,Mato,M)
+       endif
+       deallocate(Matm)
+
+       return
+       end function
 !%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%!

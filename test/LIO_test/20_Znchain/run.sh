91ca0542f96629ec59fd7d57256c17f594622d00
fedepedron <fedekbe@gmail.com>
2019-08-02 14:00:04 -0300

    Edited Zn test.

--- test/LIO_test/20_Znchain/run.sh
+++ test/LIO_test/20_Znchain/run.sh
@@ -1,12 +1,12 @@
 #! /bin/bash
 if [ -z "$LIOBIN" ] ; then
-  LIOBIN=../../liosolo/liosolo
+  LIOBIN=../../../liosolo/liosolo
 fi
-SALIDA=salida
+SALIDA=output
 if [ -n "$1" ]
   then
     SALIDA=$1
 fi
 
-$LIOBIN -i multiZn.in -b basis -c multiZn.xyz -v > ${SALIDA}_mod
+$LIOBIN -i multiZn.in -b basis -c multiZn.xyz -v > ${SALIDA}
 

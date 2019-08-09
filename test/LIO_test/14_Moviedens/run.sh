2350571851bf75409432d6485b817271c407b166
Francisco <francisco@logos-qimail.com>
2019-08-09 15:20:26 -0300

    Changed folder number and added the copying of nuclear coordinates (unnecesary for TD but necessary for Ehrenfest).
    Also, Ehrenfest will re-write in its moviepic_0001 the same that
    SCF writes in its moviepic_0000. Keep that in mind when reading
    the outputs.

--- test/LIO_test/14_Moviedens/run.sh
+++ test/LIO_test/14_Moviedens/run.sh
@@ -10,27 +10,35 @@ fi
 
 echo "This test will take 30 min to run in CPU."
 
-tail --lines=+3 chain_test.xyz > temp_coord.in
-sed -i 's/H / 1/g' temp_coord.in
-sed -i 's/C / 6/g' temp_coord.in
-sed -i 's/N / 7/g' temp_coord.in
-sed -i 's/O / 8/g' temp_coord.in
-sed -i 's/S /16/g' temp_coord.in
-sed -i 's/Fe/26/g' temp_coord.in
-sed -i 's/Cu/29/g' temp_coord.in
+FIXINP ()
+{
+    fxyz_inp=${1}
+    fxyz_out=${2}
+    tail --lines=+3 ${fxyz_inp} > ${fxyz_out}
+    sed -i 's/H / 1/g' ${fxyz_out}
+    sed -i 's/C / 6/g' ${fxyz_out}
+    sed -i 's/N / 7/g' ${fxyz_out}
+    sed -i 's/O / 8/g' ${fxyz_out}
+    sed -i 's/S /16/g' ${fxyz_out}
+    sed -i 's/Fe/26/g' ${fxyz_out}
+    sed -i 's/Cu/29/g' ${fxyz_out}
+}
 
 source ../../../liohome.sh
 
+FIXINP chain_test.xyz temp_coord.in
 cp restart_grnd.in restart.in
 $LIOBIN -i chain_grnd.in -c temp_coord.in -v >  chain_grnd.out
 cp liomovie_el0000.out dens_neg.in
 
+FIXINP chain_test.xyz temp_coord.in
 cp restart_edyn.in restart.in
 $LIOBIN -i chain_edyn.in -c temp_coord.in -v > chain_edyn.out
 
 RUN_1PIC ()
 {
     IDN=${1}
+    FIXINP liomovie_nu${IDN}.out temp_coord.in
     cp liomovie_el${IDN}.out dens_pos.in
     ../../../tools/densdif.py
     mv dens_dif.out restart.in
@@ -51,4 +59,3 @@ RUN_1PIC 0008
 RUN_1PIC 0009
 RUN_1PIC 0010
 
-rm liomovie_nu00*.out temp_coord.in

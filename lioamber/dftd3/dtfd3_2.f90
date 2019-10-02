fceaeded721d77d653603a7100df7d2d89a35a98
fedepedron <fedekbe@gmail.com>
2019-10-02 13:37:21 -0300

    Finished gradients for 2-bodies terms.

--- lioamber/dftd3/dtfd3_2.f90
+++ lioamber/dftd3/dtfd3_2.f90
@@ -23,26 +23,50 @@ subroutine dftd3_2bodies_e(e_2, dists, n_atoms)
    enddo   
 end subroutine dftd3_2bodies_e
 
-subroutine dftd3_2bodies_g(e_2, dists, pos, n_atoms)
+subroutine dftd3_2bodies_g(grad, dists, pos, n_atoms)
    use dftd3_data, only: c6_ab, c8_ab, r0_ab
    implicit none
    integer     , intent(in)    :: n_atoms
    real(kind=8), intent(in)    :: dists(:,:), pos(:)
-   real(kind=8), intent(inout) :: e_2
+   real(kind=8), intent(inout) :: grad(:,:)
 
-   real(kind=8) :: term
+   real(kind=8) :: rtemp, btemp, grad6, grad8
    integer      :: iatom, jatom
 
    do iatom = 1      , n_atoms
    do jatom = iatom+1, n_atoms
       ! r^6 terms.
-      term = 1.0D0 + 6.0D0 * ( (1.217D0 * r0_ab(iatom,jatom)) / &
-                                dists(iatom,jatom) ) ** 14
-      e_2 = e_2 + c6_ab(iatom,jatom) / (term * dists(iatom,jatom) ** 6)
+      rtemp = dists(iatom,jatom) ** 7
+      btemp = 1.217D0 * r0_ab(iatom,jatom)
+      btemp = btemp ** 7
 
-      ! r^8 term.
-      term = 1.0D0 + 6.0D0 * (r0_ab(iatom,jatom) / dists(iatom,jatom)) ** 16
-      e_2 = e_2 + c8_ab(iatom,jatom) / (term * dists(iatom,jatom) ** 8)
+      ! dE/dR = (- 6 * C6 * f / R^7) + (C6 / R^6 * df/dR)
+      grad6 = 1.0D0 + 6.0D0 * (btemp / rtemp) ** 2
+      grad6 = -6.0D0 * c6_ab(iatom,jatom) * grad6 / rtemp + &
+              (c6_ab(iatom,jatom) / rtemp) * 84.0D0 /     &
+              (6.0D0 * btemp / rtemp + rtemp / btemp ) ** 2
+
+      ! r^8 terms.
+      rtemp = dists(iatom,jatom) ** 8
+      btemp = r0_ab(iatom,jatom) ** 8
+
+      ! dE/dR = (- 8 * C8 * f / R^9) + (C8 / R^8 * df/dR)
+      grad8 = 1.0D0 + 6.0D0 * ( btemp / rtemp ) ** 2
+      grad8 = -8.0D0 * c8_ab(iatom,jatom) * grad8 / rtemp + &
+              (c8_ab(iatom,jatom) / rtemp) * 96.0D0 /     &
+              (6.0D0 * btemp / rtemp + rtemp / btemp ) ** 2
+      grad8 = grad8 / dists(iatom,jatom)
+
+      ! Finally, gradients are expressed as dE/dR * dR/dx
+      ! and dR/dx = (xi - xj) / R
+      btemp = (grad8 + grad6) / dists(iatom,jatom)
+
+      grad(iatom,1) = btemp * (pos(iatom,1) - pos(jatom,1))
+      grad(iatom,2) = btemp * (pos(iatom,2) - pos(jatom,2))
+      grad(iatom,3) = btemp * (pos(iatom,3) - pos(jatom,3))
+      grad(jatom,1) = btemp * (pos(jatom,1) - pos(iatom,1))
+      grad(jatom,2) = btemp * (pos(jatom,2) - pos(iatom,2))
+      grad(jatom,3) = btemp * (pos(jatom,3) - pos(iatom,3))
    enddo
    enddo   
 end subroutine dftd3_2bodies_g

63efb6ffd67bd442d59859f1cc2e50585f822160
fedepedron <fedekbe@gmail.com>
2019-10-04 14:25:28 -0300

    Makefile fixes.

--- lioamber/dftd3/dftd3.mk
+++ lioamber/dftd3/dftd3.mk
@@ -1,10 +1,5 @@
-included :=
-#included += dftd3_3.f90
-#included += dftd3_2.f90
-#included += dftd3_read_params.f90
-#included += dftd3_main.f90
+included := dftd3.o
 
-#dftd3_main.f90: dftd3_read_params.o dftd3_2.o dftd3_3.o
 dftd3.mod: dftd3_data.mod
 
 $(OBJPATH)/dftd3.o : $(included) dftd3.mk

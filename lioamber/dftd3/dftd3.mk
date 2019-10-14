8c904fc3f23ece7df7a70b77ee40a0925d0947f8
fedepedron <fedekbe@gmail.com>
2019-10-14 16:25:47 -0400

    Fixed makefile.

--- lioamber/dftd3/dftd3.mk
+++ lioamber/dftd3/dftd3.mk
@@ -1,3 +1,5 @@
-dftd3.o: dftd3_main.o dftd3_c6c8.o dftd3_2.o dftd3_3.o dftd3_read_params.o
+INCLUDES :=
+OBJECTS += $(INCLUDES)
 
-$(OBJPATH)/dftd3.o : $(included) dftd3.mk
+$(OBJPATH)/dftd3.o: dftd3_2.f90  dftd3_3.f90  dftd3_c6c8.f90  dftd3.f90  dftd3_main.f90  dftd3_read_params.f90  param_c6.f90  param_r0.f90
+$(OBJPATH)/dftd3.o: $(INCLUDES:%.o=$(OBJPATH)/%.o) dftd3.mk

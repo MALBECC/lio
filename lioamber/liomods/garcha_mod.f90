82a8fc4e9c353d4b7f765c9310b1c290d585d4ce
fedepedron <fedekbe@gmail.com>
2020-02-11 13:34:04 -0300

    Moved garcha_mod to f90.

--- lioamber/liomods/garcha_mod.f90
+++ lioamber/liomods/garcha_mod.f90
@@ -1,109 +1,90 @@
 !%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%!
-       module garcha_mod
-      implicit none
-      INCLUDE 'param.f90'
-      integer natom,ntatom,NCO,NUNP,igrid,igrid2
-     >  ,Iexch,nsol,npas,npasw,watermod,noconverge,
-     > converge,nang,propagator,NBCH
-      integer ex_functional_id, ec_functional_id
-      logical use_libxc
-      integer restart_freq, energy_freq
-      character*20 fcoord,fmulliken,frestart,frestartin,solv,solv2
-      logical MEMO,predcoef
-      logical OPEN,DIRECT,VCINP
-      logical sol
-      logical primera,writexyz
-      logical writeforces
+module garcha_mod
 
-      logical cubegen_only,cube_dens,cube_orb,cube_elec, cube_sqrt_orb
-      integer cube_res,cube_sel
-      character*20 cube_dens_file,cube_orb_file,cube_elec_file
+   implicit none
+   INCLUDE 'param.f90'
 
-      real*8 e_(50,3),wang(50),e_2(116,3),wang2(116),e3(194,3), ! intg1 e intg2
-     > wang3(194)                                               !
-      integer Nr(0:54),Nr2(0:54)
+   integer :: natom, ntatom, NCO, NUNP, igrid, igrid2, Iexch, nsol, npas, &
+              npasw, watermod, noconverge, converge, nang, propagator, NBCH
+   integer :: ex_functional_id, ec_functional_id 
+   logical :: use_libxc
+   integer :: restart_freq, energy_freq
+   character(len=20) :: fcoord, fmulliken, frestart, frestartin, solv, solv2
+   logical :: MEMO, predcoef, OPEN, DIRECT, VCINP, sol, primera, writexyz
+   logical :: writeforces
 
-      real*8, dimension (:,:), ALLOCATABLE :: r,v,rqm,d
-      real*8, dimension (:), ALLOCATABLE ::  Em, Rm, pc
-      integer, dimension (:), ALLOCATABLE :: Iz
+   ! Cubegen options.
+   logical :: cubegen_only, cube_dens, cube_orb, cube_elec, cube_sqrt_orb
+   integer :: cube_res, cube_sel
+   character(len=20) :: cube_dens_file, cube_orb_file, cube_elec_file
 
-      real*8 :: Rm2(0:54)
-c Everything is dimensioned for 2 basis, normal and density
-c ncf, lt,at,ct parameters for atomic basis sets
-
-      real*8, dimension (:), ALLOCATABLE :: Fmat_vec, Fmat_vec2,
-     > Pmat_vec, Hmat_vec, Ginv_vec, Gmat_vec, Pmat_en_wgt
-       real*8, dimension (:), ALLOCATABLE :: rhoalpha,rhobeta
-       real*8, dimension (:,:), ALLOCATABLE :: X
-
-       real*8 :: pi, pi32, rpi, pi5, pi52
-       real*8 :: piss, pis32, rpis, pis5, pis52
-       parameter(pi32=5.56832799683170698D0,pi=3.14159265358979312D0,
-     >          rpi=1.77245385090551588D0, pi5=34.9868366552497108D0,
-     >    pi52=34.9868366552497108D0)
-       parameter(pis32=5.56832799683170698E0,piss=3.14159265358979312E0,
-     >          rpis=1.77245385090551588E0, pis5=34.9868366552497108E0,
-     >    pis52=34.9868366552497108E0)
+   ! Grid options.
+   real(kind=8) :: e_(50,3), wang(50), e_2(116,3), wang2(116), e3(194,3), wang3(194)
+   integer      :: Nr(0:54), Nr2(0:54)
+   real(kind=8) :: Rm2(0:54)
 
 
-c Angular momenta : up to f functions ( could be easily extended if
-c necessary)
+   real(kind=8), dimension(:,:), allocatable ::  r, v, rqm, d
+   real(kind=8), dimension(:)  , allocatable ::  Em, Rm, pc
+   integer, dimension (:), allocatable :: Iz
 
-! FFR - My global variables
-!------------------------------------------------------------------------------!
-       real*8,allocatable,dimension(:,:) :: Smat
-       real*8,allocatable,dimension(:,:) :: RealRho
+   ! Density, Fock, and similar matrices and vectors.
+   real(kind=8), dimension (:), allocatable :: Fmat_vec, Fmat_vec2, Pmat_vec, &
+                                               Hmat_vec, Ginv_vec, Gmat_vec,  &
+                                               Pmat_en_wgt, rhoalpha, rhobeta
+   real(kind=8), dimension (:,:), allocatable :: X
 
-       logical                           :: doing_ehrenfest=.false.
-       logical                           :: first_step
-       real*8,allocatable,dimension(:)   :: atom_mass
-       real*8,allocatable,dimension(:,:) :: nucpos, nucvel
-       real*8                            :: total_time
-       real*8,allocatable,dimension(:,:) :: qm_forces_ds
-       real*8,allocatable,dimension(:,:) :: qm_forces_total
-!------------------------------------------------------------------------------!
+   real(kind=8),allocatable,dimension(:,:) :: Smat, RealRho
 
-!-Variables for property calculations.
-      logical :: fukui, dipole, lowdin, mulliken, print_coeffs
-      logical :: becke = .false.
+   ! For ehrenfest?
+   logical :: doing_ehrenfest=.false.
+   logical :: first_step
+   real(kind=8) :: total_time
+   real(kind=8),allocatable,dimension(:)   :: atom_mass
+   real(kind=8),allocatable,dimension(:,:) :: nucpos, nucvel
+   real(kind=8),allocatable,dimension(:,:) :: qm_forces_ds, qm_forces_total
 
-      integer :: nng, max_func
+   ! Variables for property calculations.
+   logical :: fukui, dipole, lowdin, mulliken, print_coeffs
+   logical :: becke = .false.
 
-! GPU OPTIONS
-      logical :: assign_all_functions, remove_zero_weights,
-     >              energy_all_iterations
-      real*8  :: free_global_memory, sphere_radius, little_cube_size
-      integer :: min_points_per_cube, max_function_exponent
+   ! GPU OPTIONS and G2G
+   integer :: nng, max_func
 
-! Energy contributions
-      real*8 :: Enucl
-      real*8,dimension(:)  ,allocatable :: Eorbs, Eorbs_b
-! need this for lowdin
-      real*8,dimension(:,:),allocatable :: sqsm
-!-Variables for distance combination restrain
-      INTEGER :: number_restr, number_index
-      INTEGER, ALLOCATABLE, DIMENSION(:,:) :: restr_pairs
-      INTEGER, ALLOCATABLE, DIMENSION(:) ::  restr_index
-      DOUBLE PRECISION, ALLOCATABLE, DIMENSION(:) :: restr_k,restr_w,
-     > restr_r0
-!-Debug. Activates check of NaN in Fock and Rho
-      Logical :: Dbug
-      integer :: timers
+   logical :: assign_all_functions, remove_zero_weights, energy_all_iterations
+   real(kind=8)  :: free_global_memory, sphere_radius, little_cube_size
+   integer :: min_points_per_cube, max_function_exponent
 
-      real*8, dimension (:,:), ALLOCATABLE :: MO_coef_at, MO_coef_at_b
-!Geometry optimizations
-      logical :: steep !enables steepest decend algorithm
-      real*8 :: Force_cut, Energy_cut, minimzation_steep !energy and force convergence crit and initial steep
-      integer :: n_points ! number of points scaned for lineal search
-      integer :: n_min_steeps !number of optimization steps
-      integer :: charge, gpu_level=4
-      logical :: lineal_search !enable lineal search
+   ! Energy contributions
+   real(kind=8) :: Enucl
+   real(kind=8),dimension(:)  ,allocatable :: Eorbs, Eorbs_b
+   real(kind=8),dimension(:,:),allocatable :: sqsm
 
-! for properties calculation control
-      logical :: calc_propM
+   ! Variables for distance combination restrains
+   integer :: number_restr, number_index
+   integer, allocatable, dimension(:,:) :: restr_pairs
+   integer, allocatable, dimension(:) ::  restr_index
+   real(kind=8), allocatable, dimension(:) :: restr_k, restr_w, restr_r0
 
-! for pbe0 functional
-      logical :: PBE0 = .false.
+   ! Debug. Activates check of NaN in Fock and Rho
+   logical :: Dbug
+   integer :: timers
 
+   real(kind=8), dimension (:,:), allocatable :: MO_coef_at, MO_coef_at_b
+
+   ! Geometry optimizations
+   logical :: steep !enables steepest decend algorithm
+   real(kind=8) :: Force_cut, Energy_cut, minimzation_steep !energy and force convergence crit and initial steep
+   integer :: n_points ! number of points scaned for lineal search
+   integer :: n_min_steeps !number of optimization steps
+   integer :: charge, gpu_level=4
+   logical :: lineal_search !enable lineal search
+
+   ! For properties calculation control
+   logical :: calc_propM
+
+   ! For pbe0 functional
+   logical :: PBE0 = .false.
+
+end module garcha_mod
 !%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%!
-      end module

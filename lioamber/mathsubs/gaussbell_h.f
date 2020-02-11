3bbd53ca836f059121a58d54340aa8d1128b4611
fedepedron <fedekbe@gmail.com>
2020-02-11 12:44:38 -0300

    Removed unused files.

--- lioamber/mathsubs/gaussbell_h.f
+++
@@ -1,6 +0,0 @@
-!%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%!
-       interface gaussbell
-         module procedure gaussbell_d
-         module procedure gaussbell_z
-       end interface
-!%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%!
--- lioamber/mathsubs/mathsubs.mk
+++ lioamber/mathsubs/mathsubs.mk
@@ -1,9 +1,7 @@
 ######################################################################
 # INTERNAL DEPENDENCIES
 INCLUDES :=
-INCLUDES += gaussbell.f90 gaussbell_h.f90
 INCLUDES += commutator.f90 commutator_h.f90
-INCLUDES += basechange.f90 basechange_h.f90
 INCLUDES += basechange_gemm.f90 basechange_gemm_h.f90
 
 $(OBJPATH)/mathsubs.o : $(INCLUDES) mathsubs.mk

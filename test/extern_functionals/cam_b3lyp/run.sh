cf7170aa2eca36546c4e6c317f8b94e5773822e6
gonzalodm <gonzalodm20@gmail.com>
2020-04-16 12:29:33 -0300

    Tests with extern functionals

---
+++ test/extern_functionals/cam_b3lyp/run.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+if [ -z "$LIOBIN" ] ; then
+  LIOBIN=../../../liosolo/liosolo
+fi
+SALIDA=output
+if [ -n "$1" ]
+  then
+    SALIDA=$1
+fi
+
+source ../../../liohome.sh
+export OMP_NUM_THREADS=1
+export GFORTRAN_UNBUFFERED_ALL=1
+$LIOBIN -i agua.in -b basis -c agua.xyz -v > $SALIDA

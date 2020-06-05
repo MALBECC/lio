1d5b63336c29253b9fbc9362fc6106f10a4dd66e
fedepedron <fedekbe@gmail.com>
2020-06-05 15:29:37 -0300

    Finished data input read.

--- lioamber/ljswitch.f90
+++ lioamber/ljswitch.f90
@@ -9,6 +9,8 @@ module LJ_switch_data
       LIODBLE :: s2
       LIODBLE :: e1
       LIODBLE :: e2
+      LIODBLE :: eps
+      LIODBLE :: sig
    end type lj_atom
 
    integer :: n_lj_atoms
@@ -23,12 +25,14 @@ module LJ_switch
 
 contains
 
-subroutine lj_input_read(input_UID)
+subroutine lj_input_read(input_UID, verbose_lvl)
    use LJ_switch_data, only: n_lj_atoms, lj_atoms
    implicit none
    integer, intent(in) :: input_UID
+   integer, intent(in) :: verbose_lvl
     
    character(len=10) :: buffer
+   character(len=50) :: print_fmt
    integer           :: ios, iatom
     
    rewind(input_UID)
@@ -59,14 +63,25 @@ subroutine lj_input_read(input_UID)
    if (allocated(lj_atoms)) deallocate(lj_atoms(n_lj_atoms))
 
    ! Starts reading LJ switch data.
-   write(*,'(A)') " AtomID |  Q1  |  Q2   |  Sigma1  | Sigma2 | Epsilon1 "&
-                 &"| Epsilon2 "
    do iatom = 1, n_lj_atoms
       read(input_UID,*) lj_atoms(iatom)%idx, lj_atoms(iatom)%q1, &
                         lj_atoms(iatom)%q2 , lj_atoms(iatom)%s1, &
                         lj_atoms(iatom)%s2 , lj_atoms(iatom)%e1, &
                         lj_atoms(iatom)%e2
+      if (verbose_lvl > 3) then
+         print_fmt = "(A5, 1x, I3, 5x, A6, I3)"
+         write(*,print_fmt) "Atom: ", iatom, "Index: " lj_atoms(iatom)%idx
+
+         print_fmt = "(A5, 1x, I1, A6, F12.6, A10, F12.6, A12, F12.6)"
+         write(*,print_fmt) "Atom", 1, " - Q: ", lj_atoms(iatom)%q1,         &
+                            " - Sigma: ", lj_atoms(iatom)%s1," - Epsilon: ", &
+                            lj_atoms(iatom)%e1
+         write(*,print_fmt) "Atom", 21, " - Q: ", lj_atoms(iatom)%q2,        &
+                            " - Sigma: ", lj_atoms(iatom)%s2," - Epsilon: ", &
+                            lj_atoms(iatom)%e2
+      endif
    enddo
+   write(*,'(A)') ""
    
  end subroutine lj_input_read
 

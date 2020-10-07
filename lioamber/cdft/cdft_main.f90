b47597ad6a236efed807ff2df7cec99fdc34daa0
fedepedron <fedekbe@gmail.com>
2020-10-07 11:51:58 -0300

    Fixed regular CDFT bug.

--- lioamber/cdft/cdft_main.f90
+++ lioamber/cdft/cdft_main.f90
@@ -40,25 +40,25 @@ subroutine CDFT(fock_a, rho_a, fock_b, rho_b, Pmat_v, coefs, coefs_b, overlap, &
          ! Calculates perturbations and Jacobian.
          call cdft_get_deltaV(fock_a, rho_a, fock_b, rho_b)
          call cdft_set_potential()
-      elseif (cdft_c%mixed) then
-         ! Gets W for state 1, retrieves MO
-         call cdft_mixed_set_coefs(coefs, .true., 1)
-
-         allocate(Wmat_vec(size(Pmat_v,1)))
-         Wmat_vec = 0.0D0
-         call g2g_cdft_w(Wmat_vec)
-
-         if (op_shell) then
-            call cdft_mixed_set_coefs(coefs_b, .false., 1)
-            call cdft_mixed_invert_spin()
-            
-            allocate(Wmat_vec_b(size(Pmat_v,1)))
-            Wmat_vec_b = 0.0D0
-            call g2g_cdft_w(Wmat_vec_b)
-         endif
-
       endif
    enddo
+   if (cdft_c%mixed .and. cdft_converged) then
+      ! Gets W for state 1, retrieves MO
+      call cdft_mixed_set_coefs(coefs, .true., 1)
+
+      allocate(Wmat_vec(size(Pmat_v,1)))
+      Wmat_vec = 0.0D0
+      call g2g_cdft_w(Wmat_vec)
+
+      if (op_shell) then
+         call cdft_mixed_set_coefs(coefs_b, .false., 1)
+         call cdft_mixed_invert_spin()
+         
+         allocate(Wmat_vec_b(size(Pmat_v,1)))
+         Wmat_vec_b = 0.0D0
+         call g2g_cdft_w(Wmat_vec_b)
+      endif
+   endif
 
    ! If it is a mixed calculation, repeats everything for the second state.
    if (cdft_c%mixed) then
@@ -104,6 +104,32 @@ subroutine CDFT(fock_a, rho_a, fock_b, rho_b, Pmat_v, coefs, coefs_b, overlap, &
             endif
          endif
       enddo
+
+      if (cdft_converged) then
+         ! Retrieves MO and W for state 2
+         call cdft_mixed_set_coefs(coefs, .true., 2)
+            
+         ! We accumulate 1+2 over Wmat_vec and then extract it.
+         call g2g_cdft_w(Wmat_vec)
+
+         deallocate(Wmat); allocate(Wmat(nbasis,nbasis))
+         Wmat = 0.0D0
+         call spunpack('L', nbasis, Wmat_vec, Wmat)
+         deallocate(Wmat_vec)
+
+
+         if (op_shell) then
+            call cdft_mixed_set_coefs(coefs_b, .false., 2)
+            call cdft_mixed_invert_spin()
+              
+            call g2g_cdft_w(Wmat_vec_b)
+
+            deallocate(Wmat_b); allocate(Wmat_b(nbasis,nbasis))
+            Wmat_b = 0.0D0
+            call spunpack('L', nbasis, Wmat_vec_b, Wmat_b)
+            deallocate(Wmat_vec_b)
+         endif
+      endif
 
       allocate(Hmat(2,2))
       call cdft_mixed_hab(energ, energ2, Wmat, Wmat_b, overlap, op_shell, Hmat)

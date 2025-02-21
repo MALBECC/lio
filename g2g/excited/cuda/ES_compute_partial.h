e26e3ba0bc6f20564b9c62b3df8b725fe9aa384c
diegoarmino <12549327+diegoarmino@users.noreply.github.com>
2025-02-21 16:08:53 -0300

    Revert "liosolo/Makefile:"
    This reverts commit 84bdeceda9a0a6ffa31786569cc2998ae33df5ee.

--- g2g/excited/cuda/ES_compute_partial.h
+++ g2g/excited/cuda/ES_compute_partial.h
@@ -60,8 +60,7 @@ __global__ void ES_compute_partial(uint points,
           scalar_type rdm_this_thread2;
 
           // Transition density
-          // rdm_this_thread = tred_gpu[(bj + j) + mc*i2]; //Esta bien definida?
-          rdm_this_thread2 = tred_gpu[(bj + j) + mc*i2];
+          rdm_this_thread = tred_gpu[(bj + j) + mc*i2];
           z2  += rdm_this_thread2 * fjreg;
           z32 += fgjreg * rdm_this_thread2;
         }

5131958976443f8c973c812f32aa49ca99e98517
diegoarmino <12549327+diegoarmino@users.noreply.github.com>
2025-04-23 14:29:02 -0300

    Corrected some minor grammar mistakes in printed messages

--- cmakefiles/compileOptions.cmake
+++ cmakefiles/compileOptions.cmake
@@ -1,12 +1,12 @@
 # Compile Options
-option(CPU "LIO will be compiled using CPUs only" ON)
-option(GPU "LIO will be compiled using GPUs and CPUs" OFF)
-option(EXTERNAL "LIO will be compiled using Libxc and Libint libraries" OFF)
-option(COMPILE_EXTERNAL "LIO will compile libxc and libint and then 
-                         will make the link with LIO" OFF)
+option(CPU "LIO will be compiled to run on CPUs only" ON)
+option(CUDA "LIO will be compiled with CUDA support to run on GPUs and CPUs" OFF)
+option(EXTERNAL "LIO will be compiled with support for Libxc and Libint libraries" OFF)
+option(COMPILE_EXTERNAL "LIO will compile libxc and libint libraries and then 
+                         link them to LIO libraries and executables" OFF)
 option(DOUBLE "LIO will be compiled using Double Precision" OFF)
 option(WARNING "LIO will be compiled using Warning Flags" OFF)
 
-if (GPU) 
-   message(FATAL_ERROR "For the moment no GPU compilations is provided in cmake")
+if (CUDA) 
+   message(FATAL_ERROR "For the moment no CUDA support is provided with CMake.")
 endif()

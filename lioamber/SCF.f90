87c787cebff73a7996f28af7a744a4878c6ef034
Jonathan Semelak <jota.smk@gmail.com>
2025-09-05 15:47:19 -0300

    modularize df verbosity

--- lioamber/SCF.f90
+++ lioamber/SCF.f90
@@ -65,6 +65,7 @@ subroutine SCF(E, fock_aop, rho_aop, fock_bop, rho_bop)
    use dftd3, only: dftd3_energy
    use properties, only: do_lowdin
    use extern_functional_subs, only: libint_init, exact_exchange, exact_energies
+   use density_fitting_verbosity, only: write_af_record, write_propd
 
 !%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%!
 
@@ -645,8 +646,7 @@ subroutine SCF(E, fock_aop, rho_aop, fock_bop, rho_bop)
         Smat_checkpoint=Smat
         Fmat_vec_checkpoint=Fmat_vec
         En_checkpoint=En
-        call int1(En, Fmat_vec, Hmat_vec, Smat, d, r, Iz, natom, &
-                  ntatom)
+        call int1(En, Fmat_vec, Hmat_vec, Smat, d, r, Iz, natom, ntatom)
         if (nsol.gt.0.and.igpu.ge.1) then
           call aint_qmmm_init(0,r,pc)
           call aint_qmmm_fock(E1s,Etrash)
@@ -656,34 +656,16 @@ subroutine SCF(E, fock_aop, rho_aop, fock_bop, rho_bop)
         do kk=1,MM
           E1s = E1s + Pmat_vec(kk) * Hmat_vec(kk)
         enddo
-        Es = Ens
-        Es = Es + E1 - E1s
-        open(unit = 37744083, file='af',action='write',position='append')
-        write(37744083, *) "TEST"
-        write(37744083, *) niter
-        write(37744083, *) Md
-        write(37744083, *) E1
-        write(37744083, *) E2
-        write(37744083, *) Es
-        write(37744083, *) Exc
-!        write(37744083, *) kinE
-        write(37744083, *) En_checkpoint+E1+E2+Exc
-        do afindex=1,Md
-          write(37744083,'(F18.9)') af(afindex)
-        end do
-        write(37744083, *) "END"
-        close(unit = 37744083)
+
+        call write_af_record(niter, Md, E1, E2, Es, Exc, En_checkpoint, af)
+        
+        if (niter .eq. 1) call write_propd(Nucd, ad, cd, Md)
+        
+        !Restore state
         Hmat_vec = Hmat_vec_checkpoint
         Smat = Smat_checkpoint
         Fmat_vec = Fmat_vec_checkpoint
         En = En_checkpoint
-        if (niter.eq. 1) then
-        open(unit = 37744084, file='propd',action='write',position='append')
-        do afindex=1,Md
-          write(37744084,*) Nucd(afindex), ad(afindex,1), cd(afindex,1)
-        end do
-        close(unit = 37744084)
-        endif
 
       end if
       !-----------------------------------------------------------------------!
---
+++ lioamber/fileio/density_fitting_verbosity.f90
@@ -0,0 +1,120 @@
+#include "../datatypes/datatypes.fh"
+module density_fitting_verbosity
+  implicit none
+  private
+  public :: write_af_record, write_propd
+
+contains
+
+  ! Return a free I/O unit number (F90-safe)
+  integer function get_free_unit()
+    implicit none
+    integer :: u
+    logical :: is_open
+    do u = 10, 999
+      inquire(unit=u, opened=is_open)
+      if (.not. is_open) then
+        get_free_unit = u
+        return
+      end if
+    end do
+    get_free_unit = -1
+  end function get_free_unit
+
+
+  ! Write AF block
+  subroutine write_af_record(niter, Md, E1, E2, Es, Exc, En0, af, &
+                             filename, append_mode)
+    implicit none
+    ! inputs
+    integer, intent(in) :: niter, Md
+    LIODBLE, intent(in) :: E1, E2, Es, Exc, En0
+    LIODBLE, intent(in) :: af(Md)
+    character(*), intent(in), optional :: filename
+    logical, intent(in),     optional :: append_mode
+    ! locals
+    integer :: u, ios, i
+    character(len=256) :: fname
+    logical :: do_append
+
+    fname = 'df_coeffs.log'
+    if (present(filename)) fname = filename
+    do_append = .true.
+    if (present(append_mode)) do_append = append_mode
+
+    u = get_free_unit()
+    if (u .lt. 0) then
+      write(*,*) 'density_fitting_verbosity: Error: no free unit available to open "', trim(fname), '"'
+      return
+    end if
+
+    if (do_append) then
+      open(unit=u, file=trim(fname), status='unknown', position='append', &
+           action='write', iostat=ios)
+    else
+      open(unit=u, file=trim(fname), status='replace', action='write', iostat=ios)
+    end if
+    if (ios .ne. 0) then
+      write(*,*) 'density_fitting_verbosity: Error opening file "', trim(fname), '", iostat=', ios
+      return
+    end if
+
+    write(u, *) niter
+    write(u, *) Md
+    write(u, *) E1
+    write(u, *) E2
+    write(u, *) Exc
+    write(u, *) En0 + E1 + E2 + Exc
+
+    do i = 1, Md
+      write(u, '(F18.9)') af(i)
+    end do
+    write(u, *) 'END'
+
+    close(u)
+  end subroutine write_af_record
+
+
+  ! Write PROPD (nuc index + first components of ad, cd)
+  subroutine write_propd(Nucd, ad, cd, Md, filename, append_mode)
+    implicit none
+    integer, intent(in) :: Md
+    integer, intent(in) :: Nucd(Md)
+    LIODBLE, intent(in) :: ad(Md, *), cd(Md, *)
+    character(*), intent(in), optional :: filename
+    logical, intent(in),     optional :: append_mode
+    integer :: u, ios, i
+    character(len=256) :: fname
+    logical :: do_append
+
+    fname = 'df_props.log'
+    if (present(filename)) fname = filename
+    do_append = .true.
+    if (present(append_mode)) do_append = append_mode
+
+    u = get_free_unit()
+    if (u .lt. 0) then
+      write(*,*) 'density_fitting_verbosity: Error: no free unit available to open "', trim(fname), '"'
+      return
+    end if
+
+    if (do_append) then
+      open(unit=u, file=trim(fname), status='unknown', position='append', &
+           action='write', iostat=ios)
+    else
+      open(unit=u, file=trim(fname), status='replace', action='write', iostat=ios)
+    end if
+    if (ios .ne. 0) then
+      write(*,*) 'density_fitting_verbosity: Error opening file "', trim(fname), '", iostat=', ios
+      return
+    end if
+
+    do i = 1, Md
+      write(u,*) Nucd(i), ad(i,1), cd(i,1)
+    end do
+
+    close(u)
+  end subroutine write_propd
+
+end module density_fitting_verbosity
+
